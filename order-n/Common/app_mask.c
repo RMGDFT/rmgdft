966bbb21ff81601b6e7520db09fe9bbe90976f94
Wenchang Lu <wlu2@ncsu.edu>
2011-02-22 16:02:01 +0000

    fixed the bug in app_mask for more than one multigrid level.
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@1262 eb38b3b0-33ea-0310-94ad-d43fea0d214f

--- order-n/Common/app_mask.c
+++ order-n/Common/app_mask.c
@@ -12,19 +12,22 @@ void app_mask(int istate, double *u, int level)
     char *maskptr;
     double time1, time2;
     int idx, istart;
+    int size;
 
 
     time1 = my_crtc();
 
     maskptr = ct.states[istate].lmask[level];
+    
+    size = ct.states[istate].size/(1<<level)/(1<<level)/(1<<level);
 
     if (maskptr != NULL)
-        for (idx = 0; idx < ct.states[istate].size; idx++)
+        for (idx = 0; idx < size; idx++)
         {
             u[idx] *= (double) maskptr[idx];
         }
     else
-        for (idx = 0; idx < ct.states[istate].size; idx++)
+        for (idx = 0; idx < size; idx++)
             u[idx] = 0.;
 
     time2 = my_crtc();
