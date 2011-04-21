d3190a8d5aff8798d5462ffe47e85ddf6a34dbd4
Miroslav Hodak <mhodak@ncsu.edu>
2011-04-21 08:50:05 +0000

    Real-space US: Adding code for mixing local <beta|psi> into mix_betaxpsi.c.
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@1336 eb38b3b0-33ea-0310-94ad-d43fea0d214f

--- ultrasoft/Common/mix_betaxpsi.c
+++ ultrasoft/Common/mix_betaxpsi.c
@@ -62,13 +62,44 @@ void mix_betaxpsi (int mix)
 
     else
     {
-	my_copy (newsintR, oldsintR, ct.num_kpts * ct.num_ions * ct.num_states * ct.max_nl);
+	my_copy (newsintR, oldsintR, size);
 #if !GAMMA_PT
-	my_copy (newsintI, oldsintI, ct.num_kpts * ct.num_ions * ct.num_states * ct.max_nl);
+	my_copy (newsintI, oldsintI, size);
+#endif
+    }
+    
+
+    
+    /*Local version*/
+      newsintR = pct.newsintR_local;
+      oldsintR = pct.oldsintR_local;
+#if !GAMMA_PT
+      newsintI = pct.newsintI_local;
+      oldsintI = pct.oldsintI_local;
+#endif
+    
+      size = ct.num_kpts * pct.num_nonloc_ions * ct.num_states * ct.max_nl;
+    
+      
+      if (mix)
+    {
+	my_scal( 1.0 - ct.prjmix, oldsintR, size);
+	my_axpy(ct.prjmix, newsintR, oldsintR, size); 
+
+#if !GAMMA_PT
+	my_scal( 1.0 - ct.prjmix, oldsintI, size);
+	my_axpy(ct.prjmix, newsintI, oldsintI, size); 
 #endif
     }
 
-    
+
+    else
+    {
+	my_copy (newsintR, oldsintR, size);
+#if !GAMMA_PT
+	my_copy (newsintI, oldsintI, size);
+#endif
+    }
 
 }
 
