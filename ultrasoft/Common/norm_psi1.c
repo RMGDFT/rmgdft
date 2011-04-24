d283dbf43ffd237beccef5c08b346758a7629872
Miroslav Hodak <mhodak@ncsu.edu>
2011-04-24 08:13:56 +0000

    Real-space US: Adding update for local <beta|psi> into norm_psi1.c.
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@1337 eb38b3b0-33ea-0310-94ad-d43fea0d214f

--- ultrasoft/Common/norm_psi1.c
+++ ultrasoft/Common/norm_psi1.c
@@ -12,7 +12,7 @@
 void norm_psi1 (STATE * sp, int istate, int kidx)
 {
 
-    int idx, ion, nh, i, j, size, incx = 1, inh, sidx;
+    int idx, ion, nh, i, j, size, incx = 1, inh, sidx, sidx_local;
     REAL sumbetaR, sumpsi, sum, t1;
     REAL *tmp_psiR, *tmp_psiI, *qqq, *sintR, *sintI, *ptr;
     ION *iptr;
@@ -110,6 +110,24 @@ void norm_psi1 (STATE * sp, int istate, int kidx)
         QMD_sscal (ct.max_nl, t1, ptr, incx);
 #if !GAMMA_PT
         ptr = &iptr->newsintI[sidx];
+        QMD_sscal (ct.max_nl, t1, ptr, incx);
+#endif
+    }
+    
+    
+    /* update <beta|psi> - Local version*/
+    sidx_local = kidx * pct.num_nonloc_ions * ct.num_states * ct.max_nl + istate * ct.max_nl;
+    
+    for (ion = 0; ion < pct.num_nonloc_ions; ion++)
+    {
+
+	ptr = &pct.oldsintR_local[ion * ct.num_states * ct.max_nl];
+        ptr += sidx_local;
+        
+	QMD_sscal (ct.max_nl, t1, ptr, incx);
+#if !GAMMA_PT
+	ptr = pct.oldsintI_local[ion * ct.num_states * ct.max_nl];
+        ptr += sidx_local;
         QMD_sscal (ct.max_nl, t1, ptr, incx);
 #endif
     }
