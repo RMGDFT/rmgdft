ad854f2d1211e3682703506534e475426fd42c76
Emil Briggs <elbriggs@ncsu.edu>
2011-06-03 15:00:57 +0000

    Modified app_cir_sixth to exploit symmetries in the operator when the grid size is evenly divisible by 4
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@1485 eb38b3b0-33ea-0310-94ad-d43fea0d214f

--- RMG/Common/app_cir_sixth.c
+++ RMG/Common/app_cir_sixth.c
@@ -17,6 +17,9 @@ static void app_cir_sixth_global (REAL * a, REAL * b);
 // dimensions correpsond to the global case. If so we call a routine with those
 // dimensions set at compile time. If not we just fall through to the general case.
 
+// For the global grid case we also implement an optimized version of the operator
+// that takes advantage of certain symmetries to improve performance.
+
 void app_cir_sixth (REAL * a, REAL * b, int dimx, int dimy, int dimz)
 {
 
@@ -111,7 +114,7 @@ void app_cir_sixth_global (REAL * a, REAL * b)
     int ixs, iys, ixms, ixps, iyms, iyps;
     int incy, incx, ixmms, ixpps, iymms, iypps;
     int incyr, incxr;
-    REAL *rptr;
+    REAL *rptr, rz, rzps, rzms, rzpps;
     REAL c000, c100, c110, c200;
 
     incx = (PZ0_GRID + 4) * (PY0_GRID + 4);
@@ -128,6 +131,70 @@ void app_cir_sixth_global (REAL * a, REAL * b)
     c100 = 13.0 / 180.0;
     c110 = 1.0 / 144.0;
     c200 = -1.0 / 240.0;
+
+
+    // Handle the general case first
+    if(PZ0_GRID % 4) {
+
+        for (ix = 2; ix < PX0_GRID + 2; ix++)
+        {
+            ixs = ix * incx;
+            ixms = (ix - 1) * incx;
+            ixps = (ix + 1) * incx;
+            ixmms = (ix - 2) * incx;
+            ixpps = (ix + 2) * incx;
+
+            for (iy = 2; iy < PY0_GRID + 2; iy++)
+            {
+                iys = iy * incy;
+                iyms = (iy - 1) * incy;
+                iyps = (iy + 1) * incy;
+                iymms = (iy - 2) * incy;
+                iypps = (iy + 2) * incy;
+
+                for (iz = 2; iz < PZ0_GRID + 2; iz++)
+                {
+
+                    b[(ix - 2) * incxr + (iy - 2) * incyr + (iz - 2)] =
+                        c100 * (rptr[ixs + iys + (iz - 1)] +
+                                rptr[ixs + iys + (iz + 1)] +
+                                rptr[ixms + iys + iz] +
+                                rptr[ixps + iys + iz] +
+                                rptr[ixs + iyms + iz] +
+                                rptr[ixs + iyps + iz]) + c000 * rptr[ixs + iys + iz];
+
+                    b[(ix - 2) * incxr + (iy - 2) * incyr + (iz - 2)] +=
+                        c110 * (rptr[ixps + iyps + iz] +
+                                rptr[ixps + iyms + iz] +
+                                rptr[ixms + iyps + iz] +
+                                rptr[ixms + iyms + iz] +
+                                rptr[ixps + iys + (iz + 1)] +
+                                rptr[ixps + iys + (iz - 1)] +
+                                rptr[ixms + iys + (iz + 1)] +
+                                rptr[ixms + iys + (iz - 1)] +
+                                rptr[ixs + iyps + (iz + 1)] +
+                                rptr[ixs + iyps + (iz - 1)] +
+                                rptr[ixs + iyms + (iz + 1)] + rptr[ixs + iyms + (iz - 1)]);
+
+                    b[(ix - 2) * incxr + (iy - 2) * incyr + (iz - 2)] +=
+                        c200 * (rptr[ixs + iys + (iz - 2)] +
+                                rptr[ixs + iys + (iz + 2)] +
+                                rptr[ixmms + iys + iz] +
+                                rptr[ixpps + iys + iz] +
+                                rptr[ixs + iymms + iz] + rptr[ixs + iypps + iz]);
+
+                }                   /* end for */
+
+            }                       /* end for */
+
+        }                           /* end for */
+
+        my_free (rptr);
+        return;
+    }
+
+
+    // Optimized case for dimz divisible by 4
     for (ix = 2; ix < PX0_GRID + 2; ix++)
     {
         ixs = ix * incx;
@@ -144,36 +211,139 @@ void app_cir_sixth_global (REAL * a, REAL * b)
             iymms = (iy - 2) * incy;
             iypps = (iy + 2) * incy;
 
-            for (iz = 2; iz < PZ0_GRID + 2; iz++)
+            // Compute the middle set of edges before entry to the loop
+            rz = rptr[ixms + iys + 2] +
+                 rptr[ixps + iys + 2] +
+                 rptr[ixs + iyms + 2] +
+                 rptr[ixs + iyps + 2];
+
+            // Compute the trailing set of edges before entry to the  loop
+            rzms = rptr[ixps + iys + 1] +
+                   rptr[ixms + iys + 1] +
+                   rptr[ixs + iyps + 1] +
+                   rptr[ixs + iyms + 1];
+
+
+            for (iz = 2; iz < PZ0_GRID + 2; iz+=4)
             {
 
+                // Forward set of edges
+                rzps = rptr[ixps + iys + (iz + 1)] +
+                       rptr[ixms + iys + (iz + 1)] +
+                       rptr[ixs + iyps + (iz + 1)] +
+                       rptr[ixs + iyms + (iz + 1)];
+
+                // First
                 b[(ix - 2) * incxr + (iy - 2) * incyr + (iz - 2)] =
                     c100 * (rptr[ixs + iys + (iz - 1)] +
                             rptr[ixs + iys + (iz + 1)] +
-                            rptr[ixms + iys + iz] +
-                            rptr[ixps + iys + iz] +
-                            rptr[ixs + iyms + iz] +
-                            rptr[ixs + iyps + iz]) + c000 * rptr[ixs + iys + iz];
+                            rz) + c000 * rptr[ixs + iys + iz];
+
 
                 b[(ix - 2) * incxr + (iy - 2) * incyr + (iz - 2)] +=
                     c110 * (rptr[ixps + iyps + iz] +
                             rptr[ixps + iyms + iz] +
                             rptr[ixms + iyps + iz] +
                             rptr[ixms + iyms + iz] +
-                            rptr[ixps + iys + (iz + 1)] +
-                            rptr[ixps + iys + (iz - 1)] +
-                            rptr[ixms + iys + (iz + 1)] +
-                            rptr[ixms + iys + (iz - 1)] +
-                            rptr[ixs + iyps + (iz + 1)] +
-                            rptr[ixs + iyps + (iz - 1)] +
-                            rptr[ixs + iyms + (iz + 1)] + rptr[ixs + iyms + (iz - 1)]);
+                            rzms +
+                            rzps);
 
                 b[(ix - 2) * incxr + (iy - 2) * incyr + (iz - 2)] +=
                     c200 * (rptr[ixs + iys + (iz - 2)] +
                             rptr[ixs + iys + (iz + 2)] +
                             rptr[ixmms + iys + iz] +
                             rptr[ixpps + iys + iz] +
-                            rptr[ixs + iymms + iz] + rptr[ixs + iypps + iz]);
+                            rptr[ixs + iymms + iz] + 
+                            rptr[ixs + iypps + iz]);
+
+                // Compute another set of forward edges here
+                rzpps = rptr[ixps + iys + (iz + 2)] +
+                        rptr[ixms + iys + (iz + 2)] +
+                        rptr[ixs + iyps + (iz + 2)] +
+                        rptr[ixs + iyms + (iz + 2)];
+
+                // Second
+                b[(ix - 2) * incxr + (iy - 2) * incyr + (iz - 1)] =
+                    c100 * (rptr[ixs + iys + (iz)] +
+                            rptr[ixs + iys + (iz + 2)] +
+                            rzps) + 
+                            c000 * rptr[ixs + iys + iz + 1];
+
+                b[(ix - 2) * incxr + (iy - 2) * incyr + (iz - 1)] +=
+                    c110 * (rptr[ixps + iyps + iz+1] +
+                            rptr[ixps + iyms + iz+1] +
+                            rptr[ixms + iyps + iz+1] +
+                            rptr[ixms + iyms + iz+1] +
+                            rzpps +
+                            rz);
+
+                b[(ix - 2) * incxr + (iy - 2) * incyr + (iz - 1)] +=
+                    c200 * (rptr[ixs + iys + (iz - 1)] +
+                            rptr[ixs + iys + (iz + 3)] +
+                            rptr[ixmms + iys + iz+1] +
+                            rptr[ixpps + iys + iz+1] +
+                            rptr[ixs + iymms + iz+1] + 
+                            rptr[ixs + iypps + iz+1]);
+
+                // Compute the forward set of edges here which becomes the trailing set at the top
+                rzms = rptr[ixms + iys + iz+3] +
+                       rptr[ixps + iys + iz+3] +
+                       rptr[ixs + iyms + iz+3] +
+                       rptr[ixs + iyps + iz+3];
+
+                // Third
+                b[(ix - 2) * incxr + (iy - 2) * incyr + (iz)] =
+                    c100 * (rptr[ixs + iys + (iz+1)] +
+                            rptr[ixs + iys + (iz + 3)] +
+                            rzpps) +
+                            c000 * rptr[ixs + iys + iz + 2];
+
+                b[(ix - 2) * incxr + (iy - 2) * incyr + (iz)] +=
+                    c110 * (rptr[ixps + iyps + iz+2] +
+                            rptr[ixps + iyms + iz+2] +
+                            rptr[ixms + iyps + iz+2] +
+                            rptr[ixms + iyms + iz+2] +
+                            rzms +
+                            rzps);
+
+                b[(ix - 2) * incxr + (iy - 2) * incyr + (iz)] +=
+                    c200 * (rptr[ixs + iys + (iz)] +
+                            rptr[ixs + iys + (iz + 4)] +
+                            rptr[ixmms + iys + iz+2] +
+                            rptr[ixpps + iys + iz+2] +
+                            rptr[ixs + iymms + iz+2] + 
+                            rptr[ixs + iypps + iz+2]);
+
+
+                // Compute the forward set of edges here which becomes the middle set at the top
+                rz = rptr[ixps + iys + (iz + 4)] +
+                     rptr[ixms + iys + (iz + 4)] +
+                     rptr[ixs + iyps + (iz + 4)] +
+                     rptr[ixs + iyms + (iz + 4)];
+
+                // Fourth
+                b[(ix - 2) * incxr + (iy - 2) * incyr + (iz+1)] =
+                    c100 * (rptr[ixs + iys + (iz+2)] +
+                            rptr[ixs + iys + (iz + 4)] +
+                            rzms) + 
+                            c000 * rptr[ixs + iys + iz + 3];
+
+                b[(ix - 2) * incxr + (iy - 2) * incyr + (iz+1)] +=
+                    c110 * (rptr[ixps + iyps + iz+3] +
+                            rptr[ixps + iyms + iz+3] +
+                            rptr[ixms + iyps + iz+3] +
+                            rptr[ixms + iyms + iz+3] +
+                            rzpps +
+                            rz);
+
+                b[(ix - 2) * incxr + (iy - 2) * incyr + (iz+1)] +=
+                    c200 * (rptr[ixs + iys + (iz+1)] +
+                            rptr[ixs + iys + (iz + 5)] +
+                            rptr[ixmms + iys + iz+3] +
+                            rptr[ixpps + iys + iz+3] +
+                            rptr[ixs + iymms + iz+3] + 
+                            rptr[ixs + iypps + iz+3]);
+
 
             }                   /* end for */
 
