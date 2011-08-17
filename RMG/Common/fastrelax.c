876b9e16776efde0c8ea529e604ae296efc2339c
Miroslav Hodak <mhodak@ncsu.edu>
2011-08-17 21:41:46 +0000

    RMG: Fastrelax now writes maximum, average and RMS displacement for each step so that we have better idea how much is the system moving.
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@1551 eb38b3b0-33ea-0310-94ad-d43fea0d214f

--- RMG/Common/fastrelax.c
+++ RMG/Common/fastrelax.c
@@ -39,9 +39,11 @@
 
 void fastrelax (void)
 {
-    int ion, fpt;
+    int ion, fpt, which = 0, count = 0;
     ION *iptr;
     REAL step, mass, magf, dotfv;
+    REAL max_move = 0.0, avg_move = 0.0, rms_move = 0.0;
+    REAL move_x, move_y, move_z, move_sq, move;
 
 
     fpt = ct.fpt[0];
@@ -102,37 +104,49 @@ void fastrelax (void)
         /* Move the ion */
         if (iptr->movable)
         {
+	    move_x = step * iptr->velocity[0];
+	    move_y = step * iptr->velocity[1];
+	    move_z = step * iptr->velocity[2];
 
-            iptr->crds[0] += step * iptr->velocity[0];
-
-            iptr->crds[1] += step * iptr->velocity[1];
-
-            iptr->crds[2] += step * iptr->velocity[2];
+	    /*Update coordinates*/
+            iptr->crds[0] += move_x;
+            iptr->crds[1] += move_y;
+            iptr->crds[2] += move_z;
 
             /* enforce periodic boundary conditions on the ions */
-
             to_crystal (iptr->xtal, iptr->crds);
-            if (iptr->xtal[0] > ONE)
-                iptr->xtal[0] -= ONE;
-            if (iptr->xtal[0] < ZERO)
-                iptr->xtal[0] += ONE;
-
-            if (iptr->xtal[1] > ONE)
-                iptr->xtal[1] -= ONE;
-            if (iptr->xtal[1] < ZERO)
-                iptr->xtal[1] += ONE;
-
-            if (iptr->xtal[2] > ONE)
-                iptr->xtal[2] -= ONE;
-            if (iptr->xtal[2] < ZERO)
-                iptr->xtal[2] += ONE;
-
             to_cartesian (iptr->xtal, iptr->crds);
 
+	    /*Find maximum, average and RMS displacement*/
+	    move_sq = move_x*move_x + move_y*move_y + move_z*move_z;
+	    move = sqrt (move_sq);
+
+	    if (move > max_move)
+	    {
+		max_move = move;
+		which = ion;
+	    }
+
+	    avg_move += move;
+	    count ++;
+
+	    rms_move += move_sq;
         }                       /* end if */
 
-
+	
     }                           /* end for */
+	
+    /*Write out displacement info*/
+    printf ("\n");
+    progress_tag ();
+    printf ("Max displacement: %8.5f a0  (ion %d)", max_move, which + 1);
+    printf ("\n");
+    progress_tag ();
+    printf ("Avg displacement: %8.5f a0", avg_move/count);
+    printf ("\n");
+    progress_tag ();
+    printf ("RMS displacement: %8.5f a0", sqrt(rms_move/count));
+
 
 }                               /* end rmg_fastrelax */
 
