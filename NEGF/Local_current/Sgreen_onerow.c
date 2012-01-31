417dd393347c8cf577a1954ddb2b9ac5aef3fcaa
Wenchang Lu <wlu2@ncsu.edu>
2012-01-31 18:42:04 +0000

    Add two functions to calculate local current, it is in progress.
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@1667 eb38b3b0-33ea-0310-94ad-d43fea0d214f

---
+++ NEGF/Local_current/Sgreen_onerow.c
@@ -0,0 +1,96 @@
+/************************** SVN Revision Information **************************
+ **    $Id$    **
+******************************************************************************/
+ 
+/*
+ *  Calculate the right-up block for transmission calculations.
+ */
+
+#include <float.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+#include "md.h"
+#include "pmo.h"
+
+
+void Sgreen_onerow (complex double *Htri, complex double * sigma,
+                     int *sigma_idx, complex double * Green_C, int nC,
+                     int iprobe)
+{
+/*   H00, S00: nC * nC real matrix
+ *   sigma:  nC * nC complex matrix
+ *   Green_C  nC * nC complex matrix , output green function
+ *   nC: number of states in conductor region
+ */
+
+    complex double *H_tri;
+
+    int *ipiv, idx, idx1;
+    int i, j, nprobe; 
+    REAL time1, time2;
+    int ni[MAX_BLOCKS], ntot, ndim;
+    int N, N1, N2;
+    REAL tem;
+    
+
+    N = ct.num_blocks;
+    for (i = 0; i < N; i++)
+    {
+        ni[i] = ct.block_dim[i];
+    }
+
+
+    ndim = 0;
+    for (i = 0; i < N; i++)
+    {
+        ndim += ni[i];
+    }
+
+    if (ndim != nC)
+    {
+        error_handler ("\n %d %d ndim  not equaol to nC ", ndim, nC);
+    }
+
+
+    ntot = pmo.ntot;
+    /* allocate matrix and initialization  */
+    my_malloc_init( H_tri, ntot, complex double );
+
+    
+ 	/* Construct: H = ES - H */
+    for (i = 0; i < ntot; i++)
+    {
+        H_tri[i] = Htri[i];
+    }
+    /* put the sigma for a probe in the corresponding block
+	   of the Green's matrices  */
+
+    for (nprobe = 0; nprobe < cei.num_probe; nprobe++)
+	{	
+        N1 = cei.probe_in_block[nprobe];
+        N2 = sigma_idx[nprobe];
+
+
+        for (i = 0; i < pmo.mxllda_cond[N1] * pmo.mxlocc_cond[N1]; i++)
+        {
+            H_tri[pmo.diag_begin[N1] + i] -= sigma[N2 + i];
+        }
+    }
+
+
+    time1 = my_crtc ();
+
+
+   matrix_inverse_anyprobe_p (H_tri, N, ni, iprobe, Green_C); 
+
+
+    time2 = my_crtc ();
+    rmg_timings (matrix_inverse_lr_TIME, (time2 - time1));
+
+
+    my_free( H_tri );
+
+}
