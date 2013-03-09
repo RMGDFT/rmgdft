eb1038fc8aadee9c48923cc2263487f17480dd8a
Emil Briggs <elbriggs@ncsu.edu>
2013-03-09 14:23:44 +0000

    Updated for fixed global grid size case.
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@1941 eb38b3b0-33ea-0310-94ad-d43fea0d214f

--- ON/Headers/md.h
+++ ON/Headers/md.h
@@ -63,6 +63,9 @@
 #include    "common_prototypes.h"
 #include    "typedefs.h"
 
+/* Some definitions needed for using compile time values for global grid sizes */
+#include "fixed_dims.h"
+
 int MXLLDA, MXLCOL;
 REAL *rho, *rho_old, *rhoc, *vh, *vnuc, *vxc, *rhocore, *vtot,
     *vtot_c;
