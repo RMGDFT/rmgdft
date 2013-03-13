e83f806c1fbd4a93646db162d5adc2a4693b2322
Wenchang Lu <wlu2@ncsu.edu>
2013-03-13 19:41:42 +0000

    modified the state_overlap to adapt the odd size of orbitals.
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@1954 eb38b3b0-33ea-0310-94ad-d43fea0d214f

--- ON/ON-NEGF-share/is_state_overlap.c
+++ ON/ON-NEGF-share/is_state_overlap.c
@@ -1,10 +1,10 @@
 /************************** SVN Revision Information **************************
  **    $Id$    **
-******************************************************************************/
- 
+ ******************************************************************************/
+
 /*
 
-is_state_overlap.c
+   is_state_overlap.c
 
 */
 
@@ -20,43 +20,47 @@ void is_state_overlap(STATE * states)
 {
 
     int state1, state2;
-    REAL r;
-    REAL r1, r2;
 
-    double hx4, ax, ay, az, rmin, x, y, z;
+    int ixcenter1, iycenter1, izcenter1;
+    int ixcenter2, iycenter2, izcenter2;
+    int ixhalf1, iyhalf1, izhalf1;
+    int ixhalf2, iyhalf2, izhalf2;
 
-
-    /* Get lattice vectors */
-    ax = NX_GRID * ct.hxgrid * ct.xside;
-    ay = NY_GRID * ct.hygrid * ct.yside;
-    az = NZ_GRID * ct.hzgrid * ct.zside;
-
-/*  add hx4 to account derivative */
-    hx4 = 4.0* ct.hxgrid * ct.xside;
-    
+    int xoverlap, yoverlap, zoverlap;
 
     for (state1 = 0; state1 < ct.num_states; state1++)
     {
+        ixcenter1 = (states[state1].ixmin + states[state1].ixmax)/2;
+        iycenter1 = (states[state1].iymin + states[state1].iymax)/2;
+        izcenter1 = (states[state1].izmin + states[state1].izmax)/2;
+        ixhalf1 = (states[state1].ixmax - states[state1].ixmin)/2;
+        iyhalf1 = (states[state1].iymax - states[state1].iymin)/2;
+        izhalf1 = (states[state1].izmax - states[state1].izmin)/2;
+
         for (state2 = state1; state2 < ct.num_states; state2++)
         {
-            x = fabs(states[state1].crds[0] - states[state2].crds[0]);
-            y = fabs(states[state1].crds[1] - states[state2].crds[1]);
-            z = fabs(states[state1].crds[2] - states[state2].crds[2]);
+            ixcenter2 = (states[state2].ixmin + states[state2].ixmax)/2;
+            iycenter2 = (states[state2].iymin + states[state2].iymax)/2;
+            izcenter2 = (states[state2].izmin + states[state2].izmax)/2;
+            ixhalf2 = (states[state2].ixmax - states[state2].ixmin)/2;
+            iyhalf2 = (states[state2].iymax - states[state2].iymin)/2;
+            izhalf2 = (states[state2].izmax - states[state2].izmin)/2;
 
-            r1 = states[state1].radius;
-            r2 = states[state2].radius;
+            xoverlap =abs(ixcenter1 - ixcenter2);
+            yoverlap =abs(iycenter1 - iycenter2);
+            zoverlap =abs(izcenter1 - izcenter2);
 
-            if( x > ax/2.0 ) x = ax -x;
-            if( y > ay/2.0 ) y = ay -y;
-            if( z > az/2.0 ) z = az -z;
 
-            if (x < (r1+r2+hx4) && y < (r1+r2+hx4) && z < (r1+r2+hx4))
+            if ((xoverlap <= ixhalf1 + ixhalf2 ) && 
+                    (yoverlap <= iyhalf1 + iyhalf2 ) && 
+                    (zoverlap <= izhalf1 + izhalf2 ))
                 state_overlap_or_not[state1 * ct.num_states + state2] = 1;
             else
                 state_overlap_or_not[state1 * ct.num_states + state2] = 0;	
 
             state_overlap_or_not[state1 + state2 * ct.num_states]
                 = state_overlap_or_not[state1 * ct.num_states + state2];
+
         }
     }
 
