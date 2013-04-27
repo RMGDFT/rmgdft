13132e414e01fe3a8e58f827cc87806b444a7067
Wenchang Lu <wlu2@ncsu.edu>
2013-04-27 15:55:15 +0000

    Compiling issue from last commit for gpu fixed.
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@1992 eb38b3b0-33ea-0310-94ad-d43fea0d214f

--- RMG/Common/reinit_ionic_pp.c
+++ RMG/Common/reinit_ionic_pp.c
@@ -57,7 +57,6 @@ void reinit_ionic_pp (STATE * states, REAL * vnuc, REAL * rhocore, REAL * rhoc)
     }
     // Transfer copy of weights to GPU
     cublasSetVector( pct.P0_BASIS * pct.num_tot_proj, sizeof( REAL ), pct.weight, 1, ct.gpu_weight, 1 );
-#endif
 
     if((ct.gpu_Bweight == NULL) || (gpu_weight_alloc < (pct.P0_BASIS * pct.num_tot_proj * sizeof(REAL)))) {
         if(ct.gpu_Bweight != NULL) {
