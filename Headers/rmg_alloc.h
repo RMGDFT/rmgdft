191ae03106bc9e44931cc87f5df71490f54d38c9
Emil Briggs <elbriggs@ncsu.edu>
2014-01-17 14:42:52 +0000

    Forgot these.
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@2081 eb38b3b0-33ea-0310-94ad-d43fea0d214f

--- Headers/rmg_alloc.h
+++ Headers/rmg_alloc.h
@@ -3,9 +3,11 @@
 #define RMG_ALLOC_H 1
 
 void rmg_malloc(void *ptr, int n, size_t size );
+void rmg_malloc_init(void *ptr, int n, size_t size, char *type );
 void rmg_free( void *ptr );
 void my_free( void *ptr );
 
+#define my_free rmg_free
 #define my_malloc( _ptr_, _nobj_, _type_ ) \
         rmg_malloc(_ptr_, (int) _nobj_, sizeof(_type_))
 
@@ -13,6 +15,6 @@ void my_free( void *ptr );
         rmg_calloc(_ptr_, (int) _nobj_, sizeof(_type_))
 
 #define my_malloc_init( _ptr_, _nobj_, _type_ ) \
-        rmg_malloc_init(_ptr_, (int) _nobj_, sizeof(_type_))
+        rmg_malloc_init(_ptr_, (int) _nobj_, sizeof(_type_), "_type_")
 
 #endif
--- Headers/rmgtypes.h
+++ Headers/rmgtypes.h
@@ -1,3 +1,9 @@
+#ifndef RMG_TYPES_H
+#define RMG_TYPES_H 1
+
+
 typedef float rmg_float_t;
 typedef double rmg_double_t;
 
+
+#endif
