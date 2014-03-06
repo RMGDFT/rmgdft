89e321e0cbef04800818e18d83e00c305ef7ecfd
Emil Briggs <elbriggs@ncsu.edu>
2014-03-06 19:23:19 +0000

    Cmake module to autodetect magma libs.
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@2186 eb38b3b0-33ea-0310-94ad-d43fea0d214f

---
+++ cmake/Modules/FindMAGMA.cmake
@@ -0,0 +1,26 @@
+# - Find MAGMA
+# Find the native MAGMA includes and library
+#
+#  MAGMA_INCLUDES    - where to find fftw.h
+#  MAGMA_LIBRARIES   - List of libraries when using MAGMA.
+#  MAGMA_FOUND       - True if MAGMA found.
+
+if (MAGMA_INCLUDES)
+  # Already in cache, be silent
+  set (MAGMA_FIND_QUIETLY TRUE)
+endif (MAGMA_INCLUDES)
+
+find_path (MAGMA_INCLUDES magma.h)
+if(NOT MAGMA_INCLUDES)
+    find_path (MAGMA_INCLUDES magma/magma.h)
+endif(NOT MAGMA_INCLUDES)
+
+find_library (MAGMA_LIBRARIES NAMES magma PATH_SUFFIXES magma)
+
+# handle the QUIETLY and REQUIRED arguments and set MAGMA_FOUND to TRUE if
+# all listed variables are TRUE
+include (FindPackageHandleStandardArgs)
+find_package_handle_standard_args (MAGMA DEFAULT_MSG MAGMA_LIBRARIES MAGMA_INCLUDES)
+
+mark_as_advanced (MAGMA_LIBRARIES MAGMA_INCLUDES)
+
