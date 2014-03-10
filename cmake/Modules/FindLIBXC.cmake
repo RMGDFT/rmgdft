5f428147be1b06c82dfdaff2455985fd9201c304
Wenchang Lu <wlu2@ncsu.edu>
2014-03-10 15:33:19 +0000

    add FindLIBXC.cmake and so we can deal with lib64 on linux and lib on cray
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@2195 eb38b3b0-33ea-0310-94ad-d43fea0d214f

---
+++ cmake/Modules/FindLIBXC.cmake
@@ -0,0 +1,22 @@
+# - Find Libxc
+#
+#  LIBXC_INCLUDES    - where to find fftw.h
+#  LIBXC_LIBRARIES   - List of libraries when using LIBXC.
+#  LIBXC_FOUND       - True if LIBXC found.
+
+if (LIBXC_INCLUDES)
+  # Already in cache, be silent
+  set (LIBXC_FIND_QUIETLY TRUE)
+endif (LIBXC_INCLUDES)
+
+find_path (LIBXC_INCLUDES xc.h)
+
+find_library (LIBXC_LIBRARIES NAMES xc)
+
+# handle the QUIETLY and REQUIRED arguments and set LIBXC_FOUND to TRUE if
+# all listed variables are TRUE
+include (FindPackageHandleStandardArgs)
+find_package_handle_standard_args (LIBXC DEFAULT_MSG LIBXC_LIBRARIES LIBXC_INCLUDES)
+
+mark_as_advanced (LIBXC_LIBRARIES LIBXC_INCLUDES)
+
