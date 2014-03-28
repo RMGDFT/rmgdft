3ada9cc41c8af42e597c29218bf6775d75646b7b
Wenchang Lu <wlu2@ncsu.edu>
2014-03-28 19:56:14 +0000

    split scalapack and blacs. some scalapack libraries already include blacs, blacsCinit  and no separate blacs and blacsCinit can be found
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@2244 eb38b3b0-33ea-0310-94ad-d43fea0d214f

--- cmake/Modules/FindSCALAPACK.cmake
+++ cmake/Modules/FindSCALAPACK.cmake
@@ -9,13 +9,11 @@ if (SCALAPACK_LIBRARIES)
 endif (SCALAPACK_LIBRARIES)
 
 find_library (SCALAPACK_LIBRARIES NAMES scalapack)
-find_library (BLACS_LIBRARIES NAMES mpiblacs)
-find_library (BLACSCINIT_LIBRARIES NAMES mpiblacsCinit)
 
 # handle the QUIETLY and REQUIRED arguments and set SCALAPACK_FOUND to TRUE if
 # all listed variables are TRUE
 include (FindPackageHandleStandardArgs)
-find_package_handle_standard_args (SCALAPACK DEFAULT_MSG SCALAPACK_LIBRARIES BLACS_LIBRARIES BLACSCINIT_LIBRARIES ) 
+find_package_handle_standard_args (SCALAPACK DEFAULT_MSG SCALAPACK_LIBRARIES)
 
-mark_as_advanced (SCALAPACK_LIBRARIES BLACS_LIBRARIES BLACSCINIT_LIBRARIES )
+mark_as_advanced (SCALAPACK_LIBRARIES)
 
