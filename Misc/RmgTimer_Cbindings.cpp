f339feea393b7d360666bf56be92468af48c831f
Emil Briggs <elbriggs@ncsu.edu>
2014-07-18 17:45:48 +0000

    Fix compiling errors on bluewaters.
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@2400 eb38b3b0-33ea-0310-94ad-d43fea0d214f

--- Misc/RmgTimer_Cbindings.cpp
+++ Misc/RmgTimer_Cbindings.cpp
@@ -1,14 +1,4 @@
-#include <sys/time.h>
-#include <stdio.h>
-#include <string>
-#include <iostream>
-#include <functional>
-#include <string>
-#include <iomanip>
-#include <algorithm>
 #include "RmgTimer.h"
-
-#include "main.h"
 #include "transition.h"
 
 void RmgPrintTimings(BaseGrid *G, const char *outfile, int steps);
