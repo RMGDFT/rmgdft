7908cf02e8de33023f2edc68ddd68750ae91f33e
Emil Briggs <elbriggs@ncsu.edu>
2014-08-23 17:31:44 +0000

    C interface wrapper to support old style subdiag_gamma call for routines that have not been updated yet.
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@2432 eb38b3b0-33ea-0310-94ad-d43fea0d214f

--- Misc/transition.cpp
+++ Misc/transition.cpp
@@ -8,10 +8,21 @@
 #include "rmgtypes.h"
 #include "rmgtypedefs.h"
 #include "typedefs.h"
+#include "Subdiag.h"
 
 BaseGrid *Rmg_G;
 TradeImages *Rmg_T;
 Lattice Rmg_L;
+
+// Pointer to Kpoint class array
+/* Hartree potential */
+extern double *vh;
+/* Nuclear local potential */
+extern double *vnuc;
+/* Exchange-correlation potential */
+extern double *vxc;
+// Pointer to Kpoint class array
+extern void **Kptr;
 
 extern "C"
 MPI_Comm transition_get_grid_comm(void)
@@ -22,3 +33,10 @@ extern "C" int transition_get_gridpe(void)
 {
     return pct.gridpe;
 }
+
+extern "C" void subdiag_gamma (STATE * states, rmg_double_t * vh, rmg_double_t * vnuc, rmg_double_t * vxc)
+{
+     Kpoint<double>* kptr = (Kpoint<double> *)Kptr[0]; 
+     Subdiag<double> (kptr, vh, vnuc, vxc, ct.subdiag_driver);
+}
+
