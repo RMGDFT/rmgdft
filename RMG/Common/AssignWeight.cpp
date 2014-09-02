f3288aeea2158df88ff95315e341629a9063613d
Emil Briggs <elbriggs@ncsu.edu>
2014-09-02 14:42:17 +0000

    Updated to apply phase factor before B operator.
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@2470 eb38b3b0-33ea-0310-94ad-d43fea0d214f

--- RMG/Common/AssignWeight.cpp
+++ RMG/Common/AssignWeight.cpp
@@ -5,6 +5,7 @@
 #include "typedefs.h"
 #include <complex>
 #include "Kpoint.h"
+#include "FiniteDiff.h"
 #include "common_prototypes.h"
 #include "common_prototypes1.h"
 #include "transition.h"
@@ -17,9 +18,13 @@ template <typename KpointType>
 void AssignWeight (Kpoint<KpointType> *kptr, SPECIES * sp, int ion, fftw_complex * beptr, double * rtptr, KpointType *Bweight, KpointType *Nlweight)
 {
 
+    Lattice *L = kptr->L;
+    TradeImages *T = kptr->T;
+
+
     int pbasis = kptr->pbasis;
     int nldim = sp->nldim;
-    KpointType ZERO_T(0.0);
+    KpointType ZERO_t(0.0);
 
     std::complex<double> *Nlweight_C = (std::complex<double> *)Nlweight;
     std::complex<double> *Bweight_C = (std::complex<double> *)Bweight;
@@ -28,22 +33,27 @@ void AssignWeight (Kpoint<KpointType> *kptr, SPECIES * sp, int ion, fftw_complex
     double *Bweight_R = (double *)Bweight;
 
     for(int idx = 0; idx < pbasis; idx++) rtptr[idx] = 0.0;
-    for(int idx = 0; idx < pbasis; idx++) Bweight[idx] = ZERO_T;
-    for(int idx = 0; idx < pbasis; idx++) Nlweight[idx] = ZERO_T;
+    for(int idx = 0; idx < pbasis; idx++) Bweight[idx] = ZERO_t;
+    for(int idx = 0; idx < pbasis; idx++) Nlweight[idx] = ZERO_t;
     if(pct.idxptrlen[ion] == 0) return;
 
     std::complex<double> *nbeptr = (std::complex<double> *)beptr;
 
 
-    double *tem_array = new double[nldim * nldim * nldim];
-    double *Btem_array = new double[nldim * nldim * nldim];
+    KpointType *tem_array = new KpointType[nldim * nldim * nldim];
+    KpointType *Btem_array = new KpointType[nldim * nldim * nldim];
+    std::complex<double> *tem_array_C = (std::complex<double> *)tem_array;
+    std::complex<double> *Btem_array_C = (std::complex<double> *)Btem_array;
+    double *Btem_array_R = (double *)Btem_array;
 
-    for(int ix = 0; ix < nldim * nldim * nldim; ix++) 
+    for(int ix = 0; ix < nldim * nldim * nldim; ix++) {
+        tem_array[ix] = ZERO_t;
+        Btem_array[ix] = ZERO_t;
+    }
+
+    for(int ix = 0; ix < nldim * nldim * nldim; ix++) {
         tem_array[ix] = std::real(nbeptr[ix]);
-
-    app_cir_beta_driver (tem_array, Btem_array, nldim, nldim, 
-          nldim, ct.kohn_sham_fd_order);
-
+    }
 
     double *pR = pct.phaseptr[ion];
     pR += 2 * kptr->kidx * pbasis;
@@ -53,6 +63,44 @@ void AssignWeight (Kpoint<KpointType> *kptr, SPECIES * sp, int ion, fftw_complex
     int *dvec = pct.idxflag[ion];
     int idx = 0;
     int docount = 0;
+
+
+    // Apply phase factor
+    for (int ix = 0; ix < sp->nldim; ix++)
+    {
+
+        for (int iy = 0; iy < sp->nldim; iy++)
+        {
+
+            for (int iz = 0; iz < sp->nldim; iz++)
+            {
+
+                if (dvec[idx])
+                {
+                    int idx1 = ix * sp->nldim * sp->nldim + iy * sp->nldim + iz;
+                    rtptr[pidx[docount]] = std::real(nbeptr[idx1]);
+                    if(!ct.is_gamma) {
+                        tem_array_C[idx1] = std::real(nbeptr[idx1]) * std::complex<double> (pR[pidx[docount]], -pI[pidx[docount]]);
+                    }
+                    docount++;
+                }
+
+                idx++;
+            }
+        }
+    }
+    if (docount != pct.idxptrlen[ion])
+    {
+        rmg_printf ("docount = %d != %d = pct.idxptrlen[ion = %d]\n", docount, pct.idxptrlen[ion], ion);
+        rmg_error_handler (__FILE__, __LINE__, "wrong numbers of projectors");
+    }
+
+
+    // Apply B operator then map weights back
+    CPP_app_cir_driver (L, T, tem_array, Btem_array, nldim, nldim, nldim, ct.kohn_sham_fd_order);
+
+    idx = 0;
+    docount = 0;
     for (int ix = 0; ix < sp->nldim; ix++)
     {
 
@@ -68,11 +116,11 @@ void AssignWeight (Kpoint<KpointType> *kptr, SPECIES * sp, int ion, fftw_complex
                     rtptr[pidx[docount]] = std::real(nbeptr[idx1]);
                     if(ct.is_gamma) {
                         Nlweight_R[pidx[docount]] = std::real(nbeptr[idx1]);
-                        Bweight_R[pidx[docount]] = Btem_array[idx1];
+                        Bweight_R[pidx[docount]] = Btem_array_R[idx1];
                     }
                     else {
                         Nlweight_C[pidx[docount]] = std::complex<double>(std::real(nbeptr[idx1]) * pR[pidx[docount]], -std::real(nbeptr[idx1]) * pI[pidx[docount]]);
-                        Bweight_C[pidx[docount]] = std::complex<double>(Btem_array[idx1]*pR[pidx[docount]], -Btem_array[idx1]*pI[pidx[docount]]);
+                        Bweight_C[pidx[docount]] = Btem_array_C[idx1];
                     }
                     docount++;
                 }
@@ -90,6 +138,8 @@ void AssignWeight (Kpoint<KpointType> *kptr, SPECIES * sp, int ion, fftw_complex
 
     delete [] Btem_array;
     delete [] tem_array;
+
+
 
     
 }
