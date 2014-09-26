df12ee430cde225bf53f443674f46ef791292634
Emil Briggs <elbriggs@ncsu.edu>
2014-09-26 18:16:03 +0000

    Additional UPF updates.
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@2565 eb38b3b0-33ea-0310-94ad-d43fea0d214f

--- Input/Upf.cpp
+++ Input/Upf.cpp
@@ -15,6 +15,7 @@
 #include "params.h"
 #include "typedefs.h"
 #include "rmg_error.h"
+#include "MapElements.h"
 
 
 // Converts a string containg a PP_MESH, PP_RAB, PP_LOCAL, etc into a double array and
@@ -52,105 +53,124 @@ void LoadUpf(char *file, SPECIES *sp)
    ptree upf_tree;
    std::string PP_INFO;
    std::string upf_file = file;
+
  
-   read_xml(upf_file, upf_tree);
-   PP_INFO = upf_tree.get<std::string>("UPF.PP_INFO"); 
-   
-   std::cout << PP_INFO << std::endl; 
+   try 
+   {
+       read_xml(upf_file, upf_tree);
+       PP_INFO = upf_tree.get<std::string>("UPF.PP_INFO"); 
+       
+       std::cout << PP_INFO << std::endl; 
 
-   // Get the type of pseudopotential
-   std::string pp_type = upf_tree.get<std::string>("UPF.PP_HEADER.<xmlattr>.pseudo_type");
-   boost::to_upper(pp_type);
-   if(!pp_type.compare(0, 2, "NC")) {
-       ct.norm_conserving_pp = true;
-   }
-   else if(!pp_type.compare(0, 2, "US")) {
-       ct.norm_conserving_pp = false;
-   }
-   else {
-       rmg_error_handler(__FILE__,__LINE__,"RMG only supports norm conserving and ultrasoft pseudpotentials.\n");
-   }
+       // Atomic symbol, mass, number and zvalence
+       std::string atomic_symbol = upf_tree.get<std::string>("UPF.PP_HEADER.<xmlattr>.element");
+       sp->atomic_symbol = new char[4];
+       std::strncpy(sp->atomic_symbol, atomic_symbol.c_str(), 3);
+       sp->atomic_mass = GetAtomicMass(sp->atomic_symbol);
+       sp->atomic_number = GetAtomicNumber(sp->atomic_symbol);
+       sp->zvalence = upf_tree.get<int>("UPF.PP_HEADER.<xmlattr>.z_valence");
+       
 
-
-   // Kind of redundant information in the format
-   std::string s_is_ultrasoft = upf_tree.get<std::string>("UPF.PP_HEADER.<xmlattr>.is_ultrasoft"); 
-   boost::to_upper(s_is_ultrasoft);
-   if(!s_is_ultrasoft.compare(0,1,"F")) ct.norm_conserving_pp = true;
-   if(!s_is_ultrasoft.compare(0,5,"FALSE")) ct.norm_conserving_pp = true;
-   if(!s_is_ultrasoft.compare(0,1,"T")) ct.norm_conserving_pp = false;
-   if(!s_is_ultrasoft.compare(0,4,"TRUE")) ct.norm_conserving_pp = false;
-
-   // Core correction
-   std::string s_core_correction = upf_tree.get<std::string>("UPF.PP_HEADER.<xmlattr>.core_correction");
-   if(!s_core_correction.compare(0,1,"F")) sp->nlccflag = false;
-   if(!s_core_correction.compare(0,5,"FALSE")) sp->nlccflag = false;
-   if(!s_core_correction.compare(0,1,"T")) sp->nlccflag = true;
-   if(!s_core_correction.compare(0,4,"TRUE")) sp->nlccflag = true;
-
-   // Attributes of the mesh
-   double PP_MESH_dx = upf_tree.get<double>("UPF.PP_MESH.<xmlattr>.dx");
-   std::cout << "PP_MESH.dx    =  " << PP_MESH_dx << std::endl; 
-
-   sp->rg_points = upf_tree.get<double>("UPF.PP_MESH.<xmlattr>.mesh");
-   std::cout << "PP_MESH.mesh  =  " << sp->rg_points << std::endl; 
- 
-   double PP_MESH_xmin = upf_tree.get<double>("UPF.PP_MESH.<xmlattr>.xmin");
-   std::cout << "PP_MESH.xmin  =  " << PP_MESH_xmin << std::endl; 
-
-   double PP_MESH_rmax = upf_tree.get<double>("UPF.PP_MESH.<xmlattr>.rmax");
-   std::cout << "PP_MESH.rmax  =  " << PP_MESH_rmax << std::endl; 
-
-   double PP_MESH_zmesh = upf_tree.get<double>("UPF.PP_MESH.<xmlattr>.zmesh");
-   std::cout << "PP_MESH.zmesh  =  " << PP_MESH_zmesh << std::endl; 
-
-
-   // Read in the radial mesh and convert it into a C style array
-   std::string PP_R = upf_tree.get<std::string>("UPF.PP_MESH.PP_R");
-   sp->r = UPF_str_to_double_array(PP_R, sp->rg_points);
-
-   // Read in rab and convert it into a C style array
-   std::string PP_RAB = upf_tree.get<std::string>("UPF.PP_MESH.PP_RAB");
-   sp->rab = UPF_str_to_double_array(PP_RAB, sp->rg_points);
-
-   // Local potential
-   std::string PP_LOCAL = upf_tree.get<std::string>("UPF.PP_LOCAL");
-   sp->vloc0 = UPF_str_to_double_array(PP_LOCAL, sp->rg_points);
-
-   // Atomic charge density
-   std::string PP_RHOATOM = upf_tree.get<std::string>("UPF.PP_RHOATOM");
-   sp->atomic_rho = UPF_str_to_double_array(PP_RHOATOM, sp->rg_points);
-
-   // Number of atomic orbitals
-   int number_of_wfc = upf_tree.get<double>("UPF.PP_HEADER.<xmlattr>.number_of_wfc");
-   if(number_of_wfc > 0) {
-
-       for(int iwf = 0;iwf < number_of_wfc;iwf++) {
-           // Ugh. UPF format has embedded .s so use / as a separator
-           typedef ptree::path_type path;
-           std::string chi = "UPF/PP_PSWFC/PP_CHI." + boost::lexical_cast<std::string>(iwf + 1);
-           std::string PP_CHI = upf_tree.get<std::string>(path(chi, '/'));
-           sp->atomic_wave[iwf] = UPF_str_to_double_array(PP_CHI, sp->rg_points);
-           
+       // Get the type of pseudopotential
+       std::string pp_type = upf_tree.get<std::string>("UPF.PP_HEADER.<xmlattr>.pseudo_type");
+       boost::to_upper(pp_type);
+       if(!pp_type.compare(0, 2, "NC")) {
+           ct.norm_conserving_pp = true;
+       }
+       else if(!pp_type.compare(0, 2, "US")) {
+           ct.norm_conserving_pp = false;
+       }
+       else {
+           rmg_error_handler(__FILE__,__LINE__,"RMG only supports norm conserving and ultrasoft pseudpotentials.\n");
        }
 
-   }
 
-   // Number of projectors
-   sp->nbeta = upf_tree.get<double>("UPF.PP_HEADER.<xmlattr>.number_of_proj");
-   if(sp->nbeta > 0) {
+       // Kind of redundant information in the format
+       std::string s_is_ultrasoft = upf_tree.get<std::string>("UPF.PP_HEADER.<xmlattr>.is_ultrasoft"); 
+       boost::to_upper(s_is_ultrasoft);
+       if(!s_is_ultrasoft.compare(0,1,"F")) ct.norm_conserving_pp = true;
+       if(!s_is_ultrasoft.compare(0,5,"FALSE")) ct.norm_conserving_pp = true;
+       if(!s_is_ultrasoft.compare(0,1,"T")) ct.norm_conserving_pp = false;
+       if(!s_is_ultrasoft.compare(0,4,"TRUE")) ct.norm_conserving_pp = false;
 
-       for(int ip = 0;ip < sp->nbeta;ip++) {
-           // Ugh. UPF format has embedded .s so use / as a separator
-           typedef ptree::path_type path;
-           std::string betapath = "UPF/PP_NONLOCAL/PP_BETA." + boost::lexical_cast<std::string>(ip + 1);
-           std::string PP_BETA = upf_tree.get<std::string>(path(betapath, '/'));
-           sp->beta[ip] = UPF_str_to_double_array(PP_BETA, sp->rg_points);
-            
+       // Core correction
+       std::string s_core_correction = upf_tree.get<std::string>("UPF.PP_HEADER.<xmlattr>.core_correction");
+       if(!s_core_correction.compare(0,1,"F")) sp->nlccflag = false;
+       if(!s_core_correction.compare(0,5,"FALSE")) sp->nlccflag = false;
+       if(!s_core_correction.compare(0,1,"T")) sp->nlccflag = true;
+       if(!s_core_correction.compare(0,4,"TRUE")) sp->nlccflag = true;
+
+       // Attributes of the mesh
+       double PP_MESH_dx = upf_tree.get<double>("UPF.PP_MESH.<xmlattr>.dx");
+       std::cout << "PP_MESH.dx    =  " << PP_MESH_dx << std::endl; 
+
+       sp->rg_points = upf_tree.get<double>("UPF.PP_MESH.<xmlattr>.mesh");
+       std::cout << "PP_MESH.mesh  =  " << sp->rg_points << std::endl; 
+     
+       double PP_MESH_xmin = upf_tree.get<double>("UPF.PP_MESH.<xmlattr>.xmin");
+       std::cout << "PP_MESH.xmin  =  " << PP_MESH_xmin << std::endl; 
+
+       double PP_MESH_rmax = upf_tree.get<double>("UPF.PP_MESH.<xmlattr>.rmax");
+       std::cout << "PP_MESH.rmax  =  " << PP_MESH_rmax << std::endl; 
+
+       double PP_MESH_zmesh = upf_tree.get<double>("UPF.PP_MESH.<xmlattr>.zmesh");
+       std::cout << "PP_MESH.zmesh  =  " << PP_MESH_zmesh << std::endl; 
+
+
+       // Read in the radial mesh and convert it into a C style array
+       std::string PP_R = upf_tree.get<std::string>("UPF.PP_MESH.PP_R");
+       sp->r = UPF_str_to_double_array(PP_R, sp->rg_points);
+
+       // Read in rab and convert it into a C style array
+       std::string PP_RAB = upf_tree.get<std::string>("UPF.PP_MESH.PP_RAB");
+       sp->rab = UPF_str_to_double_array(PP_RAB, sp->rg_points);
+
+       // Local potential
+       std::string PP_LOCAL = upf_tree.get<std::string>("UPF.PP_LOCAL");
+       sp->vloc0 = UPF_str_to_double_array(PP_LOCAL, sp->rg_points);
+
+       // Atomic charge density
+       std::string PP_RHOATOM = upf_tree.get<std::string>("UPF.PP_RHOATOM");
+       sp->atomic_rho = UPF_str_to_double_array(PP_RHOATOM, sp->rg_points);
+
+       // Number of atomic orbitals
+       int number_of_wfc = upf_tree.get<double>("UPF.PP_HEADER.<xmlattr>.number_of_wfc");
+       if(number_of_wfc > 0) {
+
+           for(int iwf = 0;iwf < number_of_wfc;iwf++) {
+               // Ugh. UPF format has embedded .s so use / as a separator
+               typedef ptree::path_type path;
+               std::string chi = "UPF/PP_PSWFC/PP_CHI." + boost::lexical_cast<std::string>(iwf + 1);
+               std::string PP_CHI = upf_tree.get<std::string>(path(chi, '/'));
+               sp->atomic_wave[iwf] = UPF_str_to_double_array(PP_CHI, sp->rg_points);
+               
+           }
+
        }
 
-   }
+       // Number of projectors
+       sp->nbeta = upf_tree.get<double>("UPF.PP_HEADER.<xmlattr>.number_of_proj");
+       if(sp->nbeta > 0) {
 
+           for(int ip = 0;ip < sp->nbeta;ip++) {
+               // Ugh. UPF format has embedded .s so use / as a separator
+               typedef ptree::path_type path;
+               std::string betapath = "UPF/PP_NONLOCAL/PP_BETA." + boost::lexical_cast<std::string>(ip + 1);
+               std::string PP_BETA = upf_tree.get<std::string>(path(betapath, '/'));
+               sp->beta[ip] = UPF_str_to_double_array(PP_BETA, sp->rg_points);
+                
+           }
 
+       }
+
+    }
+
+    catch(std::exception& e)
+    {
+        std::cout << e.what() << '\n';
+        rmg_error_handler(__FILE__,__LINE__,"Problem reading pseudopotential. Terminating.\n");
+        
+    }
    
 }
 
