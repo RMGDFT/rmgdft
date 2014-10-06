829b26f0296816569558643208b97ccdf38342a6
Emil Briggs <elbriggs@ncsu.edu>
2014-10-06 16:01:09 +0000

    Fix compile error on BW.
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@2620 eb38b3b0-33ea-0310-94ad-d43fea0d214f

--- Headers/MapElements.h
+++ Headers/MapElements.h
@@ -1,5 +1,6 @@
 #ifndef RMG_MapElements_H
 #define RMG_MapElements_H 1
+#include <string>
 
 extern "C" int GetAtomicNumber(std::string symbol);
 extern "C" const char * GetAtomicSymbol(int number);
