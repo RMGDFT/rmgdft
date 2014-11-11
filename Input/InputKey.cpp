d9f9b042396dc7dc12d445ee553f1e461ab2cf5d
Emil Briggs <elbriggs@ncsu.edu>
2014-11-11 22:02:11 +0000

    Some more formatting fixes.
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@2792 eb38b3b0-33ea-0310-94ad-d43fea0d214f

--- Input/InputKey.cpp
+++ Input/InputKey.cpp
@@ -169,21 +169,18 @@ std::string InputKey::Print(void) {
     }
 
     if(this->KeyType == typeid(RmgInput::ReadVector<int>).hash_code()) {
-        std::string rstr("\"");
+        std::string rstr;
         for(size_t i = 0;i < this->count;i++)
-            rstr = rstr + boost::lexical_cast<std::string>(this->Vint.vals.at(i)) + "  ";
+            rstr = rstr + boost::lexical_cast<std::string>(this->Vint.vals.at(i));
 
-        rstr = rstr + "\"";
         return rstr;
     }
 
     if(this->KeyType == typeid(RmgInput::ReadVector<double>).hash_code()) {
-        std::string rstr("\"");
+        std::string rstr;
         for(size_t i = 0;i < this->count;i++)
-            rstr = rstr + boost::lexical_cast<std::string>(this->Vdouble.vals.at(i)) + "  ";
+            rstr = rstr + boost::lexical_cast<std::string>(this->Vdouble.vals.at(i));
 
-        rstr = rstr + "\"";
-        return rstr;
     }
 
     return "Not done yet";
