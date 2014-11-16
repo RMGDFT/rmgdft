e8bdcba21cdb4f31a057bf480fa9abb0a5bee159
Emil Briggs <elbriggs@ncsu.edu>
2014-11-16 12:59:06 +0000

    More build fixups.
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@2828 eb38b3b0-33ea-0310-94ad-d43fea0d214f

---
+++ cmake/Modules/FindPLPLOT.cmake
@@ -0,0 +1,28 @@
+# Find PLplot header and library.
+
+# This module defines the following uncached variables:
+#  PLPLOT_FOUND, if false, do not try to use PLplot.
+#  PLplot_INCLUDE_DIR, where to find plplot.h.
+#  PLplot_LIBRARIES, the libraries to link against to use PLplot
+#  PLplot_LIBRARY_DIR, the directory where the PLplot library is found.
+
+if (PLplot_INCLUDE_DIR)
+  # Already in cache, be silent
+  set (PLplot_FIND_QUIETLY TRUE)
+endif (PLplot_INCLUDE_DIR)
+
+find_path (PLplot_INCLUDE_DIR plplot.h)
+if(NOT PLplot_INCLUDE_DIR)
+    find_path (PLplot_INCLUDE_DIR plplot/plplot.h)
+endif(NOT PLplot_INCLUDE_DIR)
+
+find_library (PLplot_LIBRARY NAMES plplotd)
+find_library (PLplot_cxx_LIBRARY NAMES plplotcxxd)
+
+# handle the QUIETLY and REQUIRED arguments and set PLPLOT_FOUND to TRUE if
+# all listed variables are TRUE
+include (FindPackageHandleStandardArgs)
+find_package_handle_standard_args (PLPLOT DEFAULT_MSG PLplot_LIBRARY PLplot_cxx_LIBRARY PLplot_INCLUDE_DIR)
+
+mark_as_advanced (PLplot_LIBRARY PLplot_cxx_LIBRARY PLplot_INCLUDE_DIR)
+
