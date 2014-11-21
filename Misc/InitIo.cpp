e08ceedda872476d967623117a5a27ef5ddd2fe7
Emil Briggs <elbriggs@ncsu.edu>
2014-11-21 20:15:15 +0000

    Removed some unused code. Added new copyright header.
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@2876 eb38b3b0-33ea-0310-94ad-d43fea0d214f

--- Misc/InitIo.cpp
+++ Misc/InitIo.cpp
@@ -1,6 +1,25 @@
-/************************** SVN Revision Information **************************
- **    $Id: init_IO.c 2628 2014-10-07 02:16:21Z ebriggs $    **
- ******************************************************************************/
+/*
+ *
+ * Copyright 2014 The RMG Project Developers. See the COPYRIGHT file 
+ * at the top-level directory of this distribution or in the current
+ * directory.
+ * 
+ * This file is part of RMG. 
+ * RMG is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * RMG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+*/
+
 
 /***** RMG: Common/init_IO.c *****
  * NAME
--- Misc/cgen_prolong.c
+++ Misc/cgen_prolong.c
@@ -1,20 +1,31 @@
-/************************** SVN Revision Information **************************
- **    $Id$    **
-******************************************************************************/
+/*
+ *
+ * Copyright 2014 The RMG Project Developers. See the COPYRIGHT file 
+ * at the top-level directory of this distribution or in the current
+ * directory.
+ * 
+ * This file is part of RMG. 
+ * RMG is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * RMG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+*/
 
 /****f* QMD-MGDFT/mg_restrict_6.c *****
  * NAME
  *   Ab initio real space code with multigrid acceleration
  *   Quantum molecular dynamics package.
  *   Version: 2.1.5
- * COPYRIGHT
- *   Copyright (C) 1995  Emil Briggs
- *   Copyright (C) 1998  Emil Briggs, Charles Brabec, Mark Wensell, 
- *                       Dan Sullivan, Chris Rapcewicz, Jerzy Bernholc
- *   Copyright (C) 2001  Emil Briggs, Wenchang Lu,
- *                       Marco Buongiorno Nardelli,Charles Brabec, 
- *                       Mark Wensell,Dan Sullivan, Chris Rapcewicz,
- *                       Jerzy Bernholc
+
  * FUNCTION
  *   void cgen_prolong(rmg_double_t  coef[], rmg_double_t fraction, int order)
  * INPUTS
