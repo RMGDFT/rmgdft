77ddbddf9b96f14fc67e9171c94e22a068ac5bdd
Emil Briggs <elbriggs@ncsu.edu>
2014-12-04 21:58:24 +0000

    Disable OutputBandPlot for gamma point calculations.
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@2900 eb38b3b0-33ea-0310-94ad-d43fea0d214f

--- RMG/Common/Main.cpp
+++ RMG/Common/Main.cpp
@@ -346,7 +346,9 @@ template <typename OrbitalType> void run (Kpoint<OrbitalType> **Kptr)
 
     }
 
-    OutputBandPlot(Kptr);
+    if(!ct.is_gamma)
+        OutputBandPlot(Kptr);
+
     Output_rho_xsf(rho, pct.grid_comm);
 
 }                               /* end run */
