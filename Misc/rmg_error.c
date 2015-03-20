7375992f0942f651eaeff06e995a0cfc9185aa0a
Emil Briggs <elbriggs@ncsu.edu>
2015-03-20 19:51:22 +0000

    More portability updates.
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@2980 eb38b3b0-33ea-0310-94ad-d43fea0d214f

--- Misc/rmg_error.c
+++ Misc/rmg_error.c
@@ -1,4 +1,5 @@
 
+#include "portability.h"
 #include "stdio.h"
 #include "mpi.h"
 #include "rmg_error.h"
@@ -11,6 +12,10 @@ void rmg_error_handler(char *message)
 {
     printf("%s\n", message);
     fflush (NULL);
+#if (defined(_WIN32) || defined(_WIN64))
+    Sleep(2000);
+#else
     sleep (2);
+#endif
     MPI_Abort( MPI_COMM_WORLD, 0 );
 }
