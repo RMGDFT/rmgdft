0e9b9ddeeedc761941cc48e29b49192788fd8b5b
Emil Briggs <elbriggs@ncsu.edu>
2015-03-21 01:13:40 +0000

    Works for now. Think this should be rewritten in C++ using boost::filesystem so that it will be transparently portable.
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@2983 eb38b3b0-33ea-0310-94ad-d43fea0d214f

--- Misc/filename_increment.c
+++ Misc/filename_increment.c
@@ -1,14 +1,18 @@
 /************************** SVN Revision Information **************************
  **    $Id$    **
  ******************************************************************************/
+#include "portability.h"
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <libgen.h>
+#if !(defined(_WIN32) || defined(_WIN64))
+    #include <libgen.h>
+#else
+    #define S_ISDIR(ST_MODE) (((ST_MODE) & _S_IFMT) == _S_IFDIR)
+#endif
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
 
 
 #include "main.h"
@@ -18,38 +22,55 @@ int filename_increment(char *filename)
 {
 
     int lognum = 0, status;
+    int pathlength;
+#if (defined(_WIN32) || defined(_WIN64))
+    char workdir[_MAX_PATH], basename[_MAX_PATH], dirname[_MAX_DIR];
+    _splitpath(filename, NULL, dirname, NULL, NULL);
+    pathlength = _MAX_PATH;
+#else
     char workdir[MAX_PATH], basename[MAX_PATH];
+    pathlength = MAX_PATH;
+#endif
     struct stat buffer;
 
 
-    strcpy(basename, filename);
+    strncpy(basename, filename, sizeof(basename));
 
 
+#if (defined(_WIN32) || defined(_WIN64))
+    if (status = stat ( basename, &buffer ) == 0)
+    {
+#else
     if (status = stat ( dirname(basename), &buffer ) == 0)
     {
+#endif
         if ( !S_ISDIR( buffer.st_mode))
             error_handler 
                 ("Found %s, that is not a directory ", basename);
     }
     else
     {
+#if (defined(_WIN32) || defined(_WIN64))
+        if (_mkdir (basename))
+#else
         if ((status = mkdir (basename, S_IRWXU)) == -1)
+#endif
             error_handler
                 ("Unable to create directory \"%s\", mkdir returned %d, check file system for permissions/diskfull.",
                  basename, status);
     }
 
 
-
-    snprintf (basename, MAX_PATH, "%s.%02d", filename, lognum );
+    snprintf (basename, pathlength, "%s.%02d", filename, lognum );
     while ((status = stat (basename, &buffer)) != -1)
     {
         if (++lognum > 99)
             error_handler
                 ("You have over 100 logfiles, you need to think of a better job scenario!\n");
-        snprintf (basename, MAX_PATH, "%s.%02d", filename, lognum );
+        snprintf (basename, pathlength, "%s.%02d", filename, lognum );
     }
 
     /* open and save logfile handle, printf is stdout before here */
     return lognum;
+
 }
