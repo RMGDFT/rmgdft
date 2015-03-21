e0166e4fdccb04936a11b81fc06236e3dd1d364b
Emil Briggs <elbriggs@ncsu.edu>
2015-03-21 13:17:49 +0000

    Forgot this one.
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@2987 eb38b3b0-33ea-0310-94ad-d43fea0d214f

---
+++ Misc/FilenameIncrement.cpp
@@ -0,0 +1,55 @@
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/path.hpp>
+#include <boost/filesystem/operations.hpp>
+#include <string>
+#include <boost/algorithm/string.hpp>
+#include "transition.h"
+#include "RmgException.h"
+
+
+// Used to increment the logfile number
+
+int FilenameIncrement(char *pathname)
+{
+
+    int lognum = 0;
+
+    boost::filesystem::path current_path(pathname);
+    std::string dirname  = current_path.parent_path().string(); 
+    std::string basename = boost::filesystem::basename(pathname);
+
+    // Does parent path exist?
+    if( boost::filesystem::exists(dirname)) {
+
+        // yes so check if it's a file 
+        if(!boost::filesystem::is_directory(dirname)) {
+            throw RmgFatalException() << "Found " << dirname << "  that is not a directory in " __FILE__ << " at line " << __LINE__ << ".\n";
+        }
+
+    }
+    else {
+
+        // no so need to make it
+        if(!boost::filesystem::create_directory(dirname)) {
+            throw RmgFatalException() << "Unable to create logfile directory " << dirname << " in " << __FILE__ << " at line " << __LINE__ << ".\n";
+        }
+
+    }
+
+    
+    char lognum_str[4]; 
+    std::snprintf(lognum_str, 3, "%02d", lognum);
+    std::string nextname = std::string(pathname) + "." + lognum_str;
+    while (boost::filesystem::exists(nextname))
+    {
+        if (++lognum > 99)
+            throw RmgFatalException() << "You have over 100 logfiles, you need to think of a better job scenario!" << "\n";
+        nextname.erase();
+        std::snprintf(lognum_str, 3, "%02d", lognum);
+        nextname = std::string(pathname) + "." + lognum_str;
+    }
+
+    // return lognum
+    return lognum;
+
+}
