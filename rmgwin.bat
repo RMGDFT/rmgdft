2cf315b3f2723e3d3c9bacff172f00107f5760d3
Emil Briggs <elbriggs@ncsu.edu>
2015-06-04 18:58:36 +0000

    Windows execution environment.
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@3037 eb38b3b0-33ea-0310-94ad-d43fea0d214f

--- rmgwin.bat
+++ rmgwin.bat
@@ -1,16 +1,24 @@
 @ECHO OFF
-cd %HOMEPATH%
 doskey mpirun="C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" $1 $2 $3 $4
 set RMGBINARY="C:\Program Files\rmg\bin\rmg.exe"
 cls
+mode con: cols=120 lines=40
 @ECHO:
-@ECHO RMG command line window. RMG requires Microsoft MPI in order to run. If MS-MPI
+@ECHO RMG v1.2 command line window. RMG requires Microsoft MPI v5 in order to run. If MS-MPI
 @ECHO is installed in it's standard location you can run the sample programs using the
-@ECHO the following command.
+@ECHO following command.
 @ECHO:
-@ECHO        mpirun -np 4 %RMGBINARY% input_file
+@ECHO        mpirun -np 4 %%RMGBINARY%% input_file
 @ECHO:
 @ECHO where the number of MPI processes to use (-np 4) may be adjusted depending on your
-@ECHO hardware platform. The input_file should be located in your current directory or
-@ECHO a subdirectory that you have write permissions for.
+@ECHO hardware platform. The input_file should be located in your current directory or a
+@ECHO subdirectory of the current directory and you must have write permissions for it.
+@ECHO:
+@ECHO Example input files may be obtained from 
+@ECHO:
+@ECHO     http://sourceforge.net/projects/rmgdft/files/Releases/
+@ECHO:
+@ECHO or from the Examples subdirectory of your RMG installation.
+@ECHO
+cmd.exe
 exit /B 0
