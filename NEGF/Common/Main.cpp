ffc50d127a2863822be974d0ee8176ac64f4cfe1
Wenchang Lu <wlu2@ncsu.edu>
2015-08-03 19:56:22 +0000

    add kbpsi_comm_res and other in NEGF Main.cpp
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@3132 eb38b3b0-33ea-0310-94ad-d43fea0d214f

--- NEGF/Common/Main.cpp
+++ NEGF/Common/Main.cpp
@@ -102,6 +102,8 @@ COMPASS potentialCompass, chargeDensityCompass;
 double *projectors, *projectors_x, *projectors_y, *projectors_z;
 int *num_nonlocal_ion;
 double *kbpsi, *kbpsi_comm, *partial_kbpsi_x, *partial_kbpsi_y, *partial_kbpsi_z;
+double *kbpsi_res;
+int *kbpsi_comm_send, *kbpsi_comm_recv,  kbpsi_num_loop;
 char *state_overlap_or_not;
 int *send_to, *recv_from, num_sendrecv_loop;
 int *send_to1, *recv_from1, num_sendrecv_loop1;
