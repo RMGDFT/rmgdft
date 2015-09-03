7f12c3126c5c10144a72815ab6f575cee26b09e0
Wenchang Lu <wlu2@ncsu.edu>
2015-09-03 02:03:59 +0000

    forget to remove Kbpsi update from GetHS.cpp
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@3200 eb38b3b0-33ea-0310-94ad-d43fea0d214f

--- ON/ON-NEGF-share/GetHS.cpp
+++ ON/ON-NEGF-share/GetHS.cpp
@@ -100,35 +100,6 @@ void GetHS(STATE * states, STATE * states1, double *vtot_c, double *Hij_00, doub
     delete(RT1);
 
 
-
-    RmgTimer *RT2 = new RmgTimer("4-get_HS: kbpsi");
-    get_all_kbpsi(states, states, ion_orbit_overlap_region_nl, projectors, kbpsi);
-    delete(RT2);
-
-    for(ion = 0; ion < pct.n_ion_center; ion++)
-    {
-        Kbpsi_str.kbpsi_ion[ion].clear();
-        num_orbital_thision = Kbpsi_str.num_orbital_thision[ion];
-        num_proj = pct.prj_per_ion[pct.ionidx[ion]];
-        Kbpsi_str.orbital_index[ion].resize(num_orbital_thision);
-
-
-        // uopdate values from this process
-        for(idx = 0; idx < num_orbital_thision; idx++)
-        {
-            st1 =Kbpsi_str.orbital_index[ion][idx];
-            iip1 = (st1-ct.state_begin) * pct.n_ion_center * ct.max_nl + ion * ct.max_nl;
-            for(ip = 0; ip < num_proj; ip++)
-                Kbpsi_str.kbpsi_ion[ion].emplace_back(kbpsi[iip1 + ip]);
-        }
-
-
-    }
-
-    RmgTimer *RT2a = new RmgTimer("4-get_HS: kbpsi_comm");
-    KbpsiComm();
-    delete(RT2a);
-
     RmgTimer *RT3 = new RmgTimer("4-get_HS: Hvnlij");
 //    for (st1 = 0; st1 < (ct.state_end-ct.state_begin) * ct.num_states; st1++)
 //    {
