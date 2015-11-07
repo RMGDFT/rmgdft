b9f4ffa2d7b002426c0f010c19b9596403a88d2c
Emil Briggs <elbriggs@ncsu.edu>
2015-11-07 15:09:57 +0000

    More Van der Waals updates.
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@3253 eb38b3b0-33ea-0310-94ad-d43fea0d214f

--- Headers/vdW.h
+++ Headers/vdW.h
@@ -2,8 +2,79 @@
 #define RMG_vdW_H 1
 
 #include "BaseGrid.h"
+#include "Lattice.h"
+#include "TradeImages.h"
+#include "FiniteDiff.h"
 
-void xc_vdW_DF (BaseGrid &G, Lattice &L, TradeImages &T, int type, double *rho_valence, double *rho_core, double &etxc, double &vtxc, double *v);
+class Vdw {
+
+private:
+
+    // BaseGrid class
+    BaseGrid *G;
+
+    // TradeImages object to use
+    TradeImages *T;
+
+    // Lattice object
+    Lattice *L;
+
+    // Real space basis on this node and grid parameters
+    int pbasis;
+
+    double hxgrid;
+    double hygrid;
+    double hzgrid;
+    int dimx;
+    int dimy;
+    int dimz;
+
+    double *total_rho;
+    double *gx;
+    double *gy;
+    double *gz;
+    double *q0;
+    double *dq0_drho;
+    double *dq0_dgradrho;
+    double *thetas;
+
+    // How many terms to include in the sum of SOLER equation 5.
+    int m_cut;
+
+    // ----------------------------------------------------------------------
+    // The next 2 parameters define the q mesh to be used in the vdW_DF code.
+    // These are perhaps the most important to have set correctly. Increasing
+    // the number of q points will DRAMATICALLY increase the memory usage of
+    // the vdW_DF code because the memory consumption depends quadratically
+    // on the number of q points in the mesh. Increasing the number of q
+    // points may increase accuracy of the vdW_DF code, although, in testing
+    // it was found to have little effect. The largest value of the q mesh
+    // is q_cut. All values of q0 (DION equation 11) larger than this value
+    // during a run will be saturated to this value using equation 5 of
+    // SOLER. In testing, increasing the value of q_cut was found to have
+    // little impact on the results, though it is possible that in some
+    // systems it may be more important. Always make sure that the variable
+    // Nqs is consistent with the number of q points that are actually in the
+    // variable q_mesh. Also, do not set any q value to 0. This will cause an
+    // infinity in the Fourier transform.
+    //
+    //
+    //                CHANGE THESE VALUES AT YOUR OWN RISK
+
+    static const int Nqs;
+    static const double q_mesh[20];
+
+    // largest value of q_mesh
+    double q_cut;
+
+public:
+    Vdw (BaseGrid &G, Lattice &L, TradeImages &T, int type, double *rho_valence, double *rho_core, double &etxc, double &vtxc, double *v);
+    ~Vdw(void);
+
+    void get_q0_on_grid (void);
+
+};
+
 
 
 #endif
--- XC_useLIBXC/vdw_correlation.cpp
+++ XC_useLIBXC/vdw_correlation.cpp
@@ -39,10 +39,35 @@
 */ 
 
 #include "const.h"
-#include "Lattice.h"
-#include "TradeImages.h"
-#include "FiniteDiff.h"
 #include "vdW.h"
+
+// ----------------------------------------------------------------------
+// The next 2 parameters define the q mesh to be used in the vdW_DF code.
+// These are perhaps the most important to have set correctly. Increasing
+// the number of q points will DRAMATICALLY increase the memory usage of
+// the vdW_DF code because the memory consumption depends quadratically
+// on the number of q points in the mesh. Increasing the number of q
+// points may increase accuracy of the vdW_DF code, although, in testing
+// it was found to have little effect. The largest value of the q mesh
+// is q_cut. All values of q0 (DION equation 11) larger than this value
+// during a run will be saturated to this value using equation 5 of
+// SOLER. In testing, increasing the value of q_cut was found to have
+// little impact on the results, though it is possible that in some
+// systems it may be more important. Always make sure that the variable
+// Nqs is consistent with the number of q points that are actually in the
+// variable q_mesh. Also, do not set any q value to 0. This will cause an
+// infinity in the Fourier transform.
+//
+//
+//                CHANGE THESE VALUES AT YOUR OWN RISK
+
+const int Nqs = 20;
+const double Vdw::q_mesh[20] = {
+1.0e-5             , 0.0449420825586261, 0.0975593700991365, 0.159162633466142,
+0.231286496836006, 0.315727667369529 , 0.414589693721418 , 0.530335368404141,
+0.665848079422965, 0.824503639537924 , 1.010254382520950 , 1.227727621364570,
+1.482340921174910, 1.780437058359530 , 2.129442028133640 , 2.538050036534580,
+3.016440085356680, 3.576529545442460 , 4.232271035198720 , 5.0 };
 
 /*
 
@@ -52,38 +77,100 @@
 
 */
 
-void xc_vdW_DF (BaseGrid &G, Lattice &L, TradeImages &T, int type, double *rho_valence, double *rho_core, double &etxc, double &vtxc, double *v)
+Vdw::Vdw (BaseGrid &G, Lattice &L, TradeImages &T, int type, double *rho_valence, double *rho_core, double &etxc, double &vtxc, double *v)
 {
 
-  int pbasis = G.get_P0_BASIS(G.default_FG_RATIO);
-  double hxgrid = G.get_hxgrid(G.default_FG_RATIO);
-  double hygrid = G.get_hygrid(G.default_FG_RATIO);
-  double hzgrid = G.get_hzgrid(G.default_FG_RATIO);
-  int dimx = G.get_PX0_GRID(G.default_FG_RATIO);
-  int dimy = G.get_PY0_GRID(G.default_FG_RATIO);
-  int dimz = G.get_PZ0_GRID(G.default_FG_RATIO);
+  // Grid parameters
+  this->pbasis = G.get_P0_BASIS(G.default_FG_RATIO);
+  this->hxgrid = G.get_hxgrid(G.default_FG_RATIO);
+  this->hygrid = G.get_hygrid(G.default_FG_RATIO);
+  this->hzgrid = G.get_hzgrid(G.default_FG_RATIO);
+  this->dimx = G.get_PX0_GRID(G.default_FG_RATIO);
+  this->dimy = G.get_PY0_GRID(G.default_FG_RATIO);
+  this->dimz = G.get_PZ0_GRID(G.default_FG_RATIO);
 
+  // How many terms to include in the sum of SOLER equation 5.
+  this->m_cut = 12;
 
+  // Largest value of q_cut
+  this->q_cut = q_mesh[Nqs];
 
   // Local storage
-  double *total_rho = new double[pbasis];
-  double *gx = new double[pbasis];
-  double *gy = new double[pbasis];
-  double *gz = new double[pbasis];
+  this->total_rho = new double[pbasis];
+  this->gx = new double[pbasis];
+  this->gy = new double[pbasis];
+  this->gz = new double[pbasis];
+  this->q0 = new double[pbasis]();
+  this->dq0_drho = new double[pbasis]();
+  this->dq0_dgradrho = new double[pbasis]();
+  this->thetas = new double[pbasis];
 
 
   
   // Get total charge and compute it's gradient
   for(int i = 0;i < pbasis;i++) total_rho[i] = rho_valence[i] + rho_core[i];
 
-  CPP_app_grad_driver (&L, &T, total_rho, gx, gy, gz, dimx, dimy, dimz, hxgrid, hygrid, hzgrid, APP_CI_SIXTH);
+  CPP_app_grad_driver (&L, &T, total_rho, gx, gy, gz, this->dimx, this->dimy, this->dimz, this->hxgrid, this->hygrid, this->hzgrid, APP_CI_SIXTH);
   
+  // --------------------------------------------------------------------
+  // Find the value of q0 for all assigned grid points. q is defined in
+  // equations 11 and 12 of DION and q0 is the saturated version of q
+  // defined in equation 5 of SOLER. This routine also returns the
+  // derivatives of the q0s with respect to the charge-density and the
+  // gradient of the charge-density. These are needed for the potential
+  // calculated below. This routine also calculates the thetas.
 
 
 
-  delete [] gz;
-  delete [] gy;
-  delete [] gx;
-  delete [] total_rho;
 
 }
+
+
+// Destructor just frees memory
+Vdw::~Vdw(void)
+{
+
+  delete [] this->thetas;
+  delete [] this->dq0_dgradrho;
+  delete [] this->dq0_drho;
+  delete [] this->q0;
+  delete [] this->gz;
+  delete [] this->gy;
+  delete [] this->gx;
+  delete [] this->total_rho;
+
+}
+
+
+// ####################################################################
+//                         |                  |
+//                         |  GET_Q0_ON_GRID  |
+//                         |__________________|
+//  
+//  This routine first calculates the q value defined in (DION equations
+//  11 and 12), then saturates it according to (SOLER equation 5). More
+//  specifically it calculates the following:
+//  
+//       q0(ir) = q0 as defined above
+//       dq0_drho(ir) = total_rho * d q0 /d rho
+//       dq0_dgradrho = total_rho / |grad_rho| * d q0 / d |grad_rho|
+
+void Vdw::get_q0_on_grid (void)
+{
+
+  // Initialize q0-related arrays.
+  for(int ix = 0;ix < this->pbasis;ix++) {
+      this->q0[ix] = this->q_cut;      
+      this->dq0_drho[ix] = 0.0;
+      this->dq0_dgradrho[ix] = 0.0;
+  }
+
+  for(int ix = 0;ix < this->pbasis;ix++) {
+
+      double trho = this->total_rho[ix];
+      
+
+  }
+  
+}
+
