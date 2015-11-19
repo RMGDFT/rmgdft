8232335ce7bdf01036f4d007c39f2558637a3487
Emil Briggs <elbriggs@ncsu.edu>
2015-11-19 20:19:32 +0000

    Added cmake module for pfft library.
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@3257 eb38b3b0-33ea-0310-94ad-d43fea0d214f

---
+++ cmake/Modules/FindPFFT.cmake
@@ -0,0 +1,26 @@
+# - Find PFFT
+# Find the native PFFT3 includes and library
+#
+#  PFFT_INCLUDES    - where to find pfft.h
+#  PFFT_LIBRARIES   - List of libraries when using PFFT3.
+#  PFFT_FOUND       - True if PFFT3 found.
+
+if (PFFT_INCLUDES)
+  # Already in cache, be silent
+  set (PFFT_FIND_QUIETLY TRUE)
+endif (PFFT_INCLUDES)
+
+find_path (PFFT_INCLUDES pfft.h)
+
+find_library (PFFT_LIBRARIES NAMES pfft)
+if(NOT PFFT_LIBRARIES)
+    find_library (PFFT_LIBRARIES NAMES libpfft.a)
+endif(NOT PFFT_LIBRARIES)
+
+# handle the QUIETLY and REQUIRED arguments and set PFFT_FOUND to TRUE if
+# all listed variables are TRUE
+include (FindPackageHandleStandardArgs)
+find_package_handle_standard_args (PFFT DEFAULT_MSG PFFT_LIBRARIES PFFT_INCLUDES)
+
+mark_as_advanced (PFFT_LIBRARIES PFFT_INCLUDES)
+
