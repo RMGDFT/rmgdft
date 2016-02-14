7ce4855bbd4f013b34e19898362ffcc662c5c9a6
Emil Briggs <elbriggs@ncsu.edu>
2016-02-14 14:23:32 +0000

    Updated to libxc-2.2.2
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@3309 eb38b3b0-33ea-0310-94ad-d43fea0d214f

--- cmake/Modules/FindLIBXC.cmake
+++ cmake/Modules/FindLIBXC.cmake
@@ -1,6 +1,6 @@
 # - Find Libxc
 #
-#  LIBXC_INCLUDES    - where to find fftw.h
+#  LIBXC_INCLUDES    - where to find xc.h
 #  LIBXC_LIBRARIES   - List of libraries when using LIBXC.
 #  LIBXC_FOUND       - True if LIBXC found.
 
@@ -10,12 +10,18 @@ if (LIBXC_INCLUDES)
 endif (LIBXC_INCLUDES)
 
 find_path (LIBXC_INCLUDES xc.h 
-HINTS "${PROJECT_SOURCE_DIR}/lib/libxc-2.0.3/lib64/include")
+HINTS "${PROJECT_SOURCE_DIR}/lib/libxc-2.2.2/lib64/include")
 
-find_library (LIBXC_LIBRARIES 
+find_library (LIBXC_LIBRARIES
 NAMES xc
-HINTS "${PROJECT_SOURCE_DIR}/lib/libxc-2.0.3/lib64/lib64"
-"${PROJECT_SOURCE_DIR}/lib/libxc-2.0.3/lib64/lib"
+HINTS "${PROJECT_SOURCE_DIR}/lib/libxc-2.2.2/lib64/lib64"
+"${PROJECT_SOURCE_DIR}/lib/libxc-2.2.2/lib64/lib"
+)
+
+find_library (LIBXC_LIBRARIES_F90
+NAMES xcf90
+HINTS "${PROJECT_SOURCE_DIR}/lib/libxc-2.2.2/lib64/lib64"
+"${PROJECT_SOURCE_DIR}/lib/libxc-2.2.2/lib64/lib"
 )
 
 # handle the QUIETLY and REQUIRED arguments and set LIBXC_FOUND to TRUE if
