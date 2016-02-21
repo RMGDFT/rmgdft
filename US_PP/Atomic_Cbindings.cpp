b0afce2cd903e39ff5882bf3e1d6620aeab9c78f
Emil Briggs <elbriggs@ncsu.edu>
2016-02-21 15:37:14 +0000

    Also left this file out earlier.
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@3341 eb38b3b0-33ea-0310-94ad-d43fea0d214f

---
+++ US_PP/Atomic_Cbindings.cpp
@@ -0,0 +1,36 @@
+/*
+ *
+ * Copyright 2014 The RMG Project Developers. See the COPYRIGHT file 
+ * at the top-level directory of this distribution or in the current
+ * directory.
+ * 
+ * This file is part of RMG. 
+ * RMG is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * RMG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+*/
+
+
+#include "transition.h"
+#include "RmgException.h"
+#include "Atomic.h"
+
+static Atomic *A = NULL;
+
+extern "C" double AtomicInterpolate(double *f, double r)
+{
+    if(!A) {
+        A = new Atomic();
+    }
+    return A->Interpolate(f, r);
+}
