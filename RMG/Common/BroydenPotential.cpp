b121167ab33c688584a024f05cbe6a76b41a6932
Emil Briggs <elbriggs@ncsu.edu>
2016-04-14 13:03:42 +0000

    Switched to general matrix inversion routine which gives much better results.
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@3456 eb38b3b0-33ea-0310-94ad-d43fea0d214f

--- RMG/Common/BroydenPotential.cpp
+++ RMG/Common/BroydenPotential.cpp
@@ -47,7 +47,7 @@
 
 
 
-#define MAX_BROYDEN_ITER 20
+#define MAX_BROYDEN_ITER 12
 
 void BroydenPotential(double *rho, double *new_rho, double *rhoc, double *vh_in, int max_iter, bool reset)
 {
@@ -55,12 +55,15 @@ void BroydenPotential(double *rho, double *new_rho, double *rhoc, double *vh_in,
    static boost::circular_buffer<double *> df(MAX_BROYDEN_ITER);
    static boost::circular_buffer<double *> dv(MAX_BROYDEN_ITER);
    static boost::circular_buffer<double *> dvh(MAX_BROYDEN_ITER);
+   static boost::circular_buffer<double> convergence(MAX_BROYDEN_ITER);
    static int iter;
-   static double betamix[MAX_BROYDEN_ITER][MAX_BROYDEN_ITER];
+   static int convergence_count;
+   double betamix[MAX_BROYDEN_ITER][MAX_BROYDEN_ITER];
    int pbasis = Rmg_G->get_P0_BASIS(Rmg_G->default_FG_RATIO);
-   double vel = Rmg_L.get_omega() / ((double)(Rmg_G->get_NX_GRID(1) * Rmg_G->get_NY_GRID(1) * Rmg_G->get_NZ_GRID(1)));
+   int ratio = Rmg_G->default_FG_RATIO;
+   double vel = Rmg_L.get_omega() / ((double)(Rmg_G->get_NX_GRID(ratio) * Rmg_G->get_NY_GRID(ratio) * Rmg_G->get_NZ_GRID(ratio)));
    int ld_betamix = MAX_BROYDEN_ITER;
-
+   double nmix = 0.3;
 
    // Check if this is a reset request
    if(reset) {
@@ -77,27 +80,48 @@ void BroydenPotential(double *rho, double *new_rho, double *rhoc, double *vh_in,
            delete [] dvh[0];
            dvh.pop_front();
        }
+       while(convergence.size()) {
+           convergence.pop_front();
+       }
+       iter = 0;
        return;
    }
 
-   // Set up arrays and get delta rho
-   double *rhout_m = new double[pbasis];
-   double *rhoin_m = new double[pbasis];
-   double *vh_out = new double[pbasis];
-   for(int i = 0;i < pbasis;i++) rhoin_m[i] = rho[i];
-   for(int i = 0;i < pbasis;i++) rhout_m[i] = new_rho[i];
-   for(int i = 0;i < pbasis;i++) rhout_m[i] -= rhoin_m[i]; 
-
    // Get Hartree potential for the output density
    double rms_target = std::min(ct.rms/ct.hartree_rms_ratio, 1.0e-12);
+   double *vh_out = new double[pbasis]();
+   for(int i = 0;i < pbasis;i++) vh_out[i] = vh_in[i];
    get_vh (new_rho, rhoc, vh_out, 5, 100, ct.poi_parm.levels, rms_target, ct.boundaryflag);
 
    // Compute convergence measure
    double sum = 0.0;
-   for(int i = 0;i < pbasis;i++) sum += (vh_out[i] - vh_in[i]) * rhout_m[i];
+   for(int i = 0;i < pbasis;i++) sum += (vh_out[i] - vh_in[i]) * (new_rho[i] - rho[i]);
    sum = 0.5 * vel * sum;
    MPI_Allreduce(MPI_IN_PLACE, &sum, 1, MPI_DOUBLE, MPI_SUM, pct.grid_comm);
-   if(pct.gridpe==0)printf("Broyden convergene check = %20.12e\n", sum);
+
+   // Convergence is stored with newest entries in position 0
+   convergence.push_front(sum);
+   if(pct.gridpe==0)printf("Broyden convergence check = %20.12e\n", sum);
+
+   // If convergence goes up 3 times in a row Broyden is failing
+   if(convergence.size() > 1) {
+       if(sum > convergence[1]) {
+           if(convergence_count > 3) {
+               throw RmgFatalException() << "Broyden mixing failing. Try Pulay or linear mixing.\n";
+           }
+           convergence_count++;
+       }
+       else {
+           convergence_count = 0;
+       }
+   }
+
+   // Set up arrays and get delta rho
+   double *rhout = new double[pbasis];
+   double *rhoin = new double[pbasis];
+   for(int i = 0;i < pbasis;i++) rhoin[i] = rho[i];
+   for(int i = 0;i < pbasis;i++) rhout[i] = new_rho[i];
+   for(int i = 0;i < pbasis;i++) rhout[i] -= rhoin[i]; 
 
 
    // Check if it's time to remove old entries
@@ -108,23 +132,24 @@ void BroydenPotential(double *rho, double *new_rho, double *rhoc, double *vh_in,
        df.pop_front();
        dv.pop_front();
        dvh.pop_front();
+       convergence.pop_front();
    }
 
 
    if(iter > 0) {
-       for(int i = 0;i < pbasis;i++) df[df.size() - 1][i] -= rhout_m[i];
-       for(int i = 0;i < pbasis;i++) dv[df.size() - 1][i] -= rhoin_m[i];
+       for(int i = 0;i < pbasis;i++) df[df.size() - 1][i] -= rhout[i];
+       for(int i = 0;i < pbasis;i++) dv[df.size() - 1][i] -= rhoin[i];
    }
 
    int iter_used = df.size();
 
    // Create new entries
    double *df1 = new double[pbasis];
-   for(int i = 0;i < pbasis;i++) df1[i] = rhout_m[i];
+   for(int i = 0;i < pbasis;i++) df1[i] = rhout[i];
    df.push_back(df1);
 
    double *dv1 = new double[pbasis];
-   for(int i = 0;i < pbasis;i++) dv1[i] = rhoin_m[i];
+   for(int i = 0;i < pbasis;i++) dv1[i] = rhoin[i];
    dv.push_back(dv1);
 
    double *dvh1 = new double[pbasis];
@@ -132,39 +157,34 @@ void BroydenPotential(double *rho, double *new_rho, double *rhoc, double *vh_in,
    dvh.push_back(dvh1);
 
 
-
    if(iter_used > 0) {
 
        for(int i = 0;i < iter_used;i++) {
-           for(int j = i;j < iter_used;j++) {
+           for(int j = 0;j < iter_used;j++) {
                betamix[j][i] = 0.0;
                for(int k = 0;k < pbasis;k++) betamix[j][i] += df[i][k] * dvh[j][k];
-               betamix[i][j] = betamix[j][i];
            }
        }
 
        MPI_Allreduce(MPI_IN_PLACE, betamix, MAX_BROYDEN_ITER*MAX_BROYDEN_ITER, MPI_DOUBLE, MPI_SUM, pct.grid_comm);
 
-       double work[MAX_BROYDEN_ITER];
+       double work[MAX_BROYDEN_ITER*MAX_BROYDEN_ITER];
        int iwork[MAX_BROYDEN_ITER];
+       int ipiv[MAX_BROYDEN_ITER*MAX_BROYDEN_ITER];
+       int lwork = MAX_BROYDEN_ITER*MAX_BROYDEN_ITER;
        int info = 0;
-       dsytrf("U", &iter_used, &betamix[0][0], &ld_betamix, iwork, work, &iter_used, &info );
+       dgetrf(&iter_used, &iter_used, &betamix[0][0], &ld_betamix, ipiv,  &info );
        if(info)
-           throw RmgFatalException() << "dsytrf failed " << " in " << __FILE__ << " at line " << __LINE__ << "\n";
+           throw RmgFatalException() << "dgetrf failed " << " in " << __FILE__ << " at line " << __LINE__ << "\n";
 
-       dsytri("U", &iter_used, &betamix[0][0], &ld_betamix, iwork, work, &info );
+       dgetri(&iter_used, &betamix[0][0], &ld_betamix, ipiv, work, &lwork, &info );
        if(info)
-           throw RmgFatalException() << "dsytri failed " << " in " << __FILE__ << " at line " << __LINE__ << "\n";
+           throw RmgFatalException() << "dgetri failed " << " in " << __FILE__ << " at line " << __LINE__ << "\n";
 
-       for(int i = 0;i < iter_used;i++) {
-           for(int j = 0;j < iter_used;j++) {
-               if(j > i) betamix[i][j] = betamix[j][i];
-           }
-       }
        
        for(int i = 0;i < iter_used;i++) {
            work[i] = 0.0;
-           for(int k = 0;k < pbasis;k++) work[i] += dvh[i][k] * rhout_m[k];
+           for(int k = 0;k < pbasis;k++) work[i] += dvh[i][k] * rhout[k];
            MPI_Allreduce(MPI_IN_PLACE, &work[i], 1, MPI_DOUBLE, MPI_SUM, pct.grid_comm);
        }
 
@@ -172,10 +192,10 @@ void BroydenPotential(double *rho, double *new_rho, double *rhoc, double *vh_in,
            double gamma = 0.0;
            for(int j=0;j < iter_used;j++) gamma += work[j] * betamix[i][j];
 
-           for(int k=0;k < pbasis;k++) rhout_m[k] -= gamma * df[i][k];
+           for(int k=0;k < pbasis;k++) rhout[k] -= gamma * df[i][k];
 
            for(int k=0;k < pbasis;k++) {
-               rhoin_m[k] -= gamma * dv[i][k];
+               rhoin[k] -= gamma * dv[i][k];
            }
 
        }
@@ -183,13 +203,13 @@ void BroydenPotential(double *rho, double *new_rho, double *rhoc, double *vh_in,
    }
 
    // New density. This is the place to do things with screening and frequency based mixing
-   for(int k=0;k < pbasis;k++)  rhoin_m[k] =  rhoin_m[k] + 0.5*rhout_m[k];
-   for(int k=0;k < pbasis;k++)  rho[k] = rhoin_m[k];
+   for(int k=0;k < pbasis;k++)  rhoin[k] =  rhoin[k] + nmix*rhout[k];
+   for(int k=0;k < pbasis;k++)  rho[k] = rhoin[k];
 
 
    delete [] vh_out;
-   delete [] rhoin_m;
-   delete [] rhout_m;
+   delete [] rhoin;
+   delete [] rhout;
 
    iter++;
 }
