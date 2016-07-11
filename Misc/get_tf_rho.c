1951494a7a018543172e2f2b734c54af294a89c0
Miroslav Hodak <mhodak@ncsu.edu>
2016-07-11 00:25:48 +0000

    Updates for the  simplified solvent model.
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codes@3782 eb38b3b0-33ea-0310-94ad-d43fea0d214f

--- Misc/get_tf_rho.c
+++ Misc/get_tf_rho.c
@@ -7,8 +7,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "grid.h"
+#include "rmg_error.h"
 #include "common_prototypes.h"
 #include "main.h"
+#include "AtomicInterpolate.h"
 
 
 void get_tf_rho (double * tf_rho)
@@ -25,14 +27,15 @@ void get_tf_rho (double * tf_rho)
     int FNX_GRID, FNY_GRID, FNZ_GRID;
     int tf_num_ions;
 
-    double r, t1;
+    double r, t1, t2, t3;
     double x[3];
     double hxxgrid, hyygrid, hzzgrid;
     double xside, yside, zside;
     TF_ION *tf_iptr;
     int npes = get_PE_X() * get_PE_Y() * get_PE_Z();
     double lradius = 9.0;
-    double alpha, alpha0, q0, q;
+    double alpha, alpha0, q0, q, prefactor, prefactor0;
+
 
     hxxgrid = get_hxxgrid();
     hyygrid = get_hyygrid();
@@ -63,6 +66,7 @@ void get_tf_rho (double * tf_rho)
     tf_num_ions = ct.num_tfions;
 
 
+#if 1
 
 
     /* Initialize the TF charge array */
@@ -70,18 +74,23 @@ void get_tf_rho (double * tf_rho)
     {
 	tf_rho[idx] = 0.0;
     }
+#endif
 
     /* Loop over ions */
     for (ion = 0; ion < tf_num_ions; ion++)
     {
 	tf_iptr = &ct.tf_ions[ion];
 	to_crystal (tf_iptr->xtal, tf_iptr->crds);
+	
 
+#if 1
 	/*Coefficients for gaussians representing charge density and ions of water molecules */
 	q = tf_iptr->q;
 	q0 = tf_iptr->q0;
 	alpha = tf_iptr->alpha;
 	alpha0 = tf_iptr->alpha0;
+
+	//printf("\n TF ion %d: q:%f q0:%f alpha:%f alpha0:%f", ion, q, q0, alpha, alpha0);
 
 
 	dimx =  lradius/(hxxgrid*xside);
@@ -93,12 +102,16 @@ void get_tf_rho (double * tf_rho)
 	dimz = dimz * 2 + 1;
 
 
-	xstart = tf_iptr[ion].xtal[0] / hxxgrid - dimx/2;
+	xstart =  tf_iptr->xtal[0] / hxxgrid - dimx/2;
 	xend = xstart + dimx;
-	ystart = tf_iptr[ion].xtal[1] / hyygrid - dimy/2;
+	ystart =  tf_iptr->xtal[1] / hyygrid - dimy/2;
 	yend = ystart + dimy;
-	zstart = tf_iptr[ion].xtal[2] / hzzgrid - dimz/2;
+	zstart =  tf_iptr->xtal[2] / hzzgrid - dimz/2;
 	zend = zstart + dimz;
+	
+
+	prefactor = q * pow( (alpha/3.14159265), 1.5);
+	prefactor0 = q0 * pow( (alpha0/3.14159265), 1.5);
 
 
 	for (ix = xstart; ix < xend; ix++)
@@ -122,14 +135,20 @@ void get_tf_rho (double * tf_rho)
 			    {
 
 				idx = (ixx-ilow) * FPY0_GRID * FPZ0_GRID + (iyy-jlow) * FPZ0_GRID + izz-klow;
-				x[0] = ix * hxxgrid - tf_iptr[ion].xtal[0];
-				x[1] = iy * hyygrid - tf_iptr[ion].xtal[1];
-				x[2] = iz * hzzgrid - tf_iptr[ion].xtal[2];
+
+				/*if ((idx < 0) || (idx >= FP0_BASIS))
+				{
+				    printf("\n Warning: idx out pf bounds, should be 0-%d, but is %d", FP0_BASIS, idx);
+				    rmg_error_handler ("idx out of bounds");
+				}*/
+				    
+				x[0] = ix * hxxgrid -  tf_iptr->xtal[0];
+				x[1] = iy * hyygrid -  tf_iptr->xtal[1];
+				x[2] = iz * hzzgrid -  tf_iptr->xtal[2];
 				r = metric (x);
 
-				tf_rho[idx] += q * pow( (alpha/3.14159265), 1.5) * exp (-1.0*alpha*r*r);
-				tf_rho[idx] -= q0 * pow( (alpha0/3.14159265), 1.5) * exp (-1.0*alpha0*r*r);
-
+				tf_rho[idx] += prefactor * exp (-1.0*alpha*r*r);
+				tf_rho[idx] -= prefactor0 * exp (-1.0*alpha0*r*r);
 
 
 
@@ -141,19 +160,29 @@ void get_tf_rho (double * tf_rho)
 	    }
 
 	}
+#endif
     }
     
     
+#if 1
     /* Set net TF charge to 0 */
     t1 = 0.0;
+    t2 = 0.0;
+    t3 = 0.0;
     for (idx = 0; idx < FP0_BASIS; idx++)
+    {
 	t1 += tf_rho[idx];
+    }
+
+
+    printf("\n t1 t2 t3: %f %f %f", t1, t2, t3);
 
 
     t1 = real_sum_all (t1, pct.grid_comm);  /* sum over pct.grid_comm  */
     
+    
     if (pct.imgpe==0)
-	printf("\nTotal TF charge is %.8e. It is automatically being set to 0\n", t1*get_vel_f());
+	printf("\nTotal TF charge is %.8e \n", t1*get_vel_f());
 
 
     t1 /= (FP0_BASIS*npes);
@@ -173,6 +202,7 @@ void get_tf_rho (double * tf_rho)
 	printf("\nTotal TF charge after adjustment is %.8e\n", t1*get_vel_f());
 
 
+#endif
 
 
 
