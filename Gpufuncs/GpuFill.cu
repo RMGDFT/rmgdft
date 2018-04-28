db0d07ffc1c2ac02566ee587052e66f037db845d
Emil Briggs <elbriggs@ncsu.edu>
2018-04-28 16:59:53 +0000

    Get rid of thrust dependence for setting GPU to a specific floating point value.
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codesv3@4368 eb38b3b0-33ea-0310-94ad-d43fea0d214f

--- Gpufuncs/GpuFill.cu
+++ Gpufuncs/GpuFill.cu
@@ -23,20 +23,24 @@
 
 
 #if GPU_ENABLED
-#include <thrust/host_vector.h>
-#include <thrust/device_vector.h>
-
-#include <thrust/copy.h>
-#include <thrust/fill.h>
-#include <thrust/sequence.h>
+#include <cuda.h>
 #include "Gpufuncs.h"
+
+template <typename RmgType>
+__global__ void FillMatrix(RmgType *matrix, int n, RmgType val)
+{
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    for (int i = idx; i < n; i += gridDim.x * blockDim.x) matrix[i]=val;
+}
+
 
 // Fills a device pointer with a floating point value
 void GpuFill(double *dptr, int n, double fillval)
 {
     cudaDeviceSynchronize();
-    thrust::device_ptr<double> wptr = thrust::device_pointer_cast(dptr);
-    thrust::fill(wptr, wptr + n, (double) fillval); 
+    int blockSize = 256;
+    int numBlocks = (n + blockSize - 1) / n;
+    FillMatrix<<<numBlocks, blockSize>>>(dptr, n, fillval);
     cudaDeviceSynchronize();
 }
 
