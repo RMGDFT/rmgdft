fc77d6f693c1aa1746c923fc898df5ff27e01e03
Emil Briggs <elbriggs@ncsu.edu>
2018-09-07 17:30:45 +0000

    Filtering update that provides better grid independence.
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codesv3@4498 eb38b3b0-33ea-0310-94ad-d43fea0d214f

--- Misc/FftInterpolation.cpp
+++ Misc/FftInterpolation.cpp
@@ -99,7 +99,7 @@ void FftInterpolation (BaseGrid &G, double *coarse, double *fine, int ratio, boo
   // Zero higher frequency components
   for(int ix = 0;ix < pbasis_c;ix++)
   {
-      //if(coarse_pwaves->gmags[ix] >= coarse_pwaves->gcut) base_coarse[ix]=std::complex<double>(0.0, 0.0);
+      if(coarse_pwaves->gmask[ix] < 1.0) base_coarse[ix]=std::complex<double>(0.0, 0.0);
   }
 
 
