3b57f9c8048ed172c5e00b8d60467f43ad2bc10f
Wenchang Lu <wlu2@ncsu.edu>
2019-01-23 21:52:10 +0000

    speed up for augmented charge density calculation
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codesv3@4605 eb38b3b0-33ea-0310-94ad-d43fea0d214f

--- ON/ON-NEGF-share/RhoQnmMat.cpp
+++ ON/ON-NEGF-share/RhoQnmMat.cpp
@@ -43,6 +43,7 @@ void RhoQnmMat(double *Aij, double * global_mat_X)
 
     int idx1, idx2, num_orb, tot_orb;
 
+    double *mat_kbpsi = new double[ct.max_nl];
 
     for (ion = 0; ion < pct.n_ion_center; ion++)
     {
@@ -56,23 +57,31 @@ void RhoQnmMat(double *Aij, double * global_mat_X)
             st1 = Kbpsi_str.orbital_index[ion][idx1];
             st11 = st1 - ct.state_begin;
 
+            for(ip1 = 0; ip1 < nh; ip1++) mat_kbpsi[ip1] = 0.0;
+
             for(idx2 = 0; idx2 < tot_orb; idx2++)
             {
                 st2 = Kbpsi_str.orbital_index[ion][idx2];
 
-
-                for (ip1 = 0; ip1 < nh; ip1++)
+                for (ip2 = 0; ip2 < nh; ip2++)
                 {
-                    for (ip2 = 0; ip2 < nh; ip2++)
-                    {
-                        ist = ion1 * ct.max_nl * ct.max_nl + ip1 * ct.max_nl + ip2;
-                        Aij[ist] +=
-                            global_mat_X[st11 * ct.num_states + st2] *
-                            Kbpsi_str.kbpsi_ion[ion][idx1 * nh + ip1] *
-                            Kbpsi_str.kbpsi_ion[ion][idx2 * nh + ip2];
-                    }
+                    mat_kbpsi[ip2] += global_mat_X[st11 * ct.num_states + st2] *
+                        Kbpsi_str.kbpsi_ion[ion][idx2 * nh + ip2];
                 }
-            }             
-        }                
-    }
+            }
+
+
+            for (ip1 = 0; ip1 < nh; ip1++)
+            {
+                for (ip2 = 0; ip2 < nh; ip2++)
+                {
+                    ist = ion1 * ct.max_nl * ct.max_nl + ip1 * ct.max_nl + ip2;
+                    Aij[ist] += mat_kbpsi[ip2] *
+                        Kbpsi_str.kbpsi_ion[ion][idx1 * nh + ip1];
+                }
+            }
+        }             
+    }                
+
+    delete [] mat_kbpsi;
 }
