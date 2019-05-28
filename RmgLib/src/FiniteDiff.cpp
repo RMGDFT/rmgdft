f64eebc87ec204939dee36f600d5b09fcc62147c
Emil Briggs <elbriggs@ncsu.edu>
2019-05-28 18:49:09 +0000

    Some updates for anisotropic case.
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codesv3@4705 eb38b3b0-33ea-0310-94ad-d43fea0d214f

--- RmgLib/src/FiniteDiff.cpp
+++ RmgLib/src/FiniteDiff.cpp
@@ -1716,7 +1716,7 @@ double FiniteDiff::app8_del2(RmgType * __restrict__ a, RmgType * __restrict__ b,
     double maxh = std::max(h2x, h2y);
     maxh = std::max(maxh, h2z);
 
-    double hadj = h2x / maxh;
+    double hadj = sqrt(h2x / maxh);
     if(this->alt_laplacian) c2 = -1.0/(1.0+dr*hadj/k2);
     c1 = 1.0 + c2;
     double th2 = (c1*w1[ic] - c2*w2[ic-1]) / h2x;
@@ -1725,7 +1725,7 @@ double FiniteDiff::app8_del2(RmgType * __restrict__ a, RmgType * __restrict__ b,
     RmgType t3x ((c1*w1[ic+3] - c2*w2[ic+2]) * hf / h2x);
     RmgType t4x (c1*w1[ic+4]*hf / h2x);
 
-    hadj = h2y / maxh;
+    hadj = sqrt(h2y / maxh);
     if(this->alt_laplacian) c2 = -1.0/(1.0+dr*hadj/k2);
     c1 = 1.0 + c2;
     th2 += (c1*w1[ic] - c2*w2[ic-1]) /  h2y;
@@ -1734,7 +1734,7 @@ double FiniteDiff::app8_del2(RmgType * __restrict__ a, RmgType * __restrict__ b,
     RmgType t3y ((c1*w1[ic+3] - c2*w2[ic+2]) * hf / h2y);
     RmgType t4y (c1*w1[ic+4]*hf / h2y);
 
-    hadj = h2z / maxh;
+    hadj = sqrt(h2z / maxh);
     if(this->alt_laplacian) c2 = -1.0/(1.0+dr*hadj/k2);
     c1 = 1.0 + c2;
     th2 += (c1*w1[ic] - c2*w2[ic-1]) /  h2z;
@@ -1884,39 +1884,41 @@ double FiniteDiff::app10_del2(RmgType * a, RmgType * b, int dimx, int dimy, int 
     double d2 = 6.01250601250605e-5;
     double dr = d1 / d2;
     double k2 = PI*PI/8.0;
+    double h2x = gridhx * gridhx * L->get_xside() * L->get_xside();
+    double h2y = gridhy * gridhy * L->get_yside() * L->get_yside();
+    double h2z = gridhz * gridhz * L->get_zside() * L->get_zside();
+    double maxh = std::max(h2x, h2y);
+    maxh = std::max(maxh, h2z);
 
-    double h2 = gridhx * gridhx * L->get_xside() * L->get_xside();
-    if(this->alt_laplacian) c2 = -1.0/(1.0+dr/k2);
+    double hadj = sqrt(h2x / maxh);
+    if(this->alt_laplacian) c2 = -1.0/(1.0+dr*hadj/k2);
     c1 = 1.0 + c2;
+    double th2 = (c1*w1[ic] - c2*w2[ic-1]) / h2x;
+    RmgType t1x ((c1*w1[ic+1] - c2*w2[ic]) * hf / h2x);
+    RmgType t2x ((c1*w1[ic+2] - c2*w2[ic+1]) * hf / h2x);
+    RmgType t3x ((c1*w1[ic+3] - c2*w2[ic+2]) * hf / h2x);
+    RmgType t4x ((c1*w1[ic+4] - c2*w2[ic+3])*hf / h2x);
+    RmgType t5x (c1*w1[ic+5]*hf / h2x);
 
-    double th2 = (c1*w1[ic] - c2*w2[ic-1]) / h2;
-    RmgType t1x ((c1*w1[ic+1] - c2*w2[ic]) * hf / h2);
-    RmgType t2x ((c1*w1[ic+2] - c2*w2[ic+1]) * hf / h2);
-    RmgType t3x ((c1*w1[ic+3] - c2*w2[ic+2]) * hf / h2);
-    RmgType t4x ((c1*w1[ic+4] - c2*w2[ic+3])*hf / h2);
-    RmgType t5x (c1*w1[ic+5]*hf / h2);
-
-    h2 = gridhy * gridhy * L->get_yside() * L->get_yside();
-    if(this->alt_laplacian) c2 = -1.0/(1.0+dr/k2);
+    hadj = sqrt(h2y / maxh);
+    if(this->alt_laplacian) c2 = -1.0/(1.0+dr*hadj/k2);
     c1 = 1.0 + c2;
+    th2 += (c1*w1[ic] - c2*w2[ic-1]) /  h2y;
+    RmgType t1y ((c1*w1[ic+1] - c2*w2[ic]) * hf / h2y);
+    RmgType t2y ((c1*w1[ic+2] - c2*w2[ic+1]) * hf / h2y);
+    RmgType t3y ((c1*w1[ic+3] - c2*w2[ic+2]) * hf / h2y);
+    RmgType t4y ((c1*w1[ic+4] - c2*w2[ic+3])*hf / h2y);
+    RmgType t5y (c1*w1[ic+5]*hf / h2y);
 
-    th2 += (c1*w1[ic] - c2*w2[ic-1]) /  h2;
-    RmgType t1y ((c1*w1[ic+1] - c2*w2[ic]) * hf / h2);
-    RmgType t2y ((c1*w1[ic+2] - c2*w2[ic+1]) * hf / h2);
-    RmgType t3y ((c1*w1[ic+3] - c2*w2[ic+2]) * hf / h2);
-    RmgType t4y ((c1*w1[ic+4] - c2*w2[ic+3])*hf / h2);
-    RmgType t5y (c1*w1[ic+5]*hf / h2);
-
-    h2 = gridhz * gridhz * L->get_zside() * L->get_zside();
-    if(this->alt_laplacian) c2 = -1.0/(1.0+dr/k2);
+    hadj = sqrt(h2z / maxh);
+    if(this->alt_laplacian) c2 = -1.0/(1.0+dr*hadj/k2);
     c1 = 1.0 + c2;
-
-    th2 += (c1*w1[ic] - c2*w2[ic-1]) /  h2;
-    RmgType t1z ((c1*w1[ic+1] - c2*w2[ic]) * hf / h2);
-    RmgType t2z ((c1*w1[ic+2] - c2*w2[ic+1]) * hf / h2);
-    RmgType t3z ((c1*w1[ic+3] - c2*w2[ic+2]) * hf / h2);
-    RmgType t4z ((c1*w1[ic+4] - c2*w2[ic+3])*hf / h2);
-    RmgType t5z (c1*w1[ic+5]*hf / h2);
+    th2 += (c1*w1[ic] - c2*w2[ic-1]) /  h2z;
+    RmgType t1z ((c1*w1[ic+1] - c2*w2[ic]) * hf / h2z);
+    RmgType t2z ((c1*w1[ic+2] - c2*w2[ic+1]) * hf / h2z);
+    RmgType t3z ((c1*w1[ic+3] - c2*w2[ic+2]) * hf / h2z);
+    RmgType t4z ((c1*w1[ic+4] - c2*w2[ic+3])*hf / h2z);
+    RmgType t5z (c1*w1[ic+5]*hf / h2z);
     RmgType t0 (th2);
 
 
