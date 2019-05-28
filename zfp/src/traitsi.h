59c13f5b63502a602c3b6b166c6b068c4dd78c6a
Emil Briggs <elbriggs@ncsu.edu>
2019-05-28 16:15:12 +0000

    Update to latest zfp libs.
    git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codesv3@4702 eb38b3b0-33ea-0310-94ad-d43fea0d214f

--- zfp/src/traitsi.h
+++
@@ -1,6 +0,0 @@
-/* 32-bit integer traits */
-
-#define Scalar int32       /* integer type */
-#define Int int32          /* corresponding signed integer type */
-#define UInt uint32        /* corresponding unsigned integer type */
-#define NBMASK 0xaaaaaaaau /* negabinary mask */
