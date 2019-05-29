RMGDFT/rmgdft

commit dd8848d94899cfe6fe5113baf0035743e9fba841

Updated laplacian operators plus some filtering and total energy adjustments. Default grid ratio for norm-conserving is now set to 1.
git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codesv3@4518 eb38b3b0-33ea-0310-94ad-d43fea0d214f

RmgLib/src/FiniteDiff.cpp
@@ -1710,28 +1710,40 @@ double FiniteDiff::app8_del2(RmgType * __restrict__ a, RmgType * __restrict__ b,
     int ixs = (dimy + 8) * (dimz + 8);
     int iys = (dimz + 8);
 
-    double hf = 1.0;
+    // nine and seven point stencils, 2nd derivative, extrapolated
+    int ic = 4;
+    double x[9], w1[9], w2[9];
+    for(int i=0;i<9;i++) x[i] = (double)i;
+    gen_weights(9, 2, (double)ic, x, w1);
+    gen_weights(7, 2, (double)(ic-1), x, w2);
+    double hf = 1.0, c1, c2, beta = 3.0;
     if(ibrav == HEXAGONAL) hf = 2.0/3.0;
     double h2 = gridhx * gridhx * L->get_xside() * L->get_xside();
-    double th2 (-205.0 / (72.0 * h2));
-    RmgType t1x (8.0 * hf / ( 5.0 * h2));
-    RmgType t2x (-1.0 * hf / (5.0 * h2));
-    RmgType t3x (8.0 * hf / (315.0 * h2));
-    RmgType t4x (-1.0 * hf / (560.0 * h2));
+    c1 = 1.0 + beta;
+    c2 = beta;
+    double th2 = (c1*w1[ic] - c2*w2[ic-1]) / h2;
+    RmgType t1x ((c1*w1[ic+1] - c2*w2[ic]) * hf / h2);
+    RmgType t2x ((c1*w1[ic+2] - c2*w2[ic+1]) * hf / h2);
+    RmgType t3x ((c1*w1[ic+3] - c2*w2[ic+2]) * hf / h2);
+    RmgType t4x (c1*w1[ic+4]*hf / h2);
 
     h2 = gridhy * gridhy * L->get_yside() * L->get_yside();
-    th2 -= (205.0 / (72.0 * h2));
-    RmgType t1y (8.0 / ( 5.0 * h2));
-    RmgType t2y (-1.0 / (5.0 * h2));
-    RmgType t3y (8.0 / (315.0 * h2));
-    RmgType t4y (-1.0 / (560.0 * h2));
+    c1 = 1.0 + beta;
+    c2 = beta;
+    th2 += (c1*w1[ic] - c2*w2[ic-1]) /  h2;
+    RmgType t1y ((c1*w1[ic+1] - c2*w2[ic]) * hf / h2);
+    RmgType t2y ((c1*w1[ic+2] - c2*w2[ic+1]) * hf / h2);
+    RmgType t3y ((c1*w1[ic+3] - c2*w2[ic+2]) * hf / h2);
+    RmgType t4y (c1*w1[ic+4]*hf / h2);
 
     h2 = gridhz * gridhz * L->get_zside() * L->get_zside();
-    th2 -= (205.0 / (72.0 * h2));
-    RmgType t1z (8.0 / ( 5.0 * h2));
-    RmgType t2z (-1.0 / (5.0 * h2));
-    RmgType t3z (8.0 / (315.0 * h2));
-    RmgType t4z (-1.0 / (560.0 * h2));
+    c1 = 1.0 + beta;
+    c2 = beta;
+    th2 += (c1*w1[ic] - c2*w2[ic-1]) /  h2;
+    RmgType t1z ((c1*w1[ic+1] - c2*w2[ic]) * hf / h2);
+    RmgType t2z ((c1*w1[ic+2] - c2*w2[ic+1]) * hf / h2);
+    RmgType t3z ((c1*w1[ic+3] - c2*w2[ic+2]) * hf / h2);
+    RmgType t4z (c1*w1[ic+4]*hf / h2);
 
     RmgType t0 (th2);
 
@@ -1886,31 +1898,44 @@ double FiniteDiff::app10_del2(RmgType * a, RmgType * b, int dimx, int dimy, int 
     ix1 = dimy * dimz;
     iy1 = dimz;
 
+    // nine and seven point stencils, 2nd derivative, extrapolated
+    int ic = 5;
+    double x[11], w1[11], w2[11];
+    for(int i=0;i<11;i++) x[i] = (double)i;
+    gen_weights(11, 2, (double)ic, x, w1);
+    gen_weights(9, 2, (double)(ic-1), x, w2);
+    double hf = 1.0, c1, c2, beta = 3.0;
+    if(ibrav == HEXAGONAL) hf = 2.0/3.0;
     double h2 = gridhx * gridhx * L->get_xside() * L->get_xside();
-    double th2 (-2.927222222222221948 / (h2));
-    RmgType t1x (5.0 / ( 3.0 * h2));
-    RmgType t2x (-0.238095238095238138 / (h2));
-    RmgType t3x (0.039682539682539694 / (h2));
-    RmgType t4x (-0.004960317460317462 / (h2));
-    RmgType t5x (0.000317460317460317 / (h2));
+    c1 = 1.0 + beta;
+    c2 = beta;
+    double th2 = (c1*w1[ic] - c2*w2[ic-1]) / h2;
+    RmgType t1x ((c1*w1[ic+1] - c2*w2[ic]) * hf / h2);
+    RmgType t2x ((c1*w1[ic+2] - c2*w2[ic+1]) * hf / h2);
+    RmgType t3x ((c1*w1[ic+3] - c2*w2[ic+2]) * hf / h2);
+    RmgType t4x ((c1*w1[ic+4] - c2*w2[ic+3])*hf / h2);
+    RmgType t5x (c1*w1[ic+5]*hf / h2);
 
     h2 = gridhy * gridhy * L->get_yside() * L->get_yside();
-    th2 -= (2.927222222222221948 / (h2));
-    RmgType t1y (5.0 / ( 3.0 * h2));
-    RmgType t2y (-0.238095238095238138 / (h2));
-    RmgType t3y (0.039682539682539694 / (h2));
-    RmgType t4y (-0.004960317460317462 / (h2));
-    RmgType t5y (0.000317460317460317 / (h2));
+    c1 = 1.0 + beta;
+    c2 = beta;
+    th2 += (c1*w1[ic] - c2*w2[ic-1]) /  h2;
+    RmgType t1y ((c1*w1[ic+1] - c2*w2[ic]) * hf / h2);
+    RmgType t2y ((c1*w1[ic+2] - c2*w2[ic+1]) * hf / h2);
+    RmgType t3y ((c1*w1[ic+3] - c2*w2[ic+2]) * hf / h2);
+    RmgType t4y ((c1*w1[ic+4] - c2*w2[ic+3])*hf / h2);
+    RmgType t5y (c1*w1[ic+5]*hf / h2);
 
     h2 = gridhz * gridhz * L->get_zside() * L->get_zside();
-    th2 -= (2.927222222222221948 / (h2));
-    RmgType t1z (5.0 / ( 3.0 * h2));
-    RmgType t2z (-0.238095238095238138 / (h2));
-    RmgType t3z (0.039682539682539694 / (h2));
-    RmgType t4z (-0.004960317460317462 / (h2));
-    RmgType t5z (0.000317460317460317 / (h2));
+    c1 = 1.0 + beta;
+    c2 = beta;
+    th2 += (c1*w1[ic] - c2*w2[ic-1]) /  h2;
+    RmgType t1z ((c1*w1[ic+1] - c2*w2[ic]) * hf / h2);
+    RmgType t2z ((c1*w1[ic+2] - c2*w2[ic+1]) * hf / h2);
+    RmgType t3z ((c1*w1[ic+3] - c2*w2[ic+2]) * hf / h2);
+    RmgType t4z ((c1*w1[ic+4] - c2*w2[ic+3])*hf / h2);
+    RmgType t5z (c1*w1[ic+5]*hf / h2);
     RmgType t0 (th2);
-
 
 
     for (ix = 5; ix < dimx + 5; ix++)
@@ -2013,7 +2038,6 @@ double FiniteDiff::app12_del2(RmgType * a, RmgType * b, int dimx, int dimy, int 
     RmgType t5z (0.001038961038961037 / (h2));
     RmgType t6z (-0.000060125060125060 / (h2));
     RmgType t0 (th2);
-
 
 
     for (ix = 6; ix < dimx + 6; ix++)

Input/AutoSet.cpp
@@ -159,7 +159,7 @@ void AutoSet(CONTROL& lc, PE_CONTROL& pelc, std::unordered_map<std::string, Inpu
     }
 
     // Cutoff parameter -- do we want separate settings for each of these?
-    lc.rhocparm = lc.cparm;
+    lc.rhocparm = 1.0;
     ct.betacparm = ct.cparm * (ct.nxfgrid - 1);
 
 

Misc/GetTe.cpp
@@ -94,8 +94,8 @@ void GetTe (double * rho, double * rho_oppo, double * rhocore, double * rhoc, do
 
     // If the coarse/fine grid ratio is not equal to 1 then we filter to be consistent
     // with what is done in the scf loop.
-    if(ct.filter_dpot && (ratio > 1)) FftFilter(vh, *fine_pwaves, sqrt(ct.filter_factor) / (double)ct.FG_RATIO, LOW_PASS);
-    if(ct.filter_dpot && (ratio > 1)) FftFilter(vxc, *fine_pwaves, sqrt(ct.filter_factor) / (double)ct.FG_RATIO, LOW_PASS);
+//    if(ct.filter_dpot && (ratio > 1)) FftFilter(vh, *fine_pwaves, sqrt(ct.filter_factor) / (double)ct.FG_RATIO, LOW_PASS);
+//    if(ct.filter_dpot && (ratio > 1)) FftFilter(vxc, *fine_pwaves, sqrt(ct.filter_factor) / (double)ct.FG_RATIO, LOW_PASS);
 
 
     vel = get_vel_f();

Misc/Pw.cpp
@@ -90,9 +90,9 @@ Pw::Pw (BaseGrid &G, Lattice &L, int ratio, bool gamma_flag, MPI_Comm comm)
     {
         if(ct.norm_conserving_pp)
         {
-              ivx = (this->global_dimx - 1) / 3 - 1;
-              ivy = (this->global_dimy - 1) / 3 - 1;
-              ivz = (this->global_dimz - 1) / 3 - 1;
+              ivx = (this->global_dimx - 1) / 3;
+              ivy = (this->global_dimy - 1) / 3;
+              ivz = (this->global_dimz - 1) / 3;
         }
         // save coarse grid values
         civx = ivx;
@@ -104,9 +104,9 @@ Pw::Pw (BaseGrid &G, Lattice &L, int ratio, bool gamma_flag, MPI_Comm comm)
   {
       // Make sure max g-vector on fine grid is an integral multiple
       // of coarse grid.
-      ivx = ratio*civx;
-      ivy = ratio*civy;
-      ivz = ratio*civz;
+      ivx = ratio*civx + ratio % 2;
+      ivy = ratio*civy + ratio % 2;
+      ivz = ratio*civz + ratio % 2;
   }
 
   gvec[0] = (double)ivx * L.b0[0] + (double)ivy * L.b1[0] + (double)ivz * L.b2[0];

RMG/Common/Scf.cpp
@@ -109,6 +109,8 @@ template <typename OrbitalType> bool Scf (double * vxc, double *vxc_in, double *
     RT1 = new RmgTimer("2-Scf steps: exchange/correlation");
     Functional *F = new Functional ( *Rmg_G, Rmg_L, *Rmg_T, ct.is_gamma);
     F->v_xc(rho, rhocore, ct.XC, ct.vtxc, vxc, ct.spin_flag );
+    if(ct.filter_dpot && (Rmg_G->default_FG_RATIO > 1)) 
+        FftFilter(vxc, *fine_pwaves, sqrt(ct.filter_factor) / (double)ct.FG_RATIO, LOW_PASS);
     //if(pct.gridpe==0)printf("\nXC = %f  %f\n", ct.XC, ct.vtxc);
     delete F;
     delete RT1;
@@ -210,7 +212,6 @@ template <typename OrbitalType> bool Scf (double * vxc, double *vxc_in, double *
     GetVtotPsi (vtot_psi, vtot, Rmg_G->default_FG_RATIO);
 
     /*Generate the Dnm_I */
-    if(ct.filter_dpot && (Rmg_G->default_FG_RATIO > 1)) FftFilter(vtot, *fine_pwaves, sqrt(ct.filter_factor) / (double)ct.FG_RATIO, LOW_PASS);
     get_ddd (vtot);
 
 

commit 20bf6deb388127e90ec1ab57f8242b31aa9f241f

Fix a bug in hexagonal symmetry that was introduced in r4518.
git-svn-id: svn+ssh://chips.ncsu.edu/home/svn/repos/codesv3@4707 eb38b3b0-33ea-0310-94ad-d43fea0d214f

RmgLib/src/FiniteDiff.cpp
@@ -1723,25 +1723,25 @@ double FiniteDiff::app8_del2(RmgType * __restrict__ a, RmgType * __restrict__ b,
     RmgType t1x ((c1*w1[ic+1] - c2*w2[ic]) * hf / h2x);
     RmgType t2x ((c1*w1[ic+2] - c2*w2[ic+1]) * hf / h2x);
     RmgType t3x ((c1*w1[ic+3] - c2*w2[ic+2]) * hf / h2x);
-    RmgType t4x (c1*w1[ic+4]*hf / h2x);
+    RmgType t4x (c1*w1[ic+4] * hf / h2x);
 
     hadj = sqrt(h2y / maxh);
     if(this->alt_laplacian) c2 = -1.0/(1.0+dr*hadj/k2);
     c1 = 1.0 + c2;
     th2 += (c1*w1[ic] - c2*w2[ic-1]) /  h2y;
-    RmgType t1y ((c1*w1[ic+1] - c2*w2[ic]) * hf / h2y);
-    RmgType t2y ((c1*w1[ic+2] - c2*w2[ic+1]) * hf / h2y);
-    RmgType t3y ((c1*w1[ic+3] - c2*w2[ic+2]) * hf / h2y);
-    RmgType t4y (c1*w1[ic+4]*hf / h2y);
+    RmgType t1y ((c1*w1[ic+1] - c2*w2[ic]) / h2y);
+    RmgType t2y ((c1*w1[ic+2] - c2*w2[ic+1]) / h2y);
+    RmgType t3y ((c1*w1[ic+3] - c2*w2[ic+2]) / h2y);
+    RmgType t4y (c1*w1[ic+4] / h2y);
 
     hadj = sqrt(h2z / maxh);
     if(this->alt_laplacian) c2 = -1.0/(1.0+dr*hadj/k2);
     c1 = 1.0 + c2;
     th2 += (c1*w1[ic] - c2*w2[ic-1]) /  h2z;
-    RmgType t1z ((c1*w1[ic+1] - c2*w2[ic]) * hf / h2z);
-    RmgType t2z ((c1*w1[ic+2] - c2*w2[ic+1]) * hf / h2z);
-    RmgType t3z ((c1*w1[ic+3] - c2*w2[ic+2]) * hf / h2z);
-    RmgType t4z (c1*w1[ic+4]*hf / h2z);
+    RmgType t1z ((c1*w1[ic+1] - c2*w2[ic]) / h2z);
+    RmgType t2z ((c1*w1[ic+2] - c2*w2[ic+1]) / h2z);
+    RmgType t3z ((c1*w1[ic+3] - c2*w2[ic+2]) / h2z);
+    RmgType t4z (c1*w1[ic+4] / h2z);
 
     RmgType t0 (th2);
 
@@ -1897,28 +1897,28 @@ double FiniteDiff::app10_del2(RmgType * a, RmgType * b, int dimx, int dimy, int 
     RmgType t1x ((c1*w1[ic+1] - c2*w2[ic]) * hf / h2x);
     RmgType t2x ((c1*w1[ic+2] - c2*w2[ic+1]) * hf / h2x);
     RmgType t3x ((c1*w1[ic+3] - c2*w2[ic+2]) * hf / h2x);
-    RmgType t4x ((c1*w1[ic+4] - c2*w2[ic+3])*hf / h2x);
-    RmgType t5x (c1*w1[ic+5]*hf / h2x);
+    RmgType t4x ((c1*w1[ic+4] - c2*w2[ic+3]) * hf / h2x);
+    RmgType t5x (c1*w1[ic+5] * hf / h2x);
 
     hadj = sqrt(h2y / maxh);
     if(this->alt_laplacian) c2 = -1.0/(1.0+dr*hadj/k2);
     c1 = 1.0 + c2;
     th2 += (c1*w1[ic] - c2*w2[ic-1]) /  h2y;
-    RmgType t1y ((c1*w1[ic+1] - c2*w2[ic]) * hf / h2y);
-    RmgType t2y ((c1*w1[ic+2] - c2*w2[ic+1]) * hf / h2y);
-    RmgType t3y ((c1*w1[ic+3] - c2*w2[ic+2]) * hf / h2y);
-    RmgType t4y ((c1*w1[ic+4] - c2*w2[ic+3])*hf / h2y);
-    RmgType t5y (c1*w1[ic+5]*hf / h2y);
+    RmgType t1y ((c1*w1[ic+1] - c2*w2[ic]) / h2y);
+    RmgType t2y ((c1*w1[ic+2] - c2*w2[ic+1]) / h2y);
+    RmgType t3y ((c1*w1[ic+3] - c2*w2[ic+2]) / h2y);
+    RmgType t4y ((c1*w1[ic+4] - c2*w2[ic+3]) / h2y);
+    RmgType t5y (c1*w1[ic+5] / h2y);
 
     hadj = sqrt(h2z / maxh);
     if(this->alt_laplacian) c2 = -1.0/(1.0+dr*hadj/k2);
     c1 = 1.0 + c2;
     th2 += (c1*w1[ic] - c2*w2[ic-1]) /  h2z;
-    RmgType t1z ((c1*w1[ic+1] - c2*w2[ic]) * hf / h2z);
-    RmgType t2z ((c1*w1[ic+2] - c2*w2[ic+1]) * hf / h2z);
-    RmgType t3z ((c1*w1[ic+3] - c2*w2[ic+2]) * hf / h2z);
-    RmgType t4z ((c1*w1[ic+4] - c2*w2[ic+3])*hf / h2z);
-    RmgType t5z (c1*w1[ic+5]*hf / h2z);
+    RmgType t1z ((c1*w1[ic+1] - c2*w2[ic]) / h2z);
+    RmgType t2z ((c1*w1[ic+2] - c2*w2[ic+1]) / h2z);
+    RmgType t3z ((c1*w1[ic+3] - c2*w2[ic+2]) / h2z);
+    RmgType t4z ((c1*w1[ic+4] - c2*w2[ic+3]) / h2z);
+    RmgType t5z (c1*w1[ic+5] / h2z);
     RmgType t0 (th2);
 
 