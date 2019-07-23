fd0caed940a05ca30098dce27fe94d4de9fda8b0
Emil Briggs <290825+elbriggs@users.noreply.github.com>
2019-07-23 17:45:21 -0400

    Initial work on RMG to QMC converter. This patch takes the experimental QBOX converter from QMCPACK and sets it up to be linked against the RMG classes that are required to read and work with RMG data.

---
+++ Interfaces/RmgToQMC.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sys/stat.h>
+#include "XmlRep.h"
+#include "WriteEshdf.h"
+
+// RMG includes
+#include "const.h"
+#include "RmgTimer.h"
+#include "RmgException.h"
+#include "rmgtypedefs.h"
+#include "params.h"
+#include "typedefs.h"
+#include "rmg_error.h"
+#include "transition.h"
+#include "Kpoint.h"
+#include "InputKey.h"
+
+using namespace std;
+
+void convertToVecStrings(int argc, char* argv[], std::vector<std::string>& vec) {
+  for (int i = 1; i < argc; i++) {
+    vec.push_back(argv[i]);
+  }
+}
+
+inline bool file_exists (const std::string& name) {
+  struct stat buffer;   
+  return (stat (name.c_str(), &buffer) == 0); 
+}
+
+int main(int argc, char* argv[]) {
+  vector<string> vecParams;
+  convertToVecStrings(argc, argv, vecParams);
+  string fname;
+  for (int i = 0; i < vecParams.size(); i++) {
+    // in principle check for other flags here
+    fname = vecParams[i];
+  }
+  if (file_exists(fname) == false) {
+    cout << "must specify a valid rmg input file name as an argument" << endl;
+    exit(1);
+  }
+
+  ifstream is(fname.c_str());
+  xmlNode qboxSampleXml(&is, 0, true);
+
+
+  string ofname = "qboxEshdf.h5";
+  eshdfFile outFile(ofname);
+  outFile.writeBoilerPlate("qbox", qboxSampleXml);
+  outFile.writeSupercell(qboxSampleXml);
+  outFile.writeAtoms(qboxSampleXml);
+  outFile.writeElectrons(qboxSampleXml);
+}
