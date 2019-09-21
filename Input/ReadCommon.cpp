eb8a7387162999dc38a168e6857f94de36cbd066
WenchangLu <wlu2@ncsu.edu>
2019-09-21 13:52:34 -0400

    add a function in Exxbase to write all wave functions into a sinlge file

--- Input/ReadCommon.cpp
+++ Input/ReadCommon.cpp
@@ -201,7 +201,7 @@ void ReadCommon(char *cfile, CONTROL& lc, PE_CONTROL& pelc, std::unordered_map<s
                      "File/path for runtime disk storage of orbitals. ", 
                      "", CONTROL_OPTIONS);
 
-    If.RegisterInputKey("exx_integrals_filepath", &ExxIntfile, "",
+    If.RegisterInputKey("exx_integrals_filepath", &ExxIntfile, "ExxIntegrals",
                      CHECK_AND_FIX, OPTIONAL,
                      "File/path for exact exchange integrals. ", 
                      "", CONTROL_OPTIONS);
--- XC/Exxbase.cpp
+++ XC/Exxbase.cpp
@@ -63,139 +63,37 @@ template <class T> Exxbase<T>::Exxbase (
     alpha = L.get_omega() / ((double)(G.get_NX_GRID(1) * G.get_NY_GRID(1) * G.get_NZ_GRID(1)));
     mode = EXX_DIST_FFT;
     pbasis = G.get_P0_BASIS(1);
-    N = (size_t)G.get_NX_GRID(G.default_FG_RATIO) *
-        (size_t)G.get_NY_GRID(G.default_FG_RATIO) *
-        (size_t)G.get_NZ_GRID(G.default_FG_RATIO);
 
-    int ratio = G.default_FG_RATIO;
+    if(mode == EXX_DIST_FFT) 
+    {
+        pwave = coarse_pwaves;
+    }
+    else
+    {
+        LG = new BaseGrid(G.get_NX_GRID(1), G.get_NY_GRID(1), G.get_NZ_GRID(1), 1, 1, 1, 0, 1);
+        int rank = G.get_rank();
+        MPI_Comm_split(G.comm, rank+1, rank, &lcomm);
+        LG->set_rank(0, lcomm);
+        pwave = new Pw(*LG, L, 1, false);
+    }
 
     setup_gfac();
 
     if(mode == EXX_DIST_FFT) return;
 
-    // Write the domain distributed wavefunction array and map it to psi_s
-    MPI_Datatype wftype = MPI_DOUBLE;
-    if(typeid(T) == typeid(std::complex<double>)) wftype = MPI_DOUBLE_COMPLEX;
-
-    int sizes_c[3], sizes_f[3];
-    int subsizes_c[3], subsizes_f[3];
-    int starts_c[3], starts_f[3];
-
-    sizes_c[0] = G.get_NX_GRID(1);
-    sizes_c[1] = G.get_NY_GRID(1);
-    sizes_c[2] = G.get_NZ_GRID(1);
-
-    sizes_f[0] = G.get_NX_GRID(ratio);
-    sizes_f[1] = G.get_NY_GRID(ratio);
-    sizes_f[2] = G.get_NZ_GRID(ratio);
-
-    subsizes_c[0] = G.get_PX0_GRID(1);
-    subsizes_c[1] = G.get_PY0_GRID(1);
-    subsizes_c[2] = G.get_PZ0_GRID(1);
-
-    subsizes_f[0] = G.get_PX0_GRID(ratio);
-    subsizes_f[1] = G.get_PY0_GRID(ratio);
-    subsizes_f[2] = G.get_PZ0_GRID(ratio);
-
-    starts_c[0] = G.get_PX_OFFSET(1);
-    starts_c[1] = G.get_PY_OFFSET(1);
-    starts_c[2] = G.get_PZ_OFFSET(1);
-
-    starts_f[0] = G.get_PX_OFFSET(ratio);
-    starts_f[1] = G.get_PY_OFFSET(ratio);
-    starts_f[2] = G.get_PZ_OFFSET(ratio);
-
-    int order = MPI_ORDER_C;
-    MPI_Info fileinfo;
-    MPI_Datatype grid_c, grid_f;
-    MPI_Status status;
-
-
-    MPI_Type_create_subarray(3, sizes_c, subsizes_c, starts_c, order, wftype, &grid_c);
-    MPI_Type_commit(&grid_c);
-
-    MPI_Type_create_subarray(3, sizes_f, subsizes_f, starts_f, order, MPI_DOUBLE, &grid_f);
-    MPI_Type_commit(&grid_f);
-
-    MPI_Info_create(&fileinfo);
-
-    int amode = MPI_MODE_RDWR|MPI_MODE_CREATE;
-    MPI_File mpi_fhand ;
-
-    MPI_Barrier(G.comm);
-
-    MPI_File_open(G.comm, wavefile.c_str(), amode, fileinfo, &mpi_fhand);
-    MPI_Offset disp = 0;
-    T *wfptr = psi;
-
-    MPI_File_set_view(mpi_fhand, disp, wftype, grid_c, "native", MPI_INFO_NULL);
-    for(int st=0;st < nstates;st++)
-    {
-        MPI_File_write_all(mpi_fhand, wfptr, pbasis, MPI_DOUBLE, &status);
-        wfptr += pbasis;
-    }
-    MPI_Barrier(G.comm);
-    MPI_File_close(&mpi_fhand);
-    fflush(NULL);
-    MPI_Barrier(G.comm);
 
     serial_fd = open(wavefile.c_str(), O_RDONLY, (mode_t)0600);
     if(serial_fd < 0)
         throw RmgFatalException() << "Error! Could not open " << wavefile << " . Terminating.\n";
 
-    size_t length = nstates * N;
+    size_t length = nstates * G.get_NX_GRID(1) * G.get_NY_GRID(1) * G.get_NZ_GRID(1);
     psi_s = (T *)mmap(NULL, length, PROT_READ, MAP_PRIVATE, serial_fd, 0);
     MPI_Barrier(G.comm);
 
-    MPI_Type_free(&grid_f);
-    MPI_Type_free(&grid_c);
 
     // Generate the full set of pairs and store in a temporary vector
     // This is for gamma only right now
-    int npairs = (N*N + N) / 2;
-    std::vector< std::pair <int,int> > temp_pairs; 
-    temp_pairs.resize(npairs);
-    for(int i=0;i < npairs;i++)
-    {
-        for(int j=i;j < npairs;j++)
-        {
-            temp_pairs.push_back(std::make_pair(i, j));
-        }
-    }
 
-    // Compute start and count for this MPI task
-    pair_start = 0;
-    pair_count = npairs;
-    int rank = G.get_rank();
-    int NPES = G.get_NPES(); 
-    if(NPES > 1)
-    {
-        pair_count = npairs / NPES;
-        pair_start = pair_count * rank;
-        int rem = pair_count % NPES;
-        if(rank < rem)
-        {
-            pair_count++;
-            pair_start += rank;
-        }
-        else
-        {
-            pair_start += rem;
-        }
-
-    }
-
-    // Copy the ones we are responsible for into our local vector of pairs
-    pairs.resize(pair_count);
-    for(int st=0;st < pair_count;st++)
-    {
-        pairs[st] = temp_pairs[pair_start + st];
-    }
-
-    LG = new BaseGrid(G.get_NX_GRID(1), G.get_NY_GRID(1), G.get_NZ_GRID(1), 1, 1, 1, 0, 1);
-    MPI_Comm_split(G.comm, rank+1, rank, &lcomm);
-    LG->set_rank(0, lcomm);
-    pwave = new Pw(*LG, L, 1, false);
 
     // Now we have to figure out how to distribute the pairs over computational resources.
     // If we only have CPU's then it's easy
@@ -205,9 +103,9 @@ template <> void Exxbase<double>::fftpair(double *psi_i, double *psi_j, std::com
 {
     std::complex<double> ZERO_t(0.0, 0.0);
     for(int idx=0;idx < pbasis;idx++) p[idx] = std::complex<double>(psi_i[idx] * psi_j[idx], 0.0);
-    coarse_pwaves->FftForward(p, p);
+    pwave->FftForward(p, p);
     for(int ig=0;ig < pbasis;ig++) p[ig] *= gfac[ig];
-    coarse_pwaves->FftInverse(p, p);
+    pwave->FftInverse(p, p);
 }
 
 template <> void Exxbase<std::complex<double>>::fftpair(std::complex<double> *psi_i, std::complex<double> *psi_j, std::complex<double> *p)
@@ -229,8 +127,8 @@ template <> void Exxbase<double>::setup_gfac(void)
         double a0 = pow(PI / gau_scrlen, 1.5);
         for(int ig=0;ig < pbasis;ig++)
         {
-            if(coarse_pwaves->gmask[ig])
-                gfac[ig] = a0 * exp(-tpiba2*coarse_pwaves->gmags[ig] / 4.0 / gau_scrlen);
+            if(pwave->gmask[ig])
+                gfac[ig] = a0 * exp(-tpiba2*pwave->gmags[ig] / 4.0 / gau_scrlen);
             else
                 gfac[ig] = 0.0;
         }
@@ -240,8 +138,8 @@ template <> void Exxbase<double>::setup_gfac(void)
         double a0 = 4.0 * PI;
         for(int ig=0;ig < pbasis;ig++)
         {
-            if((coarse_pwaves->gmags[ig] > 1.0e-6) && coarse_pwaves->gmask[ig])
-                gfac[ig] = a0 * exp(-tpiba2*coarse_pwaves->gmags[ig] / 4.0 / (scrlen*scrlen)) / (tpiba2*coarse_pwaves->gmags[ig]);
+            if((pwave->gmags[ig] > 1.0e-6) && pwave->gmask[ig])
+                gfac[ig] = a0 * exp(-tpiba2*pwave->gmags[ig] / 4.0 / (scrlen*scrlen)) / (tpiba2*pwave->gmags[ig]);
             else
                 gfac[ig] = 0.0;
         }
@@ -251,8 +149,8 @@ template <> void Exxbase<double>::setup_gfac(void)
         double a0 = 4.0 * PI;
         for(int ig=0;ig < pbasis;ig++)
         {
-            if((coarse_pwaves->gmags[ig] > 1.0e-6) && coarse_pwaves->gmask[ig])
-                gfac[ig] = a0 * (1.0 - exp(-tpiba2*coarse_pwaves->gmags[ig] / 4.0 / (scrlen*scrlen))) / (tpiba2*coarse_pwaves->gmags[ig]);
+            if((pwave->gmags[ig] > 1.0e-6) && pwave->gmask[ig])
+                gfac[ig] = a0 * (1.0 - exp(-tpiba2*pwave->gmags[ig] / 4.0 / (scrlen*scrlen))) / (tpiba2*pwave->gmags[ig]);
             else
                 gfac[ig] = 0.0;
         }
@@ -261,8 +159,8 @@ template <> void Exxbase<double>::setup_gfac(void)
     {
         for(int ig=0;ig < pbasis;ig++)
         {
-            if((coarse_pwaves->gmags[ig] > 1.0e-6) && coarse_pwaves->gmask[ig])
-                gfac[ig] = 1.0/(coarse_pwaves->gmags[ig] *tpiba2);
+            if((pwave->gmags[ig] > 1.0e-6) && pwave->gmask[ig])
+                gfac[ig] = 1.0/(pwave->gmags[ig] *tpiba2);
             else
                 gfac[ig] = 0.0;
         }
@@ -279,7 +177,7 @@ template <> void Exxbase<std::complex<double>>::setup_gfac(void)
 template <> void Exxbase<double>::Vexx(double *vexx)
 {
     RmgTimer RT0("5-Functional: Exx potential");
-    double scale = - 1.0 / (double)coarse_pwaves->global_basis;
+    double scale = - 1.0 / (double)pwave->global_basis;
 
     double ZERO_t(0.0), ONE_T(1.0);
     char *trans_a = "t";
@@ -386,7 +284,7 @@ template <> void Exxbase<double>::Vexx_integrals_block(FILE *fp,  int ij_start, 
 template <> void Exxbase<double>::Vexx_integrals(std::string &vfile)
 {
 
-    double scale = 1.0 / (double)coarse_pwaves->global_basis;
+    double scale = 1.0 / (double)pwave->global_basis;
     int nstates_occ = 0;
     for(int st=0;st < nstates;st++) if(occ[st] > 1.0e-6) nstates_occ++;
 
@@ -424,17 +322,20 @@ template <> void Exxbase<double>::Vexx_integrals(std::string &vfile)
         {
             size_t size = 1<<18;
             buf = new char[size];
-            std::string filename = vfile + "_spin"+std::to_string(pct.spinpe)+".fcidump";
+            std::string filename = vfile + "_spin"+std::to_string(pct.spinpe)+".fcidump_"+std::to_string(pct.gridpe);
             fp= fopen(filename.c_str(), "w");
             if(int nn = setvbuf (fp, buf, _IOFBF, size) != 0) printf("\n failed buff %d\n", nn);
 
-            fprintf(fp, "&FCI\n");
-            fprintf(fp, "NORB=%d,\n", nstates_occ);
-            fprintf(fp, "NELEC=%d,\n", (int)(ct.nel+1.0e-6));
-            int ms2 = pct.spinpe;
-            if(ct.spin_flag && pct.spinpe == 0) ms2 = -1;
-            fprintf(fp, "MS2=%d,\n", ms2);
-            fprintf(fp, "\n&END\n");
+            if(pct.gridpe == 0)
+            {
+                fprintf(fp, "&FCI\n");
+                fprintf(fp, "NORB=%d,\n", nstates_occ);
+                fprintf(fp, "NELEC=%d,\n", (int)(ct.nel+1.0e-6));
+                int ms2 = pct.spinpe;
+                if(ct.spin_flag && pct.spinpe == 0) ms2 = -1;
+                fprintf(fp, "MS2=%d,\n", ms2);
+                fprintf(fp, "\n&END\n");
+            }
         }
 
         for(int ib =0; ib < num_blocks; ib++)
@@ -508,5 +409,59 @@ template <class T> Exxbase<T>::~Exxbase(void)
     delete LG;
     MPI_Comm_free(&lcomm); 
     delete pwave;
+}
+
+template <class T> void Exxbase<T>::WriteWfsToSingleFile()
+{
+    // Write the domain distributed wavefunction array and map it to psi_s
+    MPI_Datatype wftype = MPI_DOUBLE;
+    if(typeid(T) == typeid(std::complex<double>)) wftype = MPI_DOUBLE_COMPLEX;
+
+    int sizes_c[3], sizes_f[3];
+    int subsizes_c[3], subsizes_f[3];
+    int starts_c[3], starts_f[3];
+
+    sizes_c[0] = G.get_NX_GRID(1);
+    sizes_c[1] = G.get_NY_GRID(1);
+    sizes_c[2] = G.get_NZ_GRID(1);
+
+    subsizes_c[0] = G.get_PX0_GRID(1);
+    subsizes_c[1] = G.get_PY0_GRID(1);
+    subsizes_c[2] = G.get_PZ0_GRID(1);
+
+    starts_c[0] = G.get_PX_OFFSET(1);
+    starts_c[1] = G.get_PY_OFFSET(1);
+    starts_c[2] = G.get_PZ_OFFSET(1);
+
+    int order = MPI_ORDER_C;
+    MPI_Info fileinfo;
+    MPI_Datatype grid_c;
+    MPI_Status status;
+
+    MPI_Type_create_subarray(3, sizes_c, subsizes_c, starts_c, order, wftype, &grid_c);
+    MPI_Type_commit(&grid_c);
+
+    MPI_Info_create(&fileinfo);
+
+    int amode = MPI_MODE_RDWR|MPI_MODE_CREATE;
+    MPI_File mpi_fhand ;
+
+    MPI_Barrier(G.comm);
+
+    MPI_File_open(G.comm, wavefile.c_str(), amode, fileinfo, &mpi_fhand);
+    MPI_Offset disp = 0;
+    T *wfptr = psi;
+
+    MPI_File_set_view(mpi_fhand, disp, wftype, grid_c, "native", MPI_INFO_NULL);
+    for(int st=0;st < nstates;st++)
+    {
+        MPI_File_write_all(mpi_fhand, wfptr, pbasis, MPI_DOUBLE, &status);
+        wfptr += pbasis;
+    }
+    MPI_Barrier(G.comm);
+    MPI_File_close(&mpi_fhand);
+    MPI_Type_free(&grid_c);
+    fflush(NULL);
+    MPI_Barrier(G.comm);
 }
 
