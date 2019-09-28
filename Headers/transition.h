8b34f8d39194c45ebfb3a9ace7685996ad880c69
Emil Briggs <290825+elbriggs@users.noreply.github.com>
2019-09-28 06:46:41 -0400

    Backing out last set of changes for now since it seems to make more sense to do things a bit differently.

--- Headers/transition.h
+++ Headers/transition.h
@@ -166,7 +166,7 @@ extern "C" int FilenameIncrement(char *pathname);
 int CountAtomicOrbitals(void);
 void InitPseudo (std::unordered_map<std::string, InputKey *>& ControlMap);
 void InitQfunct (std::unordered_map<std::string, InputKey *>& ControlMap);
-void FindPhase (SPECIES *sp, int nlxdim, int nlydim, int nlzdim, double * nlcdrs, std::complex<double> *phase);
+void FindPhase (int nlxdim, int nlydim, int nlzdim, double * nlcdrs, std::complex<double> *phase);
 void FindPhaseKpoint (double *kvec, int nlxdim, int nlydim, int nlzdim, double * nlcdrs, std::complex<double>* phase_fftw, bool localize);
 void FindNlcrds (double *xtal, double *nlcrds,
                  int nlxdim, int nlydim, int nlzdim,
