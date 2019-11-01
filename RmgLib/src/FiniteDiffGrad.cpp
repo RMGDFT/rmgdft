d164933084f3901fb9b1ad4a713f50f8f9ae060a
Emil Briggs <290825+elbriggs@users.noreply.github.com>
2019-11-01 07:09:59 -0400

    Workaround to enable vectorization of complex data. Roughly doubles performance.

--- RmgLib/src/FiniteDiffGrad.cpp
+++ RmgLib/src/FiniteDiffGrad.cpp
@@ -19,90 +19,279 @@ void FiniteDiffGrad(T * __restrict__ a, T * __restrict__ gx, T * __restrict__ gy
     int dim[3];
     LC->GetDim(dim);
 
-    //recalculate relative index
+    // recalculate relative index
     if(dim[0] != dimx || dim[1] != dimy || dim[2] != dimz) 
     {
         std::cout << "dimxyz wanted : "<< dimx << "  " <<dimy << "  " <<dimz<< std::endl;
         std::cout << "dimxyz shouldbe: "<< dim[0] << "  " <<dim[1] << "  " <<dim[2]<< std::endl;
-
         rmg_error_handler(__FILE__, __LINE__, "Generalized finite diffenerence dims not match ");
     }
 
 
-    for(int idx = 0; idx < dim[0] * dim[1] * dim[2]; idx++) gx[idx] = 0.0;
-    for(auto coeff:LC->gx_coeff_and_index)
+    // This code may seem unduly complicated but for the complex case it is almost twice as fast
+    // as the simple templated version. I think the issue is poor vectorization of complex data
+    // types but they may be treated as a double length array of floats or doubles instead which
+    // does vectorize well.
+    if(typeid(T) == typeid(std::complex<double>))
     {
-
-        for (int ix = 0; ix < dim[0]; ix++)
+        double *aad = (double *)a;
+        double *gxd = (double *)gx;
+        for(int idx = 0; idx < 2 * dim[0] * dim[1] * dim[2]; idx++) gxd[idx] = 0.0;
+        for(auto coeff:LC->gx_coeff_and_index)
         {
-            int ixs = (ix+Lorder/2) * (dim[1] + Lorder) * (dim[2] + Lorder);
-            int ixc = ix * dim[1] * dim[2];
 
-            for (int iy = 0; iy < dim[1]; iy++)
+            for (int ix = 0; ix < dim[0]; ix++)
             {
-                int iys = (iy+Lorder/2) * (dim[2] + Lorder) + ixs + Lorder/2;
-                int iyc = iy * dim[2] + ixc;
+                int ixs = (ix+Lorder/2) * (dim[1] + Lorder) * (dim[2] + Lorder);
+                int ixc = ix * dim[1] * dim[2];
 
-                for(auto index: coeff.relative_index)
-             //      daxpy(&dim[2], &coeff.coeff, &a[iys+index], &ione, &b[iyc], &ione);
-                    for (int iz = 0; iz < dim[2]; iz++)
-                    {
-                        int idxs = iys + iz + index;
-                       int idxc = iyc + iz;
-                        gx[idxc]+= a[idxs] * (T)coeff.coeff;
-                    }
+                for (int iy = 0; iy < dim[1]; iy++)
+                {
+                    int iys = (iy+Lorder/2) * (dim[2] + Lorder) + ixs + Lorder/2;
+                    int iyc = iy * dim[2] + ixc;
+
+                    for(auto index: coeff.relative_index)
+                        for (int iz = 0; iz < dim[2]; iz++)
+                        {
+                            int idxs = iys + iz + index;
+                            int idxc = iyc + iz;
+                            gxd[2*idxc]+= aad[2*idxs] * (double)coeff.coeff;
+                            gxd[2*idxc+1]+= aad[2*idxs+1] * (double)coeff.coeff;
+                        }
+                }
+            }
+        }
+    }
+    else if(typeid(T) == typeid(std::complex<float>))
+    {
+        float *aad = (float *)a;
+        float *gxd = (float *)gx;
+        for(int idx = 0; idx < 2 * dim[0] * dim[1] * dim[2]; idx++) gxd[idx] = 0.0;
+        for(auto coeff:LC->gx_coeff_and_index)
+        {
+
+            for (int ix = 0; ix < dim[0]; ix++)
+            {
+                int ixs = (ix+Lorder/2) * (dim[1] + Lorder) * (dim[2] + Lorder);
+                int ixc = ix * dim[1] * dim[2];
+
+                for (int iy = 0; iy < dim[1]; iy++)
+                {
+                    int iys = (iy+Lorder/2) * (dim[2] + Lorder) + ixs + Lorder/2;
+                    int iyc = iy * dim[2] + ixc;
+
+                    for(auto index: coeff.relative_index)
+                        for (int iz = 0; iz < dim[2]; iz++)
+                        {
+                            int idxs = iys + iz + index;
+                            int idxc = iyc + iz;
+                            gxd[2*idxc]+= aad[2*idxs] * (float)coeff.coeff;
+                            gxd[2*idxc+1]+= aad[2*idxs+1] * (float)coeff.coeff;
+                        }
+                }
+            }
+        }
+    }
+    else
+    {
+        for(int idx = 0; idx < dim[0] * dim[1] * dim[2]; idx++) gx[idx] = 0.0;
+        for(auto coeff:LC->gx_coeff_and_index)
+        {
+
+            for (int ix = 0; ix < dim[0]; ix++)
+            {
+                int ixs = (ix+Lorder/2) * (dim[1] + Lorder) * (dim[2] + Lorder);
+                int ixc = ix * dim[1] * dim[2];
+
+                for (int iy = 0; iy < dim[1]; iy++)
+                {
+                    int iys = (iy+Lorder/2) * (dim[2] + Lorder) + ixs + Lorder/2;
+                    int iyc = iy * dim[2] + ixc;
+
+                    for(auto index: coeff.relative_index)
+                        for (int iz = 0; iz < dim[2]; iz++)
+                        {
+                            int idxs = iys + iz + index;
+                            int idxc = iyc + iz;
+                            gx[idxc]+= a[idxs] * (T)coeff.coeff;
+                        }
+                }
             }
         }
     }
 
-    for(int idx = 0; idx < dim[0] * dim[1] * dim[2]; idx++) gy[idx] = 0.0;
-    for(auto coeff:LC->gy_coeff_and_index)
+    if(typeid(T) == typeid(std::complex<double>))
     {
-
-        for (int ix = 0; ix < dim[0]; ix++)
+        double *aad = (double *)a;
+        double *gyd = (double *)gy;
+        for(int idx = 0; idx < 2 * dim[0] * dim[1] * dim[2]; idx++) gyd[idx] = 0.0;
+        for(auto coeff:LC->gy_coeff_and_index)
         {
-            int ixs = (ix+Lorder/2) * (dim[1] + Lorder) * (dim[2] + Lorder);
-            int ixc = ix * dim[1] * dim[2];
 
-            for (int iy = 0; iy < dim[1]; iy++)
+            for (int ix = 0; ix < dim[0]; ix++)
             {
-                int iys = (iy+Lorder/2) * (dim[2] + Lorder) + ixs + Lorder/2;
-                int iyc = iy * dim[2] + ixc;
+                int ixs = (ix+Lorder/2) * (dim[1] + Lorder) * (dim[2] + Lorder);
+                int ixc = ix * dim[1] * dim[2];
 
-                for(auto index: coeff.relative_index)
-             //      daxpy(&dim[2], &coeff.coeff, &a[iys+index], &ione, &b[iyc], &ione);
-                    for (int iz = 0; iz < dim[2]; iz++)
-                    {
-                        int idxs = iys + iz + index;
-                       int idxc = iyc + iz;
-                        gy[idxc]+= a[idxs] * (T)coeff.coeff;
-                    }
+                for (int iy = 0; iy < dim[1]; iy++)
+                {
+                    int iys = (iy+Lorder/2) * (dim[2] + Lorder) + ixs + Lorder/2;
+                    int iyc = iy * dim[2] + ixc;
+
+                    for(auto index: coeff.relative_index)
+                        for (int iz = 0; iz < dim[2]; iz++)
+                        {
+                            int idxs = iys + iz + index;
+                            int idxc = iyc + iz;
+                            gyd[2*idxc]+= aad[2*idxs] * (double)coeff.coeff;
+                            gyd[2*idxc+1]+= aad[2*idxs+1] * (double)coeff.coeff;
+                        }
+                }
+            }
+        }
+    }
+    else if(typeid(T) == typeid(std::complex<float>))
+    {
+        float *aad = (float *)a;
+        float *gyd = (float *)gy;
+        for(int idx = 0; idx < 2 * dim[0] * dim[1] * dim[2]; idx++) gyd[idx] = 0.0;
+        for(auto coeff:LC->gy_coeff_and_index)
+        {
+
+            for (int ix = 0; ix < dim[0]; ix++)
+            {
+                int ixs = (ix+Lorder/2) * (dim[1] + Lorder) * (dim[2] + Lorder);
+                int ixc = ix * dim[1] * dim[2];
+
+                for (int iy = 0; iy < dim[1]; iy++)
+                {
+                    int iys = (iy+Lorder/2) * (dim[2] + Lorder) + ixs + Lorder/2;
+                    int iyc = iy * dim[2] + ixc;
+
+                    for(auto index: coeff.relative_index)
+                        for (int iz = 0; iz < dim[2]; iz++)
+                        {
+                            int idxs = iys + iz + index;
+                            int idxc = iyc + iz;
+                            gyd[2*idxc]+= aad[2*idxs] * (float)coeff.coeff;
+                            gyd[2*idxc+1]+= aad[2*idxs+1] * (float)coeff.coeff;
+                        }
+                }
+            }
+        }
+    }
+    else
+    {
+        for(int idx = 0; idx < dim[0] * dim[1] * dim[2]; idx++) gy[idx] = 0.0;
+        for(auto coeff:LC->gy_coeff_and_index)
+        {
+
+            for (int ix = 0; ix < dim[0]; ix++)
+            {
+                int ixs = (ix+Lorder/2) * (dim[1] + Lorder) * (dim[2] + Lorder);
+                int ixc = ix * dim[1] * dim[2];
+
+                for (int iy = 0; iy < dim[1]; iy++)
+                {
+                    int iys = (iy+Lorder/2) * (dim[2] + Lorder) + ixs + Lorder/2;
+                    int iyc = iy * dim[2] + ixc;
+
+                    for(auto index: coeff.relative_index)
+                        for (int iz = 0; iz < dim[2]; iz++)
+                        {
+                            int idxs = iys + iz + index;
+                            int idxc = iyc + iz;
+                            gy[idxc]+= a[idxs] * (T)coeff.coeff;
+                        }
+                }
             }
         }
     }
 
-    for(int idx = 0; idx < dim[0] * dim[1] * dim[2]; idx++) gz[idx] = 0.0;
-    for(auto coeff:LC->gz_coeff_and_index)
+    if(typeid(T) == typeid(std::complex<double>))
     {
-
-        for (int ix = 0; ix < dim[0]; ix++)
+        double *aad = (double *)a;
+        double *gzd = (double *)gz;
+        for(int idx = 0; idx < 2 * dim[0] * dim[1] * dim[2]; idx++) gzd[idx] = 0.0;
+        for(auto coeff:LC->gz_coeff_and_index)
         {
-            int ixs = (ix+Lorder/2) * (dim[1] + Lorder) * (dim[2] + Lorder);
-            int ixc = ix * dim[1] * dim[2];
 
-            for (int iy = 0; iy < dim[1]; iy++)
+            for (int ix = 0; ix < dim[0]; ix++)
             {
-                int iys = (iy+Lorder/2) * (dim[2] + Lorder) + ixs + Lorder/2;
-                int iyc = iy * dim[2] + ixc;
+                int ixs = (ix+Lorder/2) * (dim[1] + Lorder) * (dim[2] + Lorder);
+                int ixc = ix * dim[1] * dim[2];
 
-                for(auto index: coeff.relative_index)
-             //      daxpy(&dim[2], &coeff.coeff, &a[iys+index], &ione, &b[iyc], &ione);
-                    for (int iz = 0; iz < dim[2]; iz++)
-                    {
-                        int idxs = iys + iz + index;
-                       int idxc = iyc + iz;
-                        gz[idxc]+= a[idxs] * (T)coeff.coeff;
-                    }
+                for (int iy = 0; iy < dim[1]; iy++)
+                {
+                    int iys = (iy+Lorder/2) * (dim[2] + Lorder) + ixs + Lorder/2;
+                    int iyc = iy * dim[2] + ixc;
+
+                    for(auto index: coeff.relative_index)
+                        for (int iz = 0; iz < dim[2]; iz++)
+                        {
+                            int idxs = iys + iz + index;
+                            int idxc = iyc + iz;
+                            gzd[2*idxc]+= aad[2*idxs] * (double)coeff.coeff;
+                            gzd[2*idxc+1]+= aad[2*idxs+1] * (double)coeff.coeff;
+                        }
+                }
+            }
+        }
+    }
+    else if(typeid(T) == typeid(std::complex<float>))
+    {
+        float *aad = (float *)a;
+        float *gzd = (float *)gz;
+        for(int idx = 0; idx < 2 * dim[0] * dim[1] * dim[2]; idx++) gzd[idx] = 0.0;
+        for(auto coeff:LC->gz_coeff_and_index)
+        {
+
+            for (int ix = 0; ix < dim[0]; ix++)
+            {
+                int ixs = (ix+Lorder/2) * (dim[1] + Lorder) * (dim[2] + Lorder);
+                int ixc = ix * dim[1] * dim[2];
+
+                for (int iy = 0; iy < dim[1]; iy++)
+                {
+                    int iys = (iy+Lorder/2) * (dim[2] + Lorder) + ixs + Lorder/2;
+                    int iyc = iy * dim[2] + ixc;
+
+                    for(auto index: coeff.relative_index)
+                        for (int iz = 0; iz < dim[2]; iz++)
+                        {
+                            int idxs = iys + iz + index;
+                           int idxc = iyc + iz;
+                            gzd[2*idxc]+= aad[2*idxs] * (float)coeff.coeff;
+                            gzd[2*idxc+1]+= aad[2*idxs+1] * (float)coeff.coeff;
+                        }
+                }
+            }
+        }
+    }
+    else
+    {
+        for(int idx = 0; idx < dim[0] * dim[1] * dim[2]; idx++) gz[idx] = 0.0;
+        for(auto coeff:LC->gz_coeff_and_index)
+        {
+
+            for (int ix = 0; ix < dim[0]; ix++)
+            {
+                int ixs = (ix+Lorder/2) * (dim[1] + Lorder) * (dim[2] + Lorder);
+                int ixc = ix * dim[1] * dim[2];
+
+                for (int iy = 0; iy < dim[1]; iy++)
+                {
+                    int iys = (iy+Lorder/2) * (dim[2] + Lorder) + ixs + Lorder/2;
+                    int iyc = iy * dim[2] + ixc;
+
+                    for(auto index: coeff.relative_index)
+                        for (int iz = 0; iz < dim[2]; iz++)
+                        {
+                            int idxs = iys + iz + index;
+                           int idxc = iyc + iz;
+                            gz[idxc]+= a[idxs] * (T)coeff.coeff;
+                        }
+                }
             }
         }
     }
--- RmgLib/src/FiniteDiffLap.cpp
+++ RmgLib/src/FiniteDiffLap.cpp
@@ -30,30 +30,99 @@ double FiniteDiffLap(T * __restrict__ a, T * __restrict__ b, int dimx, int dimy,
 
 
     for(int idx = 0; idx < dim[0] * dim[1] * dim[2]; idx++) b[idx] = 0.0;
-    for(auto coeff:LC->coeff_and_index)
+
+    // This code may seem unduly complicated but for the complex case it is almost twice as fast
+    // as the simple templated version. I think the issue is poor vectorization of complex data
+    // types but they may be treated as a double length array of floats or doubles instead which
+    // does vectorize well.
+
+    if(typeid(T) == typeid(std::complex<double>))
     {
-
-        for (int ix = 0; ix < dim[0]; ix++)
+        double *aad = (double *)a;
+        double *bbd = (double *)b;
+        for(auto coeff:LC->coeff_and_index)
         {
-            int ixs = (ix+Lorder/2) * (dim[1] + Lorder) * (dim[2] + Lorder);
-            int ixc = ix * dim[1] * dim[2];
 
-            for (int iy = 0; iy < dim[1]; iy++)
+            for (int ix = 0; ix < dim[0]; ix++)
             {
-                int iys = (iy+Lorder/2) * (dim[2] + Lorder) + ixs + Lorder/2;
-                int iyc = iy * dim[2] + ixc;
+                int ixs = (ix+Lorder/2) * (dim[1] + Lorder) * (dim[2] + Lorder);
+                int ixc = ix * dim[1] * dim[2];
 
-                for(auto index: coeff.relative_index)
-             //      daxpy(&dim[2], &coeff.coeff, &a[iys+index], &ione, &b[iyc], &ione);
-                    for (int iz = 0; iz < dim[2]; iz++)
-                    {
-                        int idxs = iys + iz + index;
-                       int idxc = iyc + iz;
-                        b[idxc]+= a[idxs] * (T)coeff.coeff;
-                    }
+                for (int iy = 0; iy < dim[1]; iy++)
+                {
+                    int iys = (iy+Lorder/2) * (dim[2] + Lorder) + ixs + Lorder/2;
+                    int iyc = iy * dim[2] + ixc;
+
+                    for(auto index: coeff.relative_index)
+                        for (int iz = 0; iz < dim[2]; iz++)
+                        {
+                            int idxs = iys + iz + index;
+                            int idxc = iyc + iz;
+                            bbd[2*idxc] += aad[2*idxs] * (double)coeff.coeff;
+                            bbd[2*idxc+1] += aad[2*idxs+1] * (double)coeff.coeff;
+                        }
+                }
+            }
+        }
+
+    }
+    else if(typeid(T) == typeid(std::complex<float>))
+    {
+        float *aad = (float *)a;
+        float *bbd = (float *)b;
+        for(auto coeff:LC->coeff_and_index)
+        {
+
+            for (int ix = 0; ix < dim[0]; ix++)
+            {
+                int ixs = (ix+Lorder/2) * (dim[1] + Lorder) * (dim[2] + Lorder);
+                int ixc = ix * dim[1] * dim[2];
+
+                for (int iy = 0; iy < dim[1]; iy++)
+                {
+                    int iys = (iy+Lorder/2) * (dim[2] + Lorder) + ixs + Lorder/2;
+                    int iyc = iy * dim[2] + ixc;
+
+                    for(auto index: coeff.relative_index)
+                        for (int iz = 0; iz < dim[2]; iz++)
+                        {
+                            int idxs = iys + iz + index;
+                            int idxc = iyc + iz;
+                            bbd[2*idxc] += aad[2*idxs] * (float)coeff.coeff;
+                            bbd[2*idxc+1] += aad[2*idxs+1] * (float)coeff.coeff;
+                        }
+                }
+            }
+        }
+
+    }
+    else
+    {
+        for(auto coeff:LC->coeff_and_index)
+        {
+
+            for (int ix = 0; ix < dim[0]; ix++)
+            {
+                int ixs = (ix+Lorder/2) * (dim[1] + Lorder) * (dim[2] + Lorder);
+                int ixc = ix * dim[1] * dim[2];
+
+                for (int iy = 0; iy < dim[1]; iy++)
+                {
+                    int iys = (iy+Lorder/2) * (dim[2] + Lorder) + ixs + Lorder/2;
+                    int iyc = iy * dim[2] + ixc;
+
+                    for(auto index: coeff.relative_index)
+                        for (int iz = 0; iz < dim[2]; iz++)
+                        {
+                            int idxs = iys + iz + index;
+                           int idxc = iyc + iz;
+                            b[idxc]+= a[idxs] * (T)coeff.coeff;
+                        }
+                }
             }
         }
     }
+
 
     /* Return the diagonal component of the operator */
     return LC->coeff_and_index[0].coeff;
