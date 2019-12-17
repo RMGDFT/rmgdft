3bd1ba2b171b2b91215561c6b187de4c89bbe918
Emil Briggs <290825+elbriggs@users.noreply.github.com>
2019-12-17 07:06:10 -0500

    No need to converge to greater than 1e-15 level so put in check.

--- RMG/Common/Scf.cpp
+++ RMG/Common/Scf.cpp
@@ -306,6 +306,7 @@ template <typename OrbitalType> bool Scf (double * vxc, double *vxc_in, double *
     // Check if this convergence threshold has been reached
     if(!Verify ("freeze_occupied", true, Kptr[0]->ControlMap)) {
       	if (ct.scf_steps && fabs(ct.scf_accuracy) < ct.exx_convergence_factor * ct.thr_energy) CONVERGED = true;
+        if (ct.scf_steps && fabs(ct.scf_accuracy < 1.0e-15)) CONVERGED = true;
     }
 
     if(CONVERGED || (ct.scf_steps == (ct.max_scf_steps-1)))
