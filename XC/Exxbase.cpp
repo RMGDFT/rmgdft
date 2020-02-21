d505a60c60f0f1c02491c39561c6c5be7c67e41a
Emil Briggs <emil@emils7370.briggspack.com>
2020-02-21 08:16:21 -0500

    Fixup end of loop cleanup.

--- XC/Exxbase.cpp
+++ XC/Exxbase.cpp
@@ -355,7 +355,7 @@ template <> void Exxbase<double>::Vexx(double *vexx, bool use_float_fft)
         }
         MPI_Waitall(reqcount, reqs, mrstatus);
         MPI_Barrier(G.comm);
-        int count = nstates_occ - trows;
+        int count = nstates - trows - rows;
 
         // This timer only picks up the last MPI_Allreduce since the ones above are asynchronous
         RmgTimer *RT3 = new RmgTimer("5-Functional: Exx allreduce");
