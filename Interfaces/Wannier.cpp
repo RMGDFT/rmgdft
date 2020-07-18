af2ef2c36ed8790d9fb8d4d2e0d4e05ad1907217
WenchangLu <wlu2@ncsu.edu>
2020-07-18 11:12:43 -0400

    wannier90 interface works with symmetry reduced kpoints.

--- Interfaces/Wannier.cpp
+++ Interfaces/Wannier.cpp
@@ -156,7 +156,7 @@ template <> void Wannier<std::complex<double>>::SetAmn()
             
             for(int idx = 0; idx < ngrid_noncoll; idx++)
             {
-                psi_s[st * ngrid_noncoll + idx] *= focc;
+                psi_s[st * ngrid_noncoll + idx] = focc * (psi_s[st * ngrid_noncoll + idx]);
             }
         }
 
@@ -203,61 +203,20 @@ template <> void Wannier<std::complex<double>>::SetAmn()
         int isym = ct.klist.k_map_symm[iq];
         int isyma = std::abs(isym)-1;
 
-        int sym_rot_inv[9], ftau_inv[3], det, sr[3][3];
-
-        for(int i= 0; i < 3; i++)
-        {
-            for(int j= 0; j < 3; j++)
-            {
-                sr[i][j] = Rmg_Symm->sym_rotate[isyma *9 +i*3 + j];
-            }
-        }
-        det = sr[0][0] * ( sr[1][1] * sr[2][2] - sr[2][1] * sr[1][2] ) -
-            sr[0][1] * ( sr[1][0] * sr[2][2] - sr[2][0] * sr[1][2] ) +
-            sr[0][2] * ( sr[1][0] * sr[2][1] - sr[2][0] * sr[1][1] );
-
-        for(int i= 0; i < 3; i++)
-        {
-            for(int j= 0; j < 3; j++)
-            {
-                sym_rot_inv[i*3+j] = sr[(i+1)%3 ][(j+1)%3] * sr[(i+2)%3 ][(j+2)%3];
-                sym_rot_inv[i*3+j] -= sr[(i+1)%3 ][(j+2)%3] * sr[(i+2)%3 ][(j+1)%3];
-                sym_rot_inv[i*3+j] /= det;
-
-            }
-        }
-
-        for(int i= 0; i < 3; i++)
-        {
-            ftau_inv[i] = 0;
-            for(int j= 0; j < 3; j++)
-            {
-                ftau_inv[i] -= sym_rot_inv[i*3+j] * Rmg_Symm->ftau_wave[isyma*3 + j];
-            }
-        }
-        std::string filename = wavefile + "_spin"+std::to_string(pct.spinpe) + "_kpt" + std::to_string(ik);
-        RmgTimer *RT1 = new RmgTimer("5-Wannier: mmap");
-        serial_fd = open(filename.c_str(), O_RDONLY, (mode_t)0600);
-        if(serial_fd < 0)
-            throw RmgFatalException() << "Error! Could not open " << filename << " . Wannier Terminating.\n";
-        delete RT1;
-
-        //psi_s = (std::complex<double> *)mmap(NULL, length, PROT_READ, MAP_PRIVATE, serial_fd, 0);
-        read(serial_fd, psi_s, length * sizeof(std::complex<double>));
+        ReadRotatePsi(ik, isym, isyma, wavefile, psi_s);
 
         int ix, iy, iz, ixx, iyy, izz;
         std::complex<double> phase_center;
         for(int iw = 0; iw < n_wannier; iw++)
         {
             int grid_idx = piv[iw]-1;
-            int ispin = 0, ispin_rev=0;
+            int ispin = 0;
             if(grid_idx >= ngrid) 
             {
                 ispin = 1;
                 grid_idx -= ngrid;
             }
 
-            if(ct.noncoll) ispin_rev = (ispin + 1) %2;
             ix = grid_idx/ny_grid/nz_grid;
             iy = (grid_idx/nz_grid ) % ny_grid;
             iz = grid_idx % nz_grid;
@@ -275,8 +234,6 @@ template <> void Wannier<std::complex<double>>::SetAmn()
             phase_center = std::exp(std::complex<double>(0.0, -kr_center * twoPI));
 
 
-            symm_ijk(sym_rot_inv, ftau_inv, ix, iy, iz, ixx, iyy, izz, nx_grid, ny_grid, nz_grid);
-
             for(int st = 0; st < nstates; st++)
             {
                 double focc = 1.0;
@@ -286,21 +243,14 @@ template <> void Wannier<std::complex<double>>::SetAmn()
                 else if(scdm == GAU_ENTANGLEMENT) focc = std::exp(-tem * tem);
                 else if(scdm == ERFC_ENTANGLEMENT) focc = 0.5 * erfc(tem);
                 else
+                {
                     throw RmgFatalException() << "scdm = " << scdm << "  wrong value \n";
-                if(isym >= 0) 
-                {
-                    psi_wan[iw * nstates + st] = std::conj(psi_s[st * ngrid_noncoll + ispin * ngrid 
-                        + ixx * ny_grid * nz_grid + iyy * nz_grid + izz]) * focc * phase_center;
-
                 }
-                else
-                {
-                    psi_wan[iw * nstates + st] = (psi_s[st * ngrid_noncoll + ispin_rev * ngrid 
-                            + ixx * ny_grid * nz_grid + iyy * nz_grid + izz])* focc * phase_center;
-                }
+                psi_wan[iw * nstates + st] = std::conj(psi_s[st * ngrid_noncoll + ispin * ngrid 
+                        + ix * ny_grid * nz_grid + iy * nz_grid + iz]) * focc * phase_center;
             }
-
         }
+
 
         double *sigma = new double[n_wannier]();
         double *rwork = new double[5*n_wannier]();
@@ -398,54 +348,54 @@ template <class T> void Wannier<T>::WriteWinEig()
         fprintf(fwin, "\nnum_iter        = 20");
         fprintf(fwin, "\nauto_projections= true\n");
         fprintf(fwin, "\nbands_plot       = true\n");
-        
-         fprintf(fwin, "bands_plot_format= xmgrace\n");
-         fprintf(fwin, "bands_num_points: 100\n");
-         fprintf(fwin, "begin kpoint_path\n");
-         fprintf(fwin, "   G 0.0 0.0 0.0 L 0.0 0.0 1.0\n");
-         fprintf(fwin, "  L 0.0 0.0 1.0 N 0.0 1.0 1.0\n");
-         fprintf(fwin, "end kpoint_path\n");
 
-         fprintf(fwin, "\nbegin atoms_cart");
-         fprintf(fwin, "\nbohr");
-         for(size_t ion = 0; ion < Atoms.size(); ion++)
-         {
+        fprintf(fwin, "bands_plot_format= xmgrace\n");
+        fprintf(fwin, "bands_num_points: 100\n");
+        fprintf(fwin, "begin kpoint_path\n");
+        fprintf(fwin, "   G 0.0 0.0 0.0 L 0.0 0.0 1.0\n");
+        fprintf(fwin, "  L 0.0 0.0 1.0 N 0.0 1.0 1.0\n");
+        fprintf(fwin, "end kpoint_path\n");
 
-             ION &Atom = Atoms[ion];
-             SPECIES &Type = Species[Atom.species];
+        fprintf(fwin, "\nbegin atoms_cart");
+        fprintf(fwin, "\nbohr");
+        for(size_t ion = 0; ion < Atoms.size(); ion++)
+        {
 
-             fprintf (fwin, "\n%-2s   %12.6e  %12.6e  %12.6e",
-                     Type.atomic_symbol, Atom.crds[0], Atom.crds[1], Atom.crds[2]);
+            ION &Atom = Atoms[ion];
+            SPECIES &Type = Species[Atom.species];
 
-         }         
-         fprintf(fwin, "\nend atoms_cart\n");
-         fprintf(fwin, "\nbegin unit_cell_cart");
-         fprintf(fwin, "\nbohr");
-         fprintf(fwin, "\n%10.6f   %10.6f   %10.6f", L.get_a0(0),L.get_a0(1),L.get_a0(2));
-         fprintf(fwin, "\n%10.6f   %10.6f   %10.6f", L.get_a1(0),L.get_a1(1),L.get_a1(2));
-         fprintf(fwin, "\n%10.6f   %10.6f   %10.6f", L.get_a2(0),L.get_a2(1),L.get_a2(2));
-         fprintf(fwin, "\nend_unit_cell_cart");
-         fprintf(fwin, "\n\nmp_grid : %d %d %d", ct.kpoint_mesh[0], ct.kpoint_mesh[1], ct.kpoint_mesh[2]);
+            fprintf (fwin, "\n%-2s   %12.6e  %12.6e  %12.6e",
+                    Type.atomic_symbol, Atom.crds[0], Atom.crds[1], Atom.crds[2]);
 
-         fprintf(fwin, "\n\nbegin kpoints");
-         for(int iq = 0; iq <ct.klist.num_k_all; iq++)
-         {
-             fprintf(fwin, "\n%10.6f   %10.6f   %10.6f", ct.klist.k_all_xtal[iq][0], ct.klist.k_all_xtal[iq][1], ct.klist.k_all_xtal[iq][2] );
-         }
-         fprintf(fwin, "\nend kpoints");
+        }         
+        fprintf(fwin, "\nend atoms_cart\n");
+        fprintf(fwin, "\nbegin unit_cell_cart");
+        fprintf(fwin, "\nbohr");
+        fprintf(fwin, "\n%10.6f   %10.6f   %10.6f", L.get_a0(0),L.get_a0(1),L.get_a0(2));
+        fprintf(fwin, "\n%10.6f   %10.6f   %10.6f", L.get_a1(0),L.get_a1(1),L.get_a1(2));
+        fprintf(fwin, "\n%10.6f   %10.6f   %10.6f", L.get_a2(0),L.get_a2(1),L.get_a2(2));
+        fprintf(fwin, "\nend_unit_cell_cart");
+        fprintf(fwin, "\n\nmp_grid : %d %d %d", ct.kpoint_mesh[0], ct.kpoint_mesh[1], ct.kpoint_mesh[2]);
 
-         fclose(fwin);
+        fprintf(fwin, "\n\nbegin kpoints");
+        for(int iq = 0; iq <ct.klist.num_k_all; iq++)
+        {
+            fprintf(fwin, "\n%10.6f   %10.6f   %10.6f", ct.klist.k_all_xtal[iq][0], ct.klist.k_all_xtal[iq][1], ct.klist.k_all_xtal[iq][2] );
+        }
+        fprintf(fwin, "\nend kpoints");
 
-         FILE *feig= fopen("Wannier90_rmg/wannier90.eig", "w+");
-         for(int iq = 0; iq <ct.klist.num_k_all; iq++)
-         {
-             int ik = ct.klist.k_map_index[iq];
-             for(int st = 0; st < nstates; st++)
-             {
-                 fprintf(feig, "%5d %5d  %18.12f\n", st+1, iq+1, ct.kp[ik].eigs[st] *Ha_eV);
-             }
-         }
-         fclose(feig);
+        fclose(fwin);
+
+        FILE *feig= fopen("Wannier90_rmg/wannier90.eig", "w+");
+        for(int iq = 0; iq <ct.klist.num_k_all; iq++)
+        {
+            int ik = ct.klist.k_map_index[iq];
+            for(int st = 0; st < nstates; st++)
+            {
+                fprintf(feig, "%5d %5d  %18.12f\n", st+1, iq+1, ct.kp[ik].eigs[st] *Ha_eV);
+            }
+        }
+        fclose(feig);
 
     }
 }
@@ -538,16 +488,16 @@ template <class T> void Wannier<T>::ReadRotatePsi(int ikindex, int isym, int isy
                 {
                     for(int st = 0; st < nstates; st++)
                     {
-                        psi_k[st * nbasis + ixx * ny_grid * nz_grid + iyy * nz_grid + izz]
-                            = (psi_map[st * nbasis + ix * ny_grid * nz_grid + iy * nz_grid + iz]);
+                        psi_k[st * nbasis + ix * ny_grid * nz_grid + iy * nz_grid + iz]
+                            = (psi_map[st * nbasis + ixx * ny_grid * nz_grid + iyy * nz_grid + izz]);
                     }
                 }
                 else
                 {
                     for(int st = 0; st < nstates; st++)
                     {
-                        psi_k[st * nbasis + ixx * ny_grid * nz_grid + iyy * nz_grid + izz]
-                            = MyConj(psi_map[st * nbasis + ix * ny_grid * nz_grid + iy * nz_grid + iz]);
+                        psi_k[st * nbasis + ix * ny_grid * nz_grid + iy * nz_grid + iz]
+                            = MyConj(psi_map[st * nbasis + ixx * ny_grid * nz_grid + iyy * nz_grid + izz]);
                     }
                 }
             }
@@ -600,7 +550,7 @@ template <class T> void Wannier<T>::SetMmn()
             int ikn_index = ct.klist.k_neighbors[ik][ikn][0];
             int ikn_irr = ct.klist.k_map_index[ikn_index];
             int isym_kn = ct.klist.k_map_symm[ikn_index];
-            int isyma_kn = std::abs(isym) -1;
+            int isyma_kn = std::abs(isym_kn) -1;
             ReadRotatePsi(ikn_irr, isym_kn, isyma_kn, wavefile, psi_q);
 
 
@@ -632,9 +582,6 @@ template <class T> void Wannier<T>::SetMmn()
 
     }
 
-    GpuFreeManaged(psi_k);
-    GpuFreeManaged(psi_q);
-    GpuFreeManaged(Mmn);
 
     if(pct.imgpe == 0)
     {
@@ -665,5 +612,9 @@ template <class T> void Wannier<T>::SetMmn()
         fclose(fmmn);
     }
 
+
+    GpuFreeManaged(psi_k);
+    GpuFreeManaged(psi_q);
+    GpuFreeManaged(Mmn);
 
 }
