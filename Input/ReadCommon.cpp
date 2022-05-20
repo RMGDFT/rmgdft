f838c44f08838a9df23c6467a9278c411f854871
Emil Briggs <290825+elbriggs@users.noreply.github.com>
2022-05-19 20:53:24 -0400

    Switched the default internal pseudopotential type to norm conserving ONCVP and updated tests to reflect the change.

--- Input/ReadCommon.cpp
+++ Input/ReadCommon.cpp
@@ -301,7 +301,7 @@ void ReadCommon(char *cfile, CONTROL& lc, PE_CONTROL& pelc, std::unordered_map<s
                      "Atomic Orbital Type. Choices are localized and delocalized. ", 
                      "atomic_orbital_type not found. ", PSEUDO_OPTIONS);
 
-    If.RegisterInputKey("internal_pseudo_type", NULL, &ct.internal_pseudo_type, "ultrasoft",
+    If.RegisterInputKey("internal_pseudo_type", NULL, &ct.internal_pseudo_type, "nc_accuracy",
                      CHECK_AND_TERMINATE, OPTIONAL, internal_pseudo_type,
                      "Internal pseudopotential type. Choices are ultrasoft, nc_accuracy ", 
                      "internal pseudopotential type not found. ", PSEUDO_OPTIONS);
