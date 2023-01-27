d393d3d700cd5c464c6111d52db981ff53cd159d
Emil Briggs <290825+elbriggs@users.noreply.github.com>
2023-01-27 13:54:08 -0500

    Potential acceleration and rho preconditioning may sometimes degrade convergence when both are selected so make them mutually exclusive.

--- Input/ReadCommon.cpp
+++ Input/ReadCommon.cpp
@@ -1709,6 +1709,11 @@ void ReadCommon(char *cfile, CONTROL& lc, PE_CONTROL& pelc, std::unordered_map<s
         rmg_error_handler (__FILE__, __LINE__, "\nError. You have selected Linear Mixing with the Davidson kohn-sham solver\nwhich is not valid. Please change to Broyden or Pulay mixing. Terminating.\n\n");
     }
 
+    if(lc.potential_acceleration_constant_step > 0.0)
+    {
+        lc.drho_precond = false;
+    }
+
     // Force grad order must match kohn_sham_fd_order unless fft is chose
     if(lc.force_grad_order != lc.kohn_sham_fd_order) lc.force_grad_order = 0;
 }
