44f62584194fcf03e43c1e10a61717d1a162f0cc
Jackson Burns <jwburns@mit.edu>
2023-06-24 17:12:07 -0400

    small formatting updates for `lattice.py`

--- RMG_GUI/lattice.py
+++ RMG_GUI/lattice.py
@@ -1,31 +1,31 @@
 # written by Wenchang Lu at NCSU
+#
+# Update Jun 24, 2023:
+# - Update by Jackson Burns for Python 3
 
-import os
 
-# here is the function that will return the lattice constant from bravias_lattice vector
+# here is the function that will return the lattice constant
+# from bravias_lattice vector
 def lattice(lattice):
     """
-    @return ascii string with lattice information 
+    @return ascii string with lattice information
     """
 
     # put parameters part into the snippet
-    #calculate length of unit cell vectors
-
+    # calculate length of unit cell vectors
 
     a = lattice[0]
     b = lattice[1]
     c = lattice[2]
 
-    # put unitcell information into the snippet
-
-    snippet = """
+    return """
 
 # Lattice constants in (x, y, z) directions
 # a, b, c
-a_length="%s" b_length="%s" c_length="%s"
+a_length="{:s}" b_length="{:s}" c_length="{:s}"
 
-""" %(a,b,c)
-
-    return snippet
-
-
+""".format(
+        a,
+        b,
+        c,
+    )
