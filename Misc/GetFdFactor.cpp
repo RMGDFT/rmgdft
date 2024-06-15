cc6f9c1a800f2f56514f4862cdfc0dc02522c746
Emil Briggs <290825+elbriggs@users.noreply.github.com>
2024-06-14 20:17:29 -0400

    Adaptive interpolation updates.

--- Misc/GetFdFactor.cpp
+++ Misc/GetFdFactor.cpp
@@ -32,23 +32,22 @@ double ComputeKineticEnergy(double *x, double *lapx, int pbasis)
 
 double ComputeRhoGoodness(double *x1, double *x2, int pbasis)
 {
-    double rg = 0.0;
+    double rg1 = 0.0;
     for(int idx=0;idx<pbasis;idx++)
     {
-        double t1 = x1[idx] - x2[idx];
-        rg += t1*t1;
+        double t1 = (x1[idx] - x2[idx]);
+        rg1 += sqrt(t1*t1);
     }
-    rg *= get_vel_f();
-    MPI_Allreduce(MPI_IN_PLACE, &rg, 1, MPI_DOUBLE, MPI_SUM, pct.grid_comm);
-    rg = std::pow(rg, 1.0/3.0);
-
-    return rg;
+    rg1 *= get_vel_f();
+    MPI_Allreduce(MPI_IN_PLACE, &rg1, 1, MPI_DOUBLE, MPI_SUM, pct.grid_comm);
+    return rg1;
 }
 
 
 void GetFdFactor(int kpt)
 {
     FiniteDiff FD(&Rmg_L);
+    int is_core_correction = false;
 
     if(ct.afd_cfac > 0.0)
     {
@@ -93,6 +92,7 @@ void GetFdFactor(int kpt)
     // Loop over species
     for (auto& sp : Species)
     {
+        if(sp.nlccflag) is_core_correction = true;
         sp.fd_slopes.clear();
         sp.fd_xint.clear();
         sp.fd_yint.clear();
@@ -132,6 +132,16 @@ void GetFdFactor(int kpt)
             coarse_pwaves->FftInverse(gbptr, beptr);
 
             for (int idx = 0; idx < pbasis; idx++) orbital[idx] = std::real(beptr[idx]);
+ 
+            // Make sure the orbital is normalized to 1.0
+            double snorm = 0.0;
+            for(int idx=0;idx < pbasis;idx++) snorm += std::real(orbital[idx] * std::conj(orbital[idx]));
+            GlobalSums(&snorm, 1, pct.grid_comm);
+            snorm *= get_vel();
+            snorm = 1.0 / sqrt(snorm);
+            for(int idx=0;idx < pbasis;idx++) orbital[idx] *= snorm;
+            if(ct.verbose && pct.gridpe==0 && pct.spinpe==0 && kpt==0)
+                printf("SNORM %d  %d  %14.8f\n",sp.num_orbitals,ip,snorm);
 
             /*Advance the fortward transform pointers */
             fptr += pbasis;
@@ -140,15 +150,6 @@ void GetFdFactor(int kpt)
             FftLaplacianCoarse(orbital, work);
             double fft_ke = ComputeKineticEnergy(orbital, work, pbasis);
 
-#if 0
- // Debug code for checking normalization
-                double snorm = 0.0;
-                for(int idx=0;idx < pbasis;idx++) snorm += std::real(orbital[idx] * std::conj(orbital[idx]));
-                GlobalSums(&snorm, 1, pct.grid_comm);
-                snorm *= get_vel();
-                snorm = 1.0 / sqrt(snorm);
-if(pct.gridpe==0)printf("SNORM %d  %d  %14.8f\n",sp.num_orbitals,ip,snorm);
-#endif
 
             // Now do the FFT Interpolation to the fine grid to use as
             // a reference standard for optimizing the prolongation operator.
@@ -176,22 +177,21 @@ if(pct.gridpe==0)printf("SNORM %d  %d  %14.8f\n",sp.num_orbitals,ip,snorm);
                 P.prolong(pwork2, orbital, ratio*pxdim, ratio*pydim, ratio*pzdim, 
                               pxdim, pydim, pzdim);
 
-#if 1
-                // Fix any normalization change from interpolation
                 double snorm_f = 0.0;
                 for(int idx=0;idx < fpbasis;idx++) snorm_f += std::real(pwork2[idx] * std::conj(pwork2[idx]));
                 GlobalSums(&snorm_f, 1, pct.grid_comm);
                 snorm_f *= get_vel_f();
                 snorm_f = 1.0 / sqrt(snorm_f);
                 for(int idx=0;idx < fpbasis;idx++) pwork2[idx] *= snorm_f;
-#endif
 
-                FftLaplacianFine(pwork2, pwork3);
                 double rhogood = ComputeRhoGoodness(pwork1, pwork2, fpbasis);
-                if(ct.verbose && pct.gridpe==0)printf("rhogood  %14.8f   %14.8e\n",c2, rhogood);
+                if(ct.verbose && pct.gridpe==0 && pct.spinpe==0 && kpt==0)
+                    printf("rhogood  %14.8f   %14.8e\n",c2, rhogood);
                 yarr2.push_back(rhogood);
                 c2 += dx; 
             }
+            if(ct.verbose && pct.gridpe==0 && pct.spinpe==0 && kpt==0)printf("rhogood  &&\n");
+
             // Setup data for adaptive finite differencing
             double m = (diffs[1] - diffs[0])/(cvals[1] - cvals[0]);
             double x_int = - diffs[0] / m;
@@ -203,16 +203,19 @@ if(pct.gridpe==0)printf("SNORM %d  %d  %14.8f\n",sp.num_orbitals,ip,snorm);
                 fprintf(ct.logfile,"IP=%d M = %e  %e  %e\n",ip,m,x_int,diffs[0]);
 
             // Linear fit for KE
+            MPI_Barrier(MPI_COMM_WORLD);
             static int korder = 1;
             static double coeffs[10];  // Fitted polynomial coefficients
 
             SimplePolyFit(cvals.data(), yarr1.data(), N, korder, coeffs);
             const int double_bits = std::numeric_limits<double>::digits;
-            if(ct.verbose && pct.gridpe==0)printf("ke params  %14.8f  %14.8f\n",coeffs[0], coeffs[1]);
+            if(ct.verbose && pct.gridpe==0 && pct.spinpe==0 && kpt==0)
+                printf("ke params  %14.8f  %14.8f\n",coeffs[0], coeffs[1]);
             double fd_xint = -coeffs[0]/coeffs[1];  // Want y-intercept
             sp.fd_mins.push_back(fd_xint);
 
-            // Cubic fit for rho
+            // Quadratic fit for rho
+            MPI_Barrier(MPI_COMM_WORLD);
             korder = 4;
             SimplePolyFit(cvals.data(), yarr2.data(), N, korder, coeffs);
 
@@ -231,7 +234,8 @@ if(pct.gridpe==0)printf("SNORM %d  %d  %14.8f\n",sp.num_orbitals,ip,snorm);
               }
             };
             std::pair<double, double> rhomin = brent_find_minima(func_rho(), 0.0, 4.0, double_bits);
-            if(ct.verbose && pct.gridpe==0)printf("rho params  %14.8f  %14.8f\n",rhomin.first, rhomin.second);
+            if(ct.verbose && pct.gridpe==0 && pct.spinpe==0 && kpt==0)
+                printf("rho params  %14.8e  %14.8e\n",rhomin.first, rhomin.second);
             sp.pd_mins.push_back(rhomin.first);
         }
     }
@@ -258,11 +262,14 @@ if(pct.gridpe==0)printf("SNORM %d  %d  %14.8f\n",sp.num_orbitals,ip,snorm);
 
     if(ct.use_cmix && ct.prolong_order > 2 && std::abs(pweight) > 1.0e-8)
     {
-        if(ct.prolong_order > 2 && std::abs(pweight) > 1.0e-8)
+        if(is_core_correction == false && ct.norm_conserving_pp)
         {
-            if(ct.verbose && pct.gridpe == 0) 
-                printf("cmix = %f\n", p/pweight);
             ct.cmix = p/pweight;
+        }
+        else
+        {
+            if(pct.gridpe==0 && pct.spinpe==0 && kpt==0)
+                printf("Notice: Adaptive interpolation is disabled for USPP and NLCC pseudopotentials.\n");
         }
     }
 
