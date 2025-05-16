56230a8959baa1cdb8890fa7e6f5e7c573db78b6
Emil Briggs <290825+elbriggs@users.noreply.github.com>
2025-05-16 14:49:18 -0400

    When performing relaxations reset mixing parameter to initial value in case it was automatically reduceced.

--- Headers/rmg_control.h
+++ Headers/rmg_control.h
@@ -415,6 +415,7 @@ public:
 
     /** Density mixing parameter. Typical values range from 0.2 to 0.9, while larger values provide faster convergence as long as they are stable. */
     double mix;
+    double init_mix;
     double drho_q0;
     int drho_precond_type;
 
