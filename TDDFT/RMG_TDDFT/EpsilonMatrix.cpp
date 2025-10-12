ac91eb226d57b14f857e5810067c8cc80a7a681d
WenchangLu <wlu2@ncsu.edu>
2025-10-12 00:26:12 -0400

    add Lorentzian for optical epsilon

--- TDDFT/RMG_TDDFT/EpsilonMatrix.cpp
+++ TDDFT/RMG_TDDFT/EpsilonMatrix.cpp
@@ -123,14 +123,16 @@ void EpsilonMatrix (Kpoint<KpointType> **Kptr)
     KpointType *block_matrix_y = block_matrix_x + num_states * nb;
     KpointType *block_matrix_z = block_matrix_y + num_states * nb;
 
-    double Emax = 10.0/Ha_eV;
-    int Epoints = 1001;
+    double Emax = 50.0/Ha_eV;
+    int Epoints = 5001;
     double delta_e = Emax/(Epoints-1);
     std::vector<double> epsilon[9];
+    std::vector<double> epsilon_lorentzian[9];
     double eps = 1.0e-6;
     for(int i = 0; i < 9; i++)
     {
         epsilon[i].resize(Epoints, 0.0);
+        epsilon_lorentzian[i].resize(Epoints, 0.0);
     }
     // ct.gaus_broad is in unit of eV
     double gaus_broad = ct.gaus_broad/Ha_eV;
@@ -264,7 +266,6 @@ void EpsilonMatrix (Kpoint<KpointType> **Kptr)
 
         }
 
-//for(int i = 0; i < num_states; i++) rmg_printf("\n aaa %d %f %f", i, Pmat[0][i*num_states + i]);
         int vmax_st = 0;
         int cmin_st = 0;
 
@@ -313,6 +314,19 @@ void EpsilonMatrix (Kpoint<KpointType> **Kptr)
                     }
                 }
 
+                for (int ie = 0; ie < Epoints; ie++)
+                {
+                    double coeff = gaus_broad/(gaus_broad * gaus_broad+(ie-egrid_center) * (ie-egrid_center) * delta_e * delta_e) * occ_diff * kptr->kp.kweight/PI;
+                    for(int ix = 0; ix < 3; ix++)
+                    {
+                        for(int iy = 0; iy < 3; iy++)
+                        {
+                            tem = coeff * std::real(MyConj(Pmat[ix][stv * num_states + stc]) * Pmat[iy][stv * num_states+stc]);
+                            epsilon_lorentzian[ix*3+iy][ie] += tem;
+                        }
+                    }
+                }
+
             }
         }
 
@@ -355,6 +369,8 @@ void EpsilonMatrix (Kpoint<KpointType> **Kptr)
     {
         BlockAllreduce(epsilon[i].data(), (size_t)Epoints , pct.grid_comm);
         BlockAllreduce(epsilon[i].data(), (size_t)Epoints , pct.kpsub_comm);
+        BlockAllreduce(epsilon_lorentzian[i].data(), (size_t)Epoints , pct.grid_comm);
+        BlockAllreduce(epsilon_lorentzian[i].data(), (size_t)Epoints , pct.kpsub_comm);
     }
 
     double eps_tem[9];
@@ -368,6 +384,15 @@ void EpsilonMatrix (Kpoint<KpointType> **Kptr)
         for(int i = 0; i < 9; i++)
         {
             epsilon[i][ie] = eps_tem[i] * 4.0 * PI *PI/(ene * ene)/L->get_omega()/gaus_broad /std::sqrt(PI);
+
+        }
+
+        for(int i = 0; i < 9; i++) eps_tem[i] = epsilon_lorentzian[i][ie];
+        Rmg_Symm->symmetrize_tensor(eps_tem);
+
+        for(int i = 0; i < 9; i++)
+        {
+            epsilon_lorentzian[i][ie] = eps_tem[i] * 4.0 * PI *PI/(ene * ene)/L->get_omega();
 
         }
     }
@@ -401,6 +426,15 @@ void EpsilonMatrix (Kpoint<KpointType> **Kptr)
         }
 
 
+        fclose(eps_fi);
+
+        filename = "Epsilon/epsilonLorentzian_spin"+std::to_string(pct.spinpe)+".dat";
+        eps_fi = fopen(filename.c_str(), "w");
+        fprintf(eps_fi, "&& imag part epsilon2(omega) tensor, xx, yy,zz xy, xz, yz");
+        for(int ie = 0; ie < Epoints; ie++)
+        {
+            fprintf(eps_fi, "\n%f    %e %e %e   %e %e %e", ie*delta_e*Ha_eV,epsilon_lorentzian[0][ie], epsilon_lorentzian[4][ie], epsilon_lorentzian[8][ie], epsilon_lorentzian[1][ie], epsilon_lorentzian[2][ie], epsilon_lorentzian[5][ie]);
+        }
         fclose(eps_fi);
     }
 
