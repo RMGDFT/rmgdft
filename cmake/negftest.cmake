6ea336c641d76a8799e629594a37a84dea867dfe
WenchangLu <wlu2@ncsu.edu>
2026-01-25 23:49:11 -0500

    change input.00.log  to input.ref.log in the test directories. change the way to delete working directories

--- cmake/negftest.cmake
+++ cmake/negftest.cmake
@@ -1,7 +1,6 @@
-file(REMOVE_RECURSE ${WORKDIR}/*/input.00.log ${WORKDIR}/*/input.00.options 
-    ${WORKDIR}/*/input.00.rmsdv.xmgr ${WORKDIR}/*/Waves
-    ${WORKDIR}/*/input.110.00.log ${WORKDIR}/*/input.110.00.options 
-    ${WORKDIR}/*/input.110.00.cond_12.dat )
+file(REMOVE_RECURSE "${WORKDIR}")
+EXECUTE_PROCESS( COMMAND ${CMAKE_COMMAND} -E copy_directory "${SRC_DIR}" "${WORKDIR}" )
+
 execute_process(COMMAND mpirun -n ${PROCS} ${RMG_ON} input WORKING_DIRECTORY ${WORKDIR}/lead1)
 execute_process(COMMAND mpirun -n ${PROCS} ${RMG_ON} input WORKING_DIRECTORY ${WORKDIR}/center)
 execute_process(COMMAND mpirun -n ${PROCS} ${RMG_NEGF} input WORKING_DIRECTORY ${WORKDIR}/3lead_lead1)
--- cmake/runtest.cmake
+++ cmake/runtest.cmake
@@ -1,4 +1,6 @@
-file(REMOVE_RECURSE ${WORKDIR}/input.00.log ${WORKDIR}/input.00.options ${WORKDIR}/input.00.rmsdv.xmgr ${WORKDIR}/Waves)
+file(REMOVE_RECURSE "${WORKDIR}")
+EXECUTE_PROCESS( COMMAND ${CMAKE_COMMAND} -E copy_directory "${SRC_DIR}" "${WORKDIR}" )
+
 execute_process(COMMAND mpirun -n ${PROCS} ${RMG_EXE} input WORKING_DIRECTORY ${WORKDIR})
 execute_process(COMMAND python3 ${SECCMD}  WORKING_DIRECTORY ${WORKDIR})
 #execute_process(COMMAND mpirun -n 8 /home/luw/rmgdft/build/rmg-cpu input WORKING_DIRECTORY ${WORKDIR})
