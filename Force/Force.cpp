8c6d764a40adb99094cec00bc5f6b0e1c3a10604
Emil Briggs <290825+elbriggs@users.noreply.github.com>
2026-05-30 15:42:41 -0400

    Remove force correction term for further testing.

--- Force/Force.cpp
+++ Force/Force.cpp
@@ -151,8 +151,8 @@ template <typename OrbitalType> void Force (double * rho, double * rho_oppo, dou
        (ct.forceflag != TDDFT_CVE) &&
        (ct.forceflag != TDDFT))
     {
-        CorrectForces (vh, vh_in, vxc, vxc_in, force_tmp);
-        for(int i = 0; i < num_ions * 3; i++) force_sum[i] += force_tmp[i];
+        //CorrectForces (vh, vh_in, vxc, vxc_in, force_tmp);
+        //for(int i = 0; i < num_ions * 3; i++) force_sum[i] += force_tmp[i];
     }
     delete RT5;
 
