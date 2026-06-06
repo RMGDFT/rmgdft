1c4874ab6bdb3cad4fbe4df6ef6b76508c0755ed
WenchangLu <wlu2@ncsu.edu>
2026-06-06 10:13:16 -0400

    fix a bug

--- TDDFT/RMG_TDDFT/rmg_tddft.cpp
+++ TDDFT/RMG_TDDFT/rmg_tddft.cpp
@@ -620,7 +620,7 @@ void rmg::tddft<OrbitalType, MatrixType>::tddft_md(void)
 
         }
           // Save T=0 basis
-          for(int kpt=0;kpt < ct.num_kpts_pe;kpt++) Kptr[kpt]->save_wavefunctions();
+          this->Kptr[kpt]->save_wavefunctions();
     }
 
     first_step++;
