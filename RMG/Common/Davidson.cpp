c8be2175f2b88720723769eed1a847448b0e675f
Emil Briggs <290825+elbriggs@users.noreply.github.com>
2026-06-12 13:49:59 -0400

    Force termination if any proc fails.

--- RMG/Common/Davidson.cpp
+++ RMG/Common/Davidson.cpp
@@ -338,13 +338,14 @@ template <class KpointType> void Kpoint<KpointType>::Davidson(double *vtot, doub
 
         RT1 = new RmgTimer("6-Davidson: diagonalization");
         int info = GeneralDiag(hr.data(), sr.data(), eigsw, vr.data(), nbase, nstates, max_states, ct.subdiag_driver);
+        //rmg::reduce(&info, 1, pct.grid_comm);
         delete RT1;
         if(info) {
             if(pct.gridpe == 0) printf("\n WARNING: Davidson GeneralDiag info = %d", info);
             // Not clear what is the best path forward here. Sometimes we can recover from this but
             // sometimes the calculation is hosed so for now print a warming.
-            return;
-            //throw RmgFatalException() << info<<" " <<nstates <<" " << nbase << " Diagonalization failed in Davidson, terminating." << " in " << __FILE__ << " at line " << __LINE__ << "\n";
+            //return;
+            throw RmgFatalException() << info<<" " <<nstates <<" " << nbase << " Diagonalization failed in Davidson, terminating." << " in " << __FILE__ << " at line " << __LINE__ << "\n";
         }
 
         // Check convergence
